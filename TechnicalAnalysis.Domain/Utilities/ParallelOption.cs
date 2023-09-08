5e6ae6eb3daeb89c717ec6e0ad3f7382dcc9952a
Nikos S <niksal1991@gmail.com>
2023-09-09 01:12:12 +0300

    Extracted parallel

---
+++ TechnicalAnalysis.Domain/Utilities/ParallelOption.cs
@@ -0,0 +1,20 @@
+﻿namespace TechnicalAnalysis.Domain.Utilities
+{
+    public static class ParallelOption
+    {
+        private static readonly ParallelOptions _options;
+
+        static ParallelOption()
+        {
+            _options = new ParallelOptions
+            {
+                MaxDegreeOfParallelism = Environment.ProcessorCount / 4
+            };
+        }
+
+        public static ParallelOptions GetOptions()
+        {
+            return _options;
+        }
+    }
+}
