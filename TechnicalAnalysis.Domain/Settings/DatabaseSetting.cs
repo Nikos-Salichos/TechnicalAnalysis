838afdd36a6751d1f316acbef0ddf39e4894a8b0
Nikos S <niksal1991@gmail.com>
2023-09-12 19:13:55 +0300

    Change Settings

--- TechnicalAnalysis.Domain/Settings/DatabaseSetting.cs
+++ TechnicalAnalysis.Domain/Settings/DatabaseSetting.cs
@@ -2,9 +2,6 @@
 {
     public class DatabaseSetting
     {
-        public string PostgreSqlTechnicalAnalysisLocalhost { get; init; }
         public string PostgreSqlTechnicalAnalysisDockerCompose { get; init; }
-        public string RedisLocalhost { get; init; }
-        public string RedisDockerCompose { get; init; }
     }
 }
