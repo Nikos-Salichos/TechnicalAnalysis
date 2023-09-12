931e6b243c1db0ab744e8f7f9e7062700f2a4c5b
Nikos S <niksal1991@gmail.com>
2023-09-12 19:13:39 +0300

    Added sync service

--- TechnicalAnalysis.Application/Services/SyncService.cs
+++ TechnicalAnalysis.Application/Services/SyncService.cs
@@ -17,42 +17,55 @@ namespace TechnicalAnalysis.Application.Services
             _adapterFactory = adapterFactory;
         }
 
-        public async Task SynchronizeProvidersAsync(Provider provider, Timeframe timeframe)
+        public Task<string> SynchronizeProvidersAsync(Provider provider, Timeframe timeframe)
         {
-            var adaptersToSync = new List<Task>();
-
-            _logger.LogInformation("Method: {Method} Synchronization started for {Provider}", nameof(SynchronizeProvidersAsync), provider);
-
-            if (provider == Provider.Binance || provider == Provider.All)
+            if (timeframe != Timeframe.Daily &&
+                (timeframe != Timeframe.Weekly || provider != Provider.Binance))
             {
-                adaptersToSync.Add(GetAndSyncAdapter(Provider.Binance));
+                return Task.FromResult($"Combination {provider} and {timeframe} timeframe is not supported yet");
             }
 
-            if (provider == Provider.Alpaca || provider == Provider.All)
-            {
-                adaptersToSync.Add(GetAndSyncAdapter(Provider.Alpaca));
-            }
+            return InternalSynchronizeProvidersAsync(provider, timeframe);
 
-            if (provider == Provider.Uniswap || provider == Provider.All)
+            async Task<string> InternalSynchronizeProvidersAsync(Provider provider, Timeframe timeframe)
             {
-                adaptersToSync.Add(GetAndSyncAdapter(Provider.Uniswap));
-            }
+                var adaptersToSync = new List<Task>();
 
-            if (provider == Provider.Pancakeswap || provider == Provider.All)
-            {
-                adaptersToSync.Add(GetAndSyncAdapter(Provider.Pancakeswap));
-            }
+                _logger.LogInformation("Method: {Method} Synchronization started for {Provider}", nameof(SynchronizeProvidersAsync), provider);
 
-            if (adaptersToSync.Count > 0)
-            {
-                await Task.WhenAll(adaptersToSync);
+                if (provider == Provider.Binance || provider == Provider.All)
+                {
+                    adaptersToSync.Add(GetAndSyncAdapter(Provider.Binance, timeframe));
+                }
+
+                if (provider == Provider.Alpaca || provider == Provider.All)
+                {
+                    adaptersToSync.Add(GetAndSyncAdapter(Provider.Alpaca, timeframe));
+                }
+
+                if (provider == Provider.Uniswap || provider == Provider.All)
+                {
+                    adaptersToSync.Add(GetAndSyncAdapter(Provider.Uniswap, timeframe));
+                }
+
+                if (provider == Provider.Pancakeswap || provider == Provider.All)
+                {
+                    adaptersToSync.Add(GetAndSyncAdapter(Provider.Pancakeswap, timeframe));
+                }
+
+                if (adaptersToSync.Count > 0)
+                {
+                    await Task.WhenAll(adaptersToSync);
+                }
+
+                return "Synchronization Completed";
             }
         }
 
-        private Task GetAndSyncAdapter(Provider provider)
+        private Task GetAndSyncAdapter(Provider provider, Timeframe timeframe)
         {
             var adapter = _adapterFactory(provider);
-            return adapter.Sync(provider);
+            return adapter.Sync(provider, timeframe);
         }
     }
 }
