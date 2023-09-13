4dfedd1606cb8cf16ba9263ef74ce7711aace6a6
Nikos S <niksal1991@gmail.com>
2023-09-14 01:49:51 +0300

    Changed ext

---
+++ TechnicalAnalysis.Application/Extensions/AdapterExtension.cs
@@ -0,0 +1,17 @@
+﻿using TechnicalAnalysis.CommonModels.BusinessModels;
+using TechnicalAnalysis.CommonModels.Enums;
+
+namespace TechnicalAnalysis.Application.Extensions
+{
+    public static class AdapterExtension
+    {
+        public static bool IsProviderSyncedToday(this Provider provider, Timeframe timeframe)
+        {
+            return provider?.LastAssetSync.Date == DateTime.UtcNow.Date
+                && provider?.LastPairSync.Date == DateTime.UtcNow.Date
+                && provider.CandlestickSyncInfos.Any(candlestickSyncInfo =>
+                    candlestickSyncInfo?.LastCandlestickSync.Date == DateTime.UtcNow.Date
+                    && candlestickSyncInfo.Timeframe == timeframe);
+        }
+    }
+}
