042f64ced175f06274a9c51f9a148f7e4378929b
Nikos S <niksal1991@gmail.com>
2023-09-14 01:50:49 +0300

    Added Hangfire job on startup

--- TechnicalAnalysis.Infrastructure.Host/Hangfire/HangfireStartupJob.cs
+++ TechnicalAnalysis.Infrastructure.Host/Hangfire/HangfireStartupJob.cs
@@ -6,13 +6,14 @@ namespace TechnicalAnalysis.Infrastructure.Host.Hangfire
 {
     public class HangfireStartupJob
     {
-        public static void SynchronizeProvidersAsyncJob(WebApplication app)
+        public static void EnqueueSynchronizeProvidersJob(WebApplication app)
         {
             var serviceProvider = app.Services;
             var syncService = serviceProvider.GetService<ISyncService>();
+
             if (syncService != null)
             {
-                BackgroundJob.Enqueue(() => syncService.SynchronizeProvidersAsync(Provider.All, Timeframe.Daily));
+                BackgroundJob.Enqueue(() => syncService.SynchronizeProvidersAsync(DataProvider.Alpaca, Timeframe.Daily));
             }
         }
     }
