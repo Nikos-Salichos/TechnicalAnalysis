ddd4543a07d9b75574d1d553a0268c818df7c460
Nikos S <niksal1991@gmail.com>
2023-09-15 10:37:53 +0300

    Changed ext

---
+++ TechnicalAnalysis.Application/Extensions/DataProviderExtension.cs
@@ -0,0 +1,69 @@
+﻿using TechnicalAnalysis.CommonModels.BusinessModels;
+using TechnicalAnalysis.CommonModels.Enums;
+
+namespace TechnicalAnalysis.Application.Extensions
+{
+    public static class DataProviderExtension
+    {
+        public static bool IsProviderSyncedToday(this ProviderSynchronization provider, Timeframe timeframe)
+        {
+            if (provider?.ProviderPairAssetSyncInfo.LastAssetSync.Date == DateTime.UtcNow.Date
+                && provider?.ProviderPairAssetSyncInfo.LastPairSync.Date == DateTime.UtcNow.Date
+                && provider.CandlestickSyncInfos.Any(candlestickSyncInfo =>
+                    candlestickSyncInfo?.LastCandlestickSync.Date == DateTime.UtcNow.Date
+                    && candlestickSyncInfo.Timeframe == timeframe
+                    && timeframe == Timeframe.Daily))
+            {
+                return true;
+            }
+
+            DateTime currentDate = DateTime.UtcNow;
+            DayOfWeek currentDayOfWeek = currentDate.DayOfWeek;
+
+            int dayDifference = (currentDayOfWeek - DayOfWeek.Monday + 7) % 7;
+
+            if (provider?.ProviderPairAssetSyncInfo.LastAssetSync.Date == DateTime.UtcNow.Date
+            && provider?.ProviderPairAssetSyncInfo.LastPairSync.Date == DateTime.UtcNow.Date
+            && provider.CandlestickSyncInfos.Any(candlestickSyncInfo =>
+                    candlestickSyncInfo?.LastCandlestickSync.Date == DateTime.UtcNow.Date
+                    && dayDifference > 0
+                    && candlestickSyncInfo.Timeframe == timeframe
+                    && timeframe == Timeframe.Weekly))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static void UpdateProviderInfo(this ProviderSynchronization provider)
+        {
+            var currentTime = DateTime.UtcNow;
+            provider.ProviderPairAssetSyncInfo.LastAssetSync = currentTime;
+            provider.ProviderPairAssetSyncInfo.LastPairSync = currentTime;
+        }
+
+        public static ProviderCandlestickSyncInfo GetOrCreateProviderCandlestickSyncInfo(this ProviderSynchronization providerSynchronization, DataProvider provider, Timeframe timeframe)
+        {
+            var providerCandlestickSyncInfoProviderFound = providerSynchronization.CandlestickSyncInfos
+                .Find(c => c.DataProvider == provider && c.Timeframe == timeframe);
+
+            if (providerCandlestickSyncInfoProviderFound is null)
+            {
+                var newProviderCandlestickSyncInfo = new ProviderCandlestickSyncInfo(providerSynchronization.DataProvider)
+                {
+                    Timeframe = timeframe,
+                    LastCandlestickSync = DateTime.UtcNow
+                };
+
+                providerSynchronization.CandlestickSyncInfos.Add(newProviderCandlestickSyncInfo);
+                return newProviderCandlestickSyncInfo;
+            }
+            else
+            {
+                return providerCandlestickSyncInfoProviderFound;
+            }
+        }
+
+    }
+}
