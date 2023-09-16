cae3092888c72931e27e5ff5fdd09b9808d5d9b5
Nikos S <niksal1991@gmail.com>
2023-09-16 10:54:40 +0300

    Changed request

---
+++ TechnicalAnalysis.CommonModels/ApiRequests/DataProviderTimeframeRequest.cs
@@ -0,0 +1,10 @@
+﻿using TechnicalAnalysis.CommonModels.Enums;
+
+namespace TechnicalAnalysis.CommonModels.ApiRequests
+{
+    public class DataProviderTimeframeRequest
+    {
+        public DataProvider DataProvider { get; init; } = DataProvider.All;
+        public Timeframe Timeframe { get; init; } = Timeframe.Daily;
+    }
+}
