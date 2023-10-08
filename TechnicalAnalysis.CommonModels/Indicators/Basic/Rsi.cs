131379bd5efbb43ee9628ae33efccd5df03241b1
Nikos S <niksal1991@gmail.com>
2023-10-08 08:09:04 +0300

    Replaced value with constant

--- TechnicalAnalysis.CommonModels/Indicators/Basic/Rsi.cs
+++ TechnicalAnalysis.CommonModels/Indicators/Basic/Rsi.cs
@@ -1,12 +1,13 @@
 ﻿using TechnicalAnalysis.CommonModels.BaseClasses;
+using TechnicalAnalysis.CommonModels.Enums;
 
 namespace TechnicalAnalysis.CommonModels.Indicators.Basic
 {
     public class Rsi : BaseIndicator
     {
         public long Period { get; init; }
-        public double Overbought { get; init; } = 80;
-        public double Oversold { get; init; } = 20;
+        public double Overbought { get; init; } = Constants.RsiOverbought;
+        public double Oversold { get; init; } = Constants.RsiOversold;
         public double? Value { get; init; }
         public long NumberOfRsiLowerThanPreviousRsis { get; init; }
         public bool RsiChangedDirectionFromPreviousCandlestick { get; set; }
