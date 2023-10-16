d2500fa91b98e9a2f336a0c86a34392e74cb5a29
Nikos S <niksal1991@gmail.com>
2023-10-16 18:38:53 +0300

    Added default empty

--- TechnicalAnalysis.CommonModels/BusinessModels/PairExtended.cs
+++ TechnicalAnalysis.CommonModels/BusinessModels/PairExtended.cs
@@ -6,19 +6,19 @@ namespace TechnicalAnalysis.CommonModels.BusinessModels
 {
     public class PairExtended : BaseEntity
     {
-        public string Symbol { get; set; }
-        public string ContractAddress { get; init; }
+        public string Symbol { get; set; } = string.Empty;
+        public string ContractAddress { get; init; } = string.Empty;
         public long BaseAssetId { get; set; }
         public long QuoteAssetId { get; set; }
-        public string BaseAssetContract { get; init; }
-        public string QuoteAssetContract { get; init; }
-        public string BaseAssetName { get; set; }
-        public string QuoteAssetName { get; set; }
+        public string BaseAssetContract { get; init; } = string.Empty;
+        public string QuoteAssetContract { get; init; } = string.Empty;
+        public string BaseAssetName { get; set; } = string.Empty;
+        public string QuoteAssetName { get; set; } = string.Empty;
         public DataProvider Provider { get; set; }
         public bool IsActive { get; set; }
         public bool AllCandles { get; init; }
         public DateTime CreatedAt { get; init; }
-        public string FeeTier { get; init; }
+        public string FeeTier { get; init; } = string.Empty;
         public decimal? Fees { get; init; }
         public long? Liquidity { get; init; }
         public decimal? TotalValueLocked { get; init; }
