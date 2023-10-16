b0e3b15ab88f8f0d807bda3d69c15ac871e0ed90
Nikos S <niksal1991@gmail.com>
2023-10-16 18:39:10 +0300

    Added async suffix

--- TechnicalAnalysis.Domain/Interfaces/Infrastructure/IPostgreSqlRepository.cs
+++ TechnicalAnalysis.Domain/Interfaces/Infrastructure/IPostgreSqlRepository.cs
@@ -6,21 +6,22 @@ namespace TechnicalAnalysis.Domain.Interfaces.Infrastructure
 {
     public interface IPostgreSqlRepository
     {
-        Task<IResult<IEnumerable<ProviderSynchronization>, string>> GetProviders();
-        Task<IResult<IEnumerable<Candlestick>, string>> GetCandlesticks();
-        Task<IResult<IEnumerable<Pair>, string>> GetPairs();
-        Task<IResult<IEnumerable<Asset>, string>> GetAssets();
-        Task<IResult<IEnumerable<Pool>, string>> GetPools();
-        Task<IResult<IEnumerable<DexCandlestick>, string>> GetDexCandlesticks();
-        Task InsertPairs(IEnumerable<Pair> pairs);
-        Task InsertCandlesticks(IEnumerable<Candlestick> candlesticks);
-        Task InsertAssets(IEnumerable<Asset> assets);
-        Task UpdateProviderPairAssetSyncInfo(ProviderPairAssetSyncInfo providerPairAssetSyncInfos);
-        Task UpdateProviderCandlestickSyncInfo(ProviderCandlestickSyncInfo providerCandlestickSyncInfos);
-        Task InsertPools(IEnumerable<Pool> pools);
-        Task InsertDexCandlesticks(IEnumerable<DexCandlestick> candlesticks);
-        Task DeletePoolsByIds(IEnumerable<long> ids);
-        Task DeleteDexCandlesticksByIds(IEnumerable<long> ids);
-        Task DeleteTokensByIds(IEnumerable<long> ids);
+        Task<IResult<IEnumerable<ProviderSynchronization>, string>> GetProvidersAsync();
+        Task<IResult<IEnumerable<Candlestick>, string>> GetCandlesticksAsync();
+        Task<IResult<IEnumerable<Pair>, string>> GetPairsAsync();
+        Task<IResult<IEnumerable<Asset>, string>> GetAssetsAsync();
+        Task<IResult<IEnumerable<Pool>, string>> GetPoolsAsync();
+        Task<IResult<IEnumerable<DexCandlestick>, string>> GetDexCandlestickssAsync();
+        Task InsertPairsAsync(IEnumerable<Pair> pairs);
+        Task InsertCandlesticksAsync(IEnumerable<Candlestick> candlesticks);
+        Task InsertAssetsAsync(IEnumerable<Asset> assets);
+        Task UpdateProviderPairAssetSyncInfoAsync(ProviderPairAssetSyncInfo providerPairAssetSyncInfos);
+        Task UpdateProviderCandlestickSyncInfoAsync(ProviderCandlestickSyncInfo providerCandlestickSyncInfos);
+        Task InsertPoolsAsync(IEnumerable<Pool> pools);
+        Task InsertDexCandlesticksAsync(IEnumerable<DexCandlestick> candlesticks);
+        Task DeletePoolsByIdsAsync(IEnumerable<long> ids);
+        Task DeleteDexCandlesticksByIdsAsync(IEnumerable<long> ids);
+        Task DeleteTokensByIdsAsync(IEnumerable<long> ids);
+        Task DeleteAssetsAsync();
     }
 }
