32eeb844dff12354e6c0542b1d010a8f825413e3
Nikos S <niksal1991@gmail.com>
2023-11-05 18:02:40 +0200

    Changed to LogError

--- TechnicalAnalysis.Persistence/Repositories/PostgreSqlRepository.cs
+++ TechnicalAnalysis.Persistence/Repositories/PostgreSqlRepository.cs
@@ -419,7 +419,7 @@ namespace TechnicalAnalysis.Infrastructure.Persistence.Repositories
             }
             catch (Exception exception)
             {
-                _logger.LogInformation("Method:{Method}, Exception{@exception}", nameof(UpdateProviderCandlestickSyncInfoAsync), exception);
+                _logger.LogError("Method:{Method}, Exception{@exception}", nameof(UpdateProviderCandlestickSyncInfoAsync), exception);
                 transaction?.Rollback();
             }
             finally
