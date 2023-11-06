b4fa9f2e7a03440aed9e1676adb11348b103ac6f
Nikos S <niksal1991@gmail.com>
2023-11-06 08:58:12 +0200

    Extracted registration

---
+++ TechnicalAnalysis.Infrastructure.Host/Services/CompressionExtension.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.ResponseCompression;
+using System.IO.Compression;
+
+namespace TechnicalAnalysis.Infrastructure.Host.Services
+{
+    public static class CompressionExtension
+    {
+        public static IServiceCollection ConfigureCompression(this IServiceCollection services)
+        {
+            services.AddResponseCompression(options =>
+            {
+                options.EnableForHttps = true;
+                options.Providers.Add<BrotliCompressionProvider>();
+            });
+
+            services.Configure<BrotliCompressionProviderOptions>(options =>
+            {
+                options.Level = CompressionLevel.Optimal;
+            });
+
+            return services;
+        }
+
+    }
+}
