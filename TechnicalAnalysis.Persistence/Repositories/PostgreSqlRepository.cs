79c268fd552c95256e2043dc25ba2aff50c547bf
Nikos S <niksal1991@gmail.com>
2023-11-07 07:48:51 +0200

    Changed to Async

--- TechnicalAnalysis.Persistence/Repositories/PostgreSqlRepository.cs
+++ TechnicalAnalysis.Persistence/Repositories/PostgreSqlRepository.cs
@@ -175,12 +175,12 @@ namespace TechnicalAnalysis.Infrastructure.Persistence.Repositories
             try
             {
                 using var dbConnection = new NpgsqlConnection(_connectionStringKey);
-                dbConnection.Open();
+                await dbConnection.OpenAsync();
                 const string query = "SELECT \"Id\" AS PrimaryId, \"PoolContract\", \"PoolId\", \"OpenDate\", \"Open\" AS OpenPrice, \"High\" AS HighPrice, \"Low\" AS LowPrice, " +
                                      "\"Close\" AS ClosePrice, \"Timeframe\", \"Fees\", \"Liquidity\", \"TotalValueLocked\", \"Volume\", \"TxCount\" " +
                                      "FROM \"DexCandlesticks\"";
                 var dexCandlesticks = await dbConnection.QueryAsync<DexCandlestick>(query);
-                dbConnection.Close();
+                await dbConnection.CloseAsync();
                 return Result<IEnumerable<DexCandlestick>, string>.Success(dexCandlesticks);
             }
             catch (Exception exception)
