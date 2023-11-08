3e28687f2e8b15655edc645eb857a1acfb925920
Nikos S <niksal1991@gmail.com>
2023-11-08 08:10:23 +0200

    Rename class

---
+++ TechnicalAnalysis.Domain/Extensions/NumberExtension.cs
@@ -0,0 +1,57 @@
+﻿using System.Globalization;
+
+namespace TechnicalAnalysis.Domain.Extensions
+{
+    public static class NumberExtension
+    {
+        public static long? ReduceDigitsToFitLongLength(this string stringWithComma)
+        {
+            if (string.IsNullOrWhiteSpace(stringWithComma))
+            {
+                return null;
+            }
+
+            // Remove any non-digit characters
+            stringWithComma = new string(stringWithComma.Where(char.IsDigit).ToArray());
+
+            // Trim the number string to the first 10 digits if it's too long
+            if (stringWithComma.Length > 10)
+            {
+                stringWithComma = stringWithComma[..10];
+            }
+
+            // Try to parse the number string into a long
+            if (long.TryParse(stringWithComma, out long parsedNumber))
+            {
+                return parsedNumber;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException(nameof(stringWithComma), "The provided string cannot be converted to a long number.");
+            }
+        }
+
+        public static decimal? ReduceDigitsToFitDecimalLength(this string stringWithComma)
+        {
+            if (string.IsNullOrWhiteSpace(stringWithComma))
+            {
+                return null;
+            }
+
+            string stringWithoutComma = stringWithComma.Replace(',', '.');
+            if (stringWithoutComma.Length > 25)
+            {
+                stringWithoutComma = stringWithoutComma[..25];
+            }
+
+            if (decimal.TryParse(stringWithoutComma, NumberStyles.Float, CultureInfo.InvariantCulture, out decimal decimalNumber))
+            {
+                return decimalNumber;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException(nameof(stringWithComma), "The provided string cannot be converted to a decimal number.");
+            }
+        }
+    }
+}
