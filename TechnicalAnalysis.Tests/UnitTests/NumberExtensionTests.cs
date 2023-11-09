09996338cc9329272b99a576260ed36f6d477c45
Nikos S <niksal1991@gmail.com>
2023-11-09 07:41:09 +0200

    Added numberExtensionTests

---
+++ TechnicalAnalysis.Tests/UnitTests/NumberExtensionTests.cs
@@ -0,0 +1,73 @@
+﻿using FluentAssertions;
+using TechnicalAnalysis.Domain.Extensions;
+
+namespace TechnicalAnalysis.Tests.UnitTests
+{
+    [Trait("Category", "UnitTests-NumberExtension")]
+    public class NumberExtensionTests
+    {
+        [Fact]
+        public void ReduceDigitsToFitLongLength_ReturnsSuccess()
+        {
+            const string numberAsString = "12345678901234567890";
+            var longNumberWithTenDigits = numberAsString.ReduceDigitsToFitLongLength();
+
+            longNumberWithTenDigits.Should().NotBeNull();
+            longNumberWithTenDigits.Value.ToString().Length.Should().Be(10);
+        }
+
+        [Fact]
+        public void ReduceDigitsToFitLongLength_WhenStringHasCharacterInTheEnd_ReturnsSuccess()
+        {
+            const string numberAsString = "12345678901234567890nikos";
+            var longNumberWithTenDigits = numberAsString.ReduceDigitsToFitLongLength();
+
+            longNumberWithTenDigits.Should().NotBeNull();
+            longNumberWithTenDigits.Value.ToString().Length.Should().Be(10);
+            longNumberWithTenDigits.Value.ToString().Should().NotContain("nikos");
+        }
+
+        [Fact]
+        public void ReduceDigitsToFitLongLength_WhenStringBeginWithCharacter_ReturnsSuccess()
+        {
+            const string numberAsString = "nikos12345678901234567890";
+            var longNumberWithTenDigits = numberAsString.ReduceDigitsToFitLongLength();
+
+            longNumberWithTenDigits.Should().NotBeNull();
+            longNumberWithTenDigits.Value.ToString().Length.Should().Be(10);
+            longNumberWithTenDigits.Value.ToString().Should().NotContain("nikos");
+        }
+
+        [Fact]
+        public void ReduceDigitsToFitLongLength_WhenStringContainsCharacterInTheMid_ReturnsSuccess()
+        {
+            const string numberAsString = "1234567890nikos1234567890";
+            var longNumberWithTenDigits = numberAsString.ReduceDigitsToFitLongLength();
+
+            longNumberWithTenDigits.Should().NotBeNull();
+            longNumberWithTenDigits.Value.ToString().Length.Should().Be(10);
+            longNumberWithTenDigits.Value.ToString().Should().NotContain("nikos");
+        }
+
+        [Fact]
+        public void ReduceDigitsToFitDecimalLength_ReturnsSuccess()
+        {
+            const string numberAsString = "123456789012345678901234567890,12345678901234567890";
+            var longNumberWithTenDigits = numberAsString.ReduceDigitsToFitDecimalLength();
+
+            longNumberWithTenDigits.Should().NotBeNull();
+            longNumberWithTenDigits.Value.ToString().Length.Should().Be(25);
+        }
+
+        [Fact]
+        public void ReduceDigitsToFitDecimalLength_WhenCommaIsInFirst25Digits_ReturnsWithComma()
+        {
+            const string numberAsString = "1234567890,1234567890123456789012345678901234567890";
+            var longNumberWithTenDigits = numberAsString.ReduceDigitsToFitDecimalLength();
+
+            longNumberWithTenDigits.Should().NotBeNull();
+            longNumberWithTenDigits.Value.ToString().Length.Should().Be(25);
+            longNumberWithTenDigits.Value.ToString().Should().Contain(",");
+        }
+    }
+}
