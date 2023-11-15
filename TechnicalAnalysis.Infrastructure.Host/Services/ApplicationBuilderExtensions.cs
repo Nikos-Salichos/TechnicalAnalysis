2c167156abb802d27829650fb5a1c45c02cbf6c3
Nikos S <niksal1991@gmail.com>
2023-11-15 21:31:03 +0200

    Added ApplicationBuilderExtensions

---
+++ TechnicalAnalysis.Infrastructure.Host/Services/ApplicationBuilderExtensions.cs
@@ -0,0 +1,10 @@
+﻿using TechnicalAnalysis.Infrastructure.Host.Middleware;
+
+namespace TechnicalAnalysis.Infrastructure.Host.Services
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder AddCorrelationIdMiddleware(this IApplicationBuilder applicationBuilder)
+            => applicationBuilder.UseMiddleware<CorrelationIdMiddleware>();
+    }
+}
