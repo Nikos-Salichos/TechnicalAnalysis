8524d922ec10b425c2c3cb322e0d852343d0e47a
Nikos S <niksal1991@gmail.com>
2023-11-17 19:21:07 +0200

    Added Program.cs

---
+++ TechnicalAnalysis.Infrastructure.GatewayApi/Program.cs
@@ -0,0 +1,31 @@
+using TechnicalAnalysis.Infrastructure.GatewayApi;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.AddInfrastructureHostModule();
+
+var proxyBuilder = builder.Services.AddReverseProxy();
+// Initialize the reverse proxy from the "ReverseProxy" section of configuration
+proxyBuilder.LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.MapReverseProxy();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
