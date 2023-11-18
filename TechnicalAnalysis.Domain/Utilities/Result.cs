a404c0bb4dbedfa1e54a764c2a746698c975a66a
Nikos S <niksal1991@gmail.com>
2023-11-18 07:46:12 +0200

    Changed Result.cs

--- TechnicalAnalysis.Domain/Utilities/Result.cs
+++ TechnicalAnalysis.Domain/Utilities/Result.cs
@@ -4,19 +4,10 @@ namespace TechnicalAnalysis.Domain.Utilities
 {
     public sealed class Result<TSuccess, TFail> : IResult<TSuccess, TFail>
     {
-        /// <summary>
-        /// Gets the value representing either a successful result or a failure result.
-        /// </summary>
         public TSuccess SuccessValue { get; }
 
-        /// <summary>
-        /// Gets the value representing the failure result.
-        /// </summary>
         public TFail FailValue { get; }
 
-        /// <summary>
-        /// Gets a value indicating whether this result is an error (failure).
-        /// </summary>
         public bool IsError { get; }
 
         private Result(TSuccess successValue)
@@ -31,17 +22,11 @@ namespace TechnicalAnalysis.Domain.Utilities
             FailValue = failValue;
         }
 
-        /// <summary>
-        /// Creates a new successful result instance.
-        /// </summary>
         public static Result<TSuccess, TFail> Success(TSuccess value)
         {
             return new Result<TSuccess, TFail>(value);
         }
 
-        /// <summary>
-        /// Creates a new failure result instance.
-        /// </summary>
         public static Result<TSuccess, TFail> Fail(TFail failValue)
         {
             return new Result<TSuccess, TFail>(failValue);
