8c8935b45caf74e43a12591ebe188b1086f74492
Nikos S <niksal1991@gmail.com>
2023-11-19 16:47:40 +0200

    Updated to primary ctor

--- TechnicalAnalysis.Application/Mediatr/Commands/DeletePoolsCommand.cs
+++ TechnicalAnalysis.Application/Mediatr/Commands/DeletePoolsCommand.cs
@@ -2,13 +2,8 @@
 
 namespace TechnicalAnalysis.Application.Mediatr.Commands
 {
-    public class DeletePoolsCommand : IRequest
+    public class DeletePoolsCommand(IEnumerable<long> ids) : IRequest
     {
-        public IEnumerable<long> Ids { get; }
-
-        public DeletePoolsCommand(IEnumerable<long> ids)
-        {
-            Ids = ids;
-        }
+        public IEnumerable<long> Ids { get; } = ids;
     }
 }
