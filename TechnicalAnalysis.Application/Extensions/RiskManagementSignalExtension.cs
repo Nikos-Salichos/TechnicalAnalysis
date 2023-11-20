70a1b0fef25411492352d7c54860f53bebb8e77a
Nikos S <niksal1991@gmail.com>
2023-11-20 07:36:54 +0200

    Added fractal trailing stop

--- TechnicalAnalysis.Application/Extensions/RiskManagementSignalExtension.cs
+++ TechnicalAnalysis.Application/Extensions/RiskManagementSignalExtension.cs
@@ -30,7 +30,7 @@ namespace TechnicalAnalysis.Application.Extensions
                     }
 
                     var positionFound = positions.LastOrDefault();
-                    if (positionFound is not null && positionFound.OpenPosition && candlestick1.AverageTrueRanges.FirstOrDefault() is not null)
+                    if (positionFound?.OpenPosition == true && candlestick1.AverageTrueRanges.FirstOrDefault() is not null)
                     {
                         var percentage = candlestick1.AverageTrueRanges.FirstOrDefault()?.AverageTrueRangePercent / 100m;
                         var thresholdValue = positionFound?.EntryPositionPrice * percentage * 2;
@@ -78,7 +78,7 @@ namespace TechnicalAnalysis.Application.Extensions
             return positions;
         }
 
-        public static IEnumerable<Position> AverageDownStrategyCloseAll(this IEnumerable<PairExtended> pairs)
+        public static IEnumerable<Position> AverageDownStrategyCloseAllBasedInFractalBreak(this IEnumerable<PairExtended> pairs)
         {
             List<Position> positions = new List<Position>();
             foreach (PairExtended pair in pairs)
@@ -86,8 +86,11 @@ namespace TechnicalAnalysis.Application.Extensions
                 _ = pair.Candlesticks.OrderBy(c => c.CloseDate).ToList();
                 long positionId = 0;
                 bool openPosition = false;
-                foreach (var candlestick in pair.Candlesticks)
+                decimal? latestFractalPrice = null;
+                for (int i = 0; i < pair.Candlesticks.Count; i++)
                 {
+                    CandlestickExtended? candlestick = pair.Candlesticks[i];
+
                     if (candlestick.EnhancedScans.FirstOrDefault()?.EnhancedScanIsBuy != null && !openPosition)
                     {
                         Position position = new Position();
@@ -103,7 +106,7 @@ namespace TechnicalAnalysis.Application.Extensions
                     }
 
                     var positionFound = positions.LastOrDefault();
-                    if (positionFound is not null && positionFound.OpenPosition)
+                    if (positionFound?.OpenPosition == true)
                     {
                         var percentage = candlestick.AverageTrueRanges.FirstOrDefault()?.AverageTrueRangePercent / 100m;
                         var thresholdValue = positionFound?.EntryPositionPrice * percentage * 2;
@@ -129,6 +132,114 @@ namespace TechnicalAnalysis.Application.Extensions
 
                     if (openPositions.Count > 0)
                     {
+                        var candlestick3 = i - 2 >= 0 ? pair.Candlesticks[i - 2] : null;
+                        if (candlestick3 is not null)
+                        {
+                            var latestFractal = candlestick3.Fractals.FirstOrDefault();
+                            if (latestFractal?.FractalType == CommonModels.Enums.FractalType.BullFractal)
+                            {
+                                latestFractalPrice = latestFractal.Value;
+                            }
+                        }
+
+                        // openPosition = TakeProfitBasedInAtrPercentage(openPosition, candlestick, openPositions);
+                        if (latestFractalPrice is null)
+                        {
+                            continue;
+                        }
+
+                        decimal? allPositionsProfitAndLoss = 0;
+                        decimal? totalEntryPrice = 0;
+                        foreach (var position in openPositions)
+                        {
+                            var positionProfitAndLoss = candlestick.ClosePrice - position.EntryPositionPrice;
+                            allPositionsProfitAndLoss += positionProfitAndLoss;
+                            totalEntryPrice += position.EntryPositionPrice;
+                        }
+
+                        decimal? positionsAverageEntryPrice = totalEntryPrice / openPositions.Count;
+
+                        if (candlestick.ClosePrice > positionsAverageEntryPrice && candlestick.ClosePrice < latestFractalPrice.Value)
+                        {
+                            foreach (var position in openPositions)
+                            {
+                                position.ClosePositionPrice = candlestick.ClosePrice;
+                                position.ClosePositionDate = candlestick.CloseDate;
+                                position.OpenPosition = false;
+                                openPosition = false;
+                            }
+                            latestFractalPrice = null;
+                        }
+                    }
+                }
+            }
+            return positions;
+        }
+
+        public static IEnumerable<Position> AverageDownStrategyCloseAllTakeProfitBasedOnAtr(this IEnumerable<PairExtended> pairs)
+        {
+            List<Position> positions = new List<Position>();
+            foreach (PairExtended pair in pairs)
+            {
+                _ = pair.Candlesticks.OrderBy(c => c.CloseDate).ToList();
+                long positionId = 0;
+                bool openPosition = false;
+                decimal? latestFractalPrice = null;
+                for (int i = 0; i < pair.Candlesticks.Count; i++)
+                {
+                    CandlestickExtended? candlestick = pair.Candlesticks[i];
+
+                    if (candlestick.EnhancedScans.FirstOrDefault()?.EnhancedScanIsBuy != null && !openPosition)
+                    {
+                        Position position = new Position();
+                        position.OpenPositionDate = candlestick.OpenDate;
+                        position.EntryPositionPrice = candlestick.ClosePrice;
+                        position.SignalType = nameof(EnhancedScan);
+                        position.OpenPosition = true;
+                        positions.Add(position);
+
+                        openPosition = true;
+                        positionId = position.Id;
+                        continue;
+                    }
+
+                    var positionFound = positions.LastOrDefault();
+                    if (positionFound?.OpenPosition == true)
+                    {
+                        var percentage = candlestick.AverageTrueRanges.FirstOrDefault()?.AverageTrueRangePercent / 100m;
+                        var thresholdValue = positionFound?.EntryPositionPrice * percentage * 2;
+                        var pricePercentageBelowEntry = positionFound?.EntryPositionPrice - thresholdValue;
+                        if (candlestick.EnhancedScans.FirstOrDefault()?.EnhancedScanIsBuy != null
+                            && openPosition
+                            && candlestick.ClosePrice <= pricePercentageBelowEntry)
+                        {
+                            Position position = new Position();
+                            position.OpenPositionDate = candlestick.OpenDate;
+                            position.EntryPositionPrice = candlestick.ClosePrice;
+                            position.SignalType = nameof(EnhancedScan);
+                            positions.Add(position);
+                            position.OpenPosition = true;
+
+                            openPosition = true;
+                            positionId = position.Id;
+                            continue;
+                        }
+                    }
+
+                    var openPositions = positions.Where(p => p.OpenPosition).ToList();
+
+                    if (openPositions.Count > 0)
+                    {
+                        var candlestick3 = i - 2 >= 0 ? pair.Candlesticks[i - 2] : null;
+                        if (candlestick3 is not null)
+                        {
+                            var latestFractal = candlestick3.Fractals.FirstOrDefault();
+                            if (latestFractal?.FractalType == CommonModels.Enums.FractalType.BullFractal)
+                            {
+                                latestFractalPrice = latestFractal.Value;
+                            }
+                        }
+
                         decimal? allPositionsProfitAndLoss = 0;
                         decimal? totalEntryPrice = 0;
                         foreach (var position in openPositions)
@@ -151,6 +262,7 @@ namespace TechnicalAnalysis.Application.Extensions
                                 position.ClosePositionDate = candlestick.CloseDate;
                                 position.OpenPosition = false;
                                 openPosition = false;
+
                             }
                         }
                     }
