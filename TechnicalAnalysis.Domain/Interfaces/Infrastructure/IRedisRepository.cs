83793fc1470fa0a10b6c133609d86570b375532c
Nikos S <niksal1991@gmail.com>
2023-11-21 16:49:48 +0200

    Added IRedisRepository

---
+++ TechnicalAnalysis.Domain/Interfaces/Infrastructure/IRedisRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace TechnicalAnalysis.Domain.Interfaces.Infrastructure
+{
+    public interface IRedisRepository
+    {
+        Task SetRecordAsync<T>(string recordId, T data, TimeSpan? absoluteExpireTime = null, TimeSpan? slidingExpireTime = null);
+        Task<T?> GetRecordAsync<T>(string recordId);
+
+    }
+}
