262cc444072f9152991d543124d245ba827aa96c
Nikos S <niksal1991@gmail.com>
2023-11-21 08:28:20 +0200

    Updated to .NET 8

--- TechnicalAnalysis.Infrastructure.Adapters/HttpClients/AlpacaHttpClient.cs
+++ TechnicalAnalysis.Infrastructure.Adapters/HttpClients/AlpacaHttpClient.cs
@@ -10,36 +10,27 @@ using TechnicalAnalysis.Domain.Utilities;
 
 namespace TechnicalAnalysis.Infrastructure.Adapters.HttpClients
 {
-    public class AlpacaHttpClient : IAlpacaHttpClient
+    public class AlpacaHttpClient(IOptionsMonitor<AlpacaSetting> alpacaSettings, ILogger<AlpacaHttpClient> logger, IPollyPolicy pollyPolicy) : IAlpacaHttpClient
     {
-        private readonly IOptionsMonitor<AlpacaSetting> _alpacaSettings;
-        private readonly ILogger<AlpacaHttpClient> _logger;
-        private readonly IAsyncPolicy<IMultiPage<IBar>> _retryPolicy;
-
-        public AlpacaHttpClient(IOptionsMonitor<AlpacaSetting> alpacaSettings, ILogger<AlpacaHttpClient> logger, IPollyPolicy pollyPolicy)
-        {
-            _alpacaSettings = alpacaSettings;
-            _logger = logger;
-            _retryPolicy = pollyPolicy.CreatePolicies<IMultiPage<IBar>>(3, TimeSpan.FromMinutes(5));
-        }
+        private readonly IAsyncPolicy<IMultiPage<IBar>> _retryPolicy = pollyPolicy.CreatePolicies<IMultiPage<IBar>>(3, TimeSpan.FromMinutes(5));
 
         public async Task<IResult<IMultiPage<IBar>, string>> GetAlpacaData(string pairName, DateTime fromDateTime, DateTime toDateTime, BarTimeFrame barTimeFrame)
         {
             try
             {
-                _logger.LogInformation("Method {Method}, pairName {pairName}, toDateTime {toDateTime}, barTimeFrame {barTimeFrame} ", nameof(GetAlpacaData), pairName, toDateTime, barTimeFrame);
-                var alpacaDataClient = Environments.Paper.GetAlpacaDataClient(new SecretKey(_alpacaSettings.CurrentValue.ApiKey, _alpacaSettings.CurrentValue.ApiSecret));
+                logger.LogInformation("Method {Method}, pairName {pairName}, toDateTime {toDateTime}, barTimeFrame {barTimeFrame} ", nameof(GetAlpacaData), pairName, toDateTime, barTimeFrame);
+                var alpacaDataClient = Environments.Paper.GetAlpacaDataClient(new SecretKey(alpacaSettings.CurrentValue.ApiKey, alpacaSettings.CurrentValue.ApiSecret));
                 HistoricalBarsRequest historicalBarsRequest = new HistoricalBarsRequest(pairName, fromDateTime, toDateTime, barTimeFrame)
                 {
                     Adjustment = Adjustment.SplitsAndDividends
                 };
                 var stockData = await _retryPolicy.ExecuteAsync(() => alpacaDataClient.GetHistoricalBarsAsync(historicalBarsRequest));
-                _logger.LogInformation("Method: {Method}, deserializedData '{@stockData}' ", nameof(GetAlpacaData), stockData);
+                logger.LogInformation("Method: {Method}, deserializedData '{@stockData}' ", nameof(GetAlpacaData), stockData);
                 return Result<IMultiPage<IBar>, string>.Success(stockData);
             }
             catch (Exception exception)
             {
-                _logger.LogWarning("Method {Method}, exception {exception}", nameof(GetAlpacaData), exception);
+                logger.LogWarning("Method {Method}, exception {exception}", nameof(GetAlpacaData), exception);
                 return Result<IMultiPage<IBar>, string>.Fail(exception.ToString());
             }
         }
