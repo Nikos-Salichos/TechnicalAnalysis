01e9d087ee78168232ff397dbbc2123e39ed1d66
Nikos S <niksal1991@gmail.com>
2023-11-21 16:51:14 +0200

    Updated to use RedisRepository

--- TechnicalAnalysis.Application/Services/CachedAnalysisService.cs
+++ TechnicalAnalysis.Application/Services/CachedAnalysisService.cs
@@ -1,15 +1,13 @@
 ﻿using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Caching.Distributed;
 using TechnicalAnalysis.CommonModels.BusinessModels;
 using TechnicalAnalysis.CommonModels.Enums;
 using TechnicalAnalysis.Domain.Interfaces.Application;
 using TechnicalAnalysis.Domain.Interfaces.Infrastructure;
-using TechnicalAnalysis.Infrastructure.Persistence.DistributedCache;
 using DataProvider = TechnicalAnalysis.CommonModels.Enums.DataProvider;
 
 namespace TechnicalAnalysis.Application.Services
 {
-    public class CachedAnalysisService(IAnalysisService inner, IDistributedCache distributedCache,
+    public class CachedAnalysisService(IAnalysisService inner, IRedisRepository redisRepository,
         ICommunication communication, IRabbitMqService rabbitMqService) : IAnalysisService
     {
         public Task<IEnumerable<PairExtended>> GetIndicatorsByPairNamesAsync(string pairName, Timeframe timeframe)
@@ -21,7 +19,7 @@ namespace TechnicalAnalysis.Application.Services
         {
             if (httpContext?.Request.Headers.ContainsKey("C-Invalid") == false)
             {
-                var cachedPairs = await distributedCache.GetRecordAsync<IEnumerable<PairExtended>>(provider.ToString());
+                var cachedPairs = await redisRepository.GetRecordAsync<IEnumerable<PairExtended>>(provider.ToString());
                 if (cachedPairs is not null)
                 {
                     await communication.CreateAttachmentSendMessage(cachedPairs);
@@ -34,7 +32,7 @@ namespace TechnicalAnalysis.Application.Services
 
             var pairs = await inner.GetPairsIndicatorsAsync(provider, httpContext);
 
-            await distributedCache.SetRecordAsync(provider.ToString(), pairs, null, null);
+            await redisRepository.SetRecordAsync(provider.ToString(), pairs, null, null);
             await communication.CreateAttachmentSendMessage(pairs);
             rabbitMqService.PublishMessage(pairs);
 
