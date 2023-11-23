175a834193e20b4eb04f4fa4342777982705370f
Nikos S <niksal1991@gmail.com>
2023-11-23 06:22:04 +0200

    Changed common models test

---
+++ TechnicalAnalysis.Tests/ArchitectureTests/CommonModelsTest.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using NetArchTest.Rules;
+
+namespace TechnicalAnalysis.Tests.ArchitectureTests
+{
+    [Trait("Category", "Architecture")]
+    public class ContractTest
+    {
+        [Fact]
+        public void CommonModels_ShouldNotHaveAnyDependency()
+        {
+            var noDependenciesResult = Types.InNamespace(BaseArchitectureSetup.CommonModelsProject)
+                .Should()
+                .NotHaveDependencyOnAny(BaseArchitectureSetup.InfrastructurePersistenceProject,
+                    BaseArchitectureSetup.InfrastructureAdaptersProject,
+                    BaseArchitectureSetup.InfrastructureHostProject,
+                    BaseArchitectureSetup.InfrastructureClientProject,
+                    BaseArchitectureSetup.PresentationProject,
+                    BaseArchitectureSetup.DomainProject)
+                .GetResult();
+
+            noDependenciesResult.IsSuccessful.Should().BeTrue();
+        }
+    }
+}
