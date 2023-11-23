84d66867c986cdcea251a66135a35bdfc5e4e913
Nikos S <niksal1991@gmail.com>
2023-11-23 06:19:57 +0200

    Fixed domain architecture test

--- TechnicalAnalysis.Tests/ArchitectureTests/DomainTest.cs
+++ TechnicalAnalysis.Tests/ArchitectureTests/DomainTest.cs
@@ -7,17 +7,26 @@ namespace TechnicalAnalysis.Tests.ArchitectureTests
     public class DomainTest
     {
         [Fact]
-        public void Domain_ShouldNotHaveAnyDependency()
+        public void Domain_ShouldNotHaveAnyDependency_OtherThanCommonProject()
         {
-            var result = Types.InCurrentDomain()
-                               .That().ResideInNamespace(BaseArchitectureSetup.ContractsProject)
-                               .And().AreClasses()
-                               .Should().HaveDependenciesOtherThan(BaseArchitectureSetup.InfrastructurePersistenceProject,
-                               BaseArchitectureSetup.InfrastructureAdaptersProject, BaseArchitectureSetup.InfrastructureHostProject,
-                               BaseArchitectureSetup.InfrastructureClientProject, BaseArchitectureSetup.PresentationProject,
-                               BaseArchitectureSetup.ContractsProject)
-                               .GetResult();
-            result.IsSuccessful.Should().BeTrue();
+            var noDependenciesResult = Types.InNamespace(BaseArchitectureSetup.DomainProject)
+                .That()
+                .AreClasses()
+                .Should()
+                .NotHaveDependencyOnAny(BaseArchitectureSetup.InfrastructurePersistenceProject,
+                    BaseArchitectureSetup.InfrastructureAdaptersProject,
+                    BaseArchitectureSetup.InfrastructureHostProject,
+                    BaseArchitectureSetup.InfrastructureClientProject,
+                    BaseArchitectureSetup.PresentationProject)
+                .GetResult();
+
+            var dependency = Types.InNamespace(BaseArchitectureSetup.DomainProjectEntities)
+                              .Should()
+                              .HaveDependencyOn(BaseArchitectureSetup.CommonModelsProject)
+                              .GetResult();
+
+            noDependenciesResult.IsSuccessful.Should().BeTrue();
+            dependency.IsSuccessful.Should().BeTrue();
         }
     }
 }
