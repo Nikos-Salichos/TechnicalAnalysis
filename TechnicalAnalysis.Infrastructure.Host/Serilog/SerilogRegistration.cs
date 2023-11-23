381abfdbbf7265fc05330c68956f1ae24c9c23f0
Nikos S <niksal1991@gmail.com>
2023-11-23 06:19:17 +0200

    Removed Serilog extra configuration

--- TechnicalAnalysis.Infrastructure.Host/Serilog/SerilogRegistration.cs
+++ TechnicalAnalysis.Infrastructure.Host/Serilog/SerilogRegistration.cs
@@ -9,12 +9,5 @@ namespace TechnicalAnalysis.Infrastructure.Host.Serilog
             builder.Host.UseSerilog((_, loggerConfiguration) => loggerConfiguration
                                .ReadFrom.Configuration(builder.Configuration));
         }
-
-        public static void SerilogConfiguration(HostBuilderContext hostContext, LoggerConfiguration loggerConfiguration)
-        {
-            loggerConfiguration
-                 .ReadFrom.Configuration(hostContext.Configuration)
-                 .Enrich.FromLogContext();
-        }
     }
 }
