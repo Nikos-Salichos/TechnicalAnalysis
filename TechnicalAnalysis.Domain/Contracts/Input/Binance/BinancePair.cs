66b2139ebcf3b6b267a4ed4980bf6a7ee50755e8
Nikos S <niksal1991@gmail.com>
2023-11-29 18:35:58 +0200

    Removed BinancePairEqualityComparer

--- TechnicalAnalysis.Domain/Contracts/Input/Binance/BinancePair.cs
+++ TechnicalAnalysis.Domain/Contracts/Input/Binance/BinancePair.cs
@@ -14,26 +14,4 @@ namespace TechnicalAnalysis.Domain.Contracts.Input.Binance
         public DateTime CreatedAt { get; init; } = DateTime.Now;
         public List<BinanceCandlestick> BinanceCandlesticks { get; set; } = new List<BinanceCandlestick>();
     }
-
-    public class BinancePairEqualityComparer : IEqualityComparer<BinancePair>
-    {
-        public bool Equals(BinancePair? x, BinancePair? y)
-        {
-            if (x == null || y == null)
-            {
-                return false;
-            }
-
-            return x.BaseAssetId == y.BaseAssetId &&
-                   x.QuoteAssetId == y.QuoteAssetId &&
-                   x.Provider == y.Provider;
-        }
-
-        public int GetHashCode(BinancePair obj)
-        {
-            return obj.BaseAssetId.GetHashCode() ^
-                   obj.QuoteAssetId.GetHashCode() ^
-                   obj.Provider.GetHashCode();
-        }
-    }
 }
