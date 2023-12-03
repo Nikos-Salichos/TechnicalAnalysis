1c18d345b2e978dd32c138bbb59cb0ba46c684fd
Nikos S <niksal1991@gmail.com>
2023-12-03 10:31:40 +0200

    Changed request to record

--- TechnicalAnalysis.CommonModels/ApiRequests/DataProviderTimeframeRequest.cs
+++ TechnicalAnalysis.CommonModels/ApiRequests/DataProviderTimeframeRequest.cs
@@ -2,10 +2,5 @@
 
 namespace TechnicalAnalysis.CommonModels.ApiRequests
 {
-    public readonly struct DataProviderTimeframeRequest(DataProvider dataProvider, Timeframe timeframe)
-    {
-        public DataProvider DataProvider { get; } = dataProvider;
-        public Timeframe Timeframe { get; } = timeframe;
-    }
-
+    public record DataProviderTimeframeRequest(DataProvider DataProvider, Timeframe Timeframe);
 }
