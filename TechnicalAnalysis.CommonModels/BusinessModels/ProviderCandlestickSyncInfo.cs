263baaf7aabfec534dcd5a98cd5bb553d51d2874
Nikos S <niksal1991@gmail.com>
2023-12-08 06:11:52 +0200

    Changed model

--- TechnicalAnalysis.CommonModels/BusinessModels/ProviderCandlestickSyncInfo.cs
+++ TechnicalAnalysis.CommonModels/BusinessModels/ProviderCandlestickSyncInfo.cs
@@ -5,18 +5,16 @@ namespace TechnicalAnalysis.CommonModels.BusinessModels
 {
     public class ProviderCandlestickSyncInfo : BaseEntity
     {
-        public DataProvider DataProvider { get; set; }
+        public DataProvider DataProvider { get; init; }
         public Timeframe Timeframe { get; set; }
         public DateTime LastCandlestickSync { get; set; }
 
-        public ProviderCandlestickSyncInfo(DataProvider dataProvider)
-        {
-            DataProvider = dataProvider;
-        }
-
-        public ProviderCandlestickSyncInfo()
-        {
-
-        }
+        public static ProviderCandlestickSyncInfo Create(DataProvider dataProvider, Timeframe timeframe, DateTime lastCandlestickSync)
+            => new()
+            {
+                DataProvider = dataProvider,
+                Timeframe = timeframe,
+                LastCandlestickSync = lastCandlestickSync
+            };
     }
 }
