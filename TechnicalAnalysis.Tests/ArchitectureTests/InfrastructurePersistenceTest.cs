ff9716a6711fac334342225c584f94d7d1b26990
Nikos S <niksal1991@gmail.com>
2023-12-13 19:13:44 +0200

    Added architecture test

--- TechnicalAnalysis.Tests/ArchitectureTests/InfrastructurePersistenceTest.cs
+++ TechnicalAnalysis.Tests/ArchitectureTests/InfrastructurePersistenceTest.cs
@@ -1,54 +1,27 @@
 ﻿using FluentAssertions;
 using NetArchTest.Rules;
-using TechnicalAnalysis.Domain.Interfaces.Infrastructure;
 
 namespace TechnicalAnalysis.Tests.ArchitectureTests
 {
-    [Trait("Category", "Architecture")]
     public class InfrastructurePersistenceTest
     {
         [Fact]
-        public void Infrastructure_ShouldHave_DependencyOnDomain()
-        {
-            var result = Types.InCurrentDomain()
-                       .That().ResideInNamespace(BaseArchitectureSetup.InfrastructurePersistenceProject)
-                       .And().AreClasses()
-                       .Should().HaveDependencyOn(BaseArchitectureSetup.DomainProject)
-                       .GetResult();
-            result.IsSuccessful.Should().BeTrue();
-        }
-
-        [Fact]
         public void Infrastructure_ShouldNotHave_DependencyOnAnyProjectExceptDomain()
         {
-            var result = Types.InCurrentDomain()
-                               .That().ResideInNamespace(BaseArchitectureSetup.InfrastructurePersistenceProject)
-                               .And().AreClasses()
-                               .Should().NotHaveDependencyOnAny(BaseArchitectureSetup.ApplicationProject, BaseArchitectureSetup.PresentationProject, BaseArchitectureSetup.CommonModelsProject)
+            var result = Types.InNamespace(typeof(Infrastructure.Persistence.Repositories.RedisRepository).Namespace)
+                               .Should().NotHaveDependencyOnAny(BaseArchitectureSetup.ApplicationProject, BaseArchitectureSetup.PresentationProject)
                                .GetResult();
 
             result.IsSuccessful.Should().BeTrue();
         }
 
         [Fact]
-        public void Repositories_ShouldImplementIRepositoryInterface()
-        {
-            var result = Types.InCurrentDomain()
-                         .That().ResideInNamespace(BaseArchitectureSetup.InfrastructurePersistenceProject)
-                         .And().AreClasses()
-                         .Should().ImplementInterface(typeof(IPostgreSqlRepository))
-                         .GetResult();
-            result.IsSuccessful.Should().BeTrue();
-        }
-
-        [Fact]
         public void Repositories_ShouldHaveNameEndingWithRepository()
         {
-            var result = Types.InCurrentDomain()
-                                   .That().ResideInNamespace(BaseArchitectureSetup.InfrastructurePersistenceProject)
-                                   .And().AreClasses()
+            var result = Types.InNamespace(typeof(Infrastructure.Persistence.Repositories.RedisRepository).Namespace)
                                    .Should().HaveNameEndingWith("Repository")
                                    .GetResult();
+
             result.IsSuccessful.Should().BeTrue();
         }
     }
