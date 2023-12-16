a80926e1df69a842ffb93ef49d1c0b5bee5fad70
Nikos S <niksal1991@gmail.com>
2023-12-16 11:04:49 +0200

    Removed auto set prop

--- TechnicalAnalysis.Domain/Contracts/Input/Binance/BinancePair.cs
+++ TechnicalAnalysis.Domain/Contracts/Input/Binance/BinancePair.cs
@@ -11,7 +11,7 @@ namespace TechnicalAnalysis.Domain.Contracts.Input.Binance
         public DataProvider Provider { get; init; }
         public bool IsActive { get; init; }
         public bool AllCandles { get; init; }
-        public DateTime CreatedAt { get; init; } = DateTime.Now;
+        public DateTime CreatedAt { get; } = DateTime.UtcNow;
         public List<BinanceCandlestick> BinanceCandlesticks { get; set; } = new List<BinanceCandlestick>();
     }
 }
