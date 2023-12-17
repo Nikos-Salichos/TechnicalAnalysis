411f7381191259f187deeeff01035ed86bbd620b
Nikos S <niksal1991@gmail.com>
2023-12-17 11:06:17 +0200

    Changed model

--- TechnicalAnalysis.CommonModels/BusinessModels/ProviderSynchronization.cs
+++ TechnicalAnalysis.CommonModels/BusinessModels/ProviderSynchronization.cs
@@ -2,16 +2,10 @@
 
 namespace TechnicalAnalysis.CommonModels.BusinessModels
 {
-    public class ProviderSynchronization
+    public sealed class ProviderSynchronization
     {
-        public DataProvider DataProvider { get; set; }
+        public DataProvider DataProvider { get; init; }
         public ProviderPairAssetSyncInfo ProviderPairAssetSyncInfo { get; set; } = new ProviderPairAssetSyncInfo();
         public List<ProviderCandlestickSyncInfo> CandlestickSyncInfos { get; set; } = [];
-
-        public static ProviderSynchronization Create(DataProvider dataProvider)
-            => new()
-            {
-                DataProvider = dataProvider
-            };
     }
 }
