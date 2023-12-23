7ef71cd995a2df44a3a0d51bccd26c42d3d56c80
Nikos S <niksal1991@gmail.com>
2023-12-23 08:21:20 +0200

    Updated model

--- TechnicalAnalysis.CommonModels/BusinessModels/ProviderSynchronization.cs
+++ TechnicalAnalysis.CommonModels/BusinessModels/ProviderSynchronization.cs
@@ -1,10 +1,7 @@
-﻿using TechnicalAnalysis.CommonModels.Enums;
-
-namespace TechnicalAnalysis.CommonModels.BusinessModels
+﻿namespace TechnicalAnalysis.CommonModels.BusinessModels
 {
     public sealed class ProviderSynchronization
     {
-        public DataProvider DataProvider { get; init; }
         public ProviderPairAssetSyncInfo ProviderPairAssetSyncInfo { get; set; } = new ProviderPairAssetSyncInfo();
         public List<ProviderCandlestickSyncInfo> CandlestickSyncInfos { get; set; } = [];
     }
