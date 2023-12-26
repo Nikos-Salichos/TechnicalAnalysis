b4408a5174f4345e7faf3c32e014aa24a7464628
Nikos S <niksal1991@gmail.com>
2023-12-26 10:01:54 +0200

    Improved polly

--- TechnicalAnalysis.Domain/Interfaces/IPollyPolicy.cs
+++
@@ -1,9 +0,0 @@
-﻿using Polly;
-
-namespace TechnicalAnalysis.Domain.Interfaces
-{
-    public interface IPollyPolicy
-    {
-        IAsyncPolicy<T> CreatePolicies<T>(int retries, TimeSpan timeSpan);
-    }
-}
