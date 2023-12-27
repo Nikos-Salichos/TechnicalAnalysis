8d933fd2ed55e9a752fa05669f3606ce14804181
Nikos S <niksal1991@gmail.com>
2023-12-27 09:33:41 +0200

    Added model

---
+++ TechnicalAnalysis.CommonModels/BusinessModels/RecordStatistic.cs
@@ -0,0 +1,9 @@
+﻿namespace TechnicalAnalysis.CommonModels.BusinessModels
+{
+    public class DailyStatistic
+    {
+        public int NumberOfPairs { get; set; }
+        public HashSet<string> PairsWithEnhancedScan { get; set; } = [];
+        public double Percentage { get; set; }
+    }
+}
