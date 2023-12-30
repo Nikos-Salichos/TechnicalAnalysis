652b593ca511f33a9c1944d429a24b72eabe9804
Nikos S <niksal1991@gmail.com>
2023-12-30 09:02:54 +0200

    Updated CachedService

--- TechnicalAnalysis.Application/Services/CachedAnalysisService.cs
+++ TechnicalAnalysis.Application/Services/CachedAnalysisService.cs
@@ -15,7 +15,7 @@ namespace TechnicalAnalysis.Application.Services
             var cachedPair = await redisRepository.GetRecordAsync<PairExtended>(pairName);
             if (cachedPair != null)
             {
-                return new List<PairExtended> { cachedPair };
+                //  return new List<PairExtended> { cachedPair };
             }
 
             var pairs = await inner.GetIndicatorsByPairNamesAsync(pairName, timeframe);
@@ -46,11 +46,39 @@ namespace TechnicalAnalysis.Application.Services
 
             var pairs = await inner.GetPairsIndicatorsAsync(provider, httpContext);
 
+            var filteredPairs = pairs
+               .OrderByDescending(pair => pair.CreatedAt)
+               .Select(pair =>
+               {
+                   var enhancedScans = pair.Candlesticks
+                       .Where(c => c.EnhancedScans.Count > 0)
+                       .OrderByDescending(c => c.CloseDate)
+                       .GroupBy(c => c.PoolOrPairId)
+                       .Select(group => new
+                       {
+                           CandlestickCloseDate = group.First().CloseDate,
+                           group.First().EnhancedScans
+                       })
+                       .ToList();
+
+                   return new
+                   {
+                       pair.Symbol,
+                       EnhancedScans = enhancedScans
+                   };
+               })
+               .Where(result => result.EnhancedScans.Count > 0)
+               .ToList();
+
+            var sortedPairs = filteredPairs.OrderByDescending(result => result.EnhancedScans.FirstOrDefault()?.CandlestickCloseDate).ToList();
+
             await redisRepository.SetRecordAsync(provider.ToString(), pairs, null, null);
-            await communication.CreateAttachmentSendMessage(pairs);
+            await communication.CreateAttachmentSendMessage(sortedPairs);
             rabbitMqService.PublishMessage(pairs);
 
             return pairs;
         }
+
+
     }
 }
