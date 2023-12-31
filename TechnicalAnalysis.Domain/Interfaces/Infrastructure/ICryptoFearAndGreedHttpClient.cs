84b3ae4dac65f0a8f776491d4b371c4dc5542954
Nikos S <niksal1991@gmail.com>
2023-12-31 14:58:59 +0200

    Added interface

---
+++ TechnicalAnalysis.Domain/Interfaces/Infrastructure/ICryptoFearAndGreedHttpClient.cs
@@ -0,0 +1,10 @@
+﻿using TechnicalAnalysis.Domain.Contracts.Input.CryptoAndFearIndex;
+using TechnicalAnalysis.Domain.Interfaces.Utilities;
+
+namespace TechnicalAnalysis.Domain.Interfaces.Infrastructure
+{
+    public interface ICryptoFearAndGreedHttpClient
+    {
+        Task<IResult<IEnumerable<CryptoFearAndGreedData>, string>> GetCryptoFearAndGreedIndex(int numberOfDates);
+    }
+}
