8f69e7b80884200e98a4fa3a99184d07f0bd4906
Nikos S <niksal1991@gmail.com>
2023-12-31 14:59:56 +0200

    Added Query

---
+++ TechnicalAnalysis.Application/Mediatr/Queries/GetCryptoFearAndGreedIndexQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using TechnicalAnalysis.Domain.Contracts.Input.CryptoAndFearIndex;
+
+namespace TechnicalAnalysis.Application.Mediatr.Queries
+{
+    public class GetCryptoFearAndGreedIndexQuery : IRequest<IEnumerable<CryptoFearAndGreedData>>
+    {
+    }
+}
