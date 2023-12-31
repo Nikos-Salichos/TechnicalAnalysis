30f301b276df0800fe7274a736ee4b7a286ab111
Nikos S <niksal1991@gmail.com>
2023-12-31 14:59:42 +0200

    Added handler

---
+++ TechnicalAnalysis.Application/Mediatr/Handlers/GetCryptoFearAndGreedIndexHandler.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using TechnicalAnalysis.Application.Mediatr.Queries;
+using TechnicalAnalysis.Domain.Contracts.Input.CryptoAndFearIndex;
+using TechnicalAnalysis.Domain.Interfaces.Infrastructure;
+
+namespace TechnicalAnalysis.Application.Mediatr.Handlers
+{
+    public class GetCryptoFearAndGreedIndexHandler(IPostgreSqlRepository repository) : IRequestHandler<GetCryptoFearAndGreedIndexQuery, IEnumerable<CryptoFearAndGreedData>>
+    {
+        public async Task<IEnumerable<CryptoFearAndGreedData>> Handle(GetCryptoFearAndGreedIndexQuery getCryptoFearAndGreedIndexQuery, CancellationToken cancellationToken)
+        {
+            var result = await repository.GetCryptoFearAndGreedIndexAsync();
+            if (result.IsError)
+            {
+                return Enumerable.Empty<CryptoFearAndGreedData>();
+            }
+            return result.SuccessValue;
+        }
+    }
+}
