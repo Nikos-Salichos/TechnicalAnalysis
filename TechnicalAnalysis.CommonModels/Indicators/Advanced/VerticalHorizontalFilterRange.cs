03a698c08e92e76b5084f0ac8fbfa5d758dff05e
Nikos S <niksal1991@gmail.com>
2024-01-01 19:11:35 +0200

    Added vhf

---
+++ TechnicalAnalysis.CommonModels/Indicators/Advanced/VerticalHorizontalFilterRange.cs
@@ -0,0 +1,18 @@
+﻿using TechnicalAnalysis.CommonModels.BaseClasses;
+
+namespace TechnicalAnalysis.CommonModels.Indicators.Advanced
+{
+    public sealed class VerticalHorizontalFilterRange : BaseIndicator
+    {
+        public double RangeLimit { get; init; }
+        public int NumberOfCandlesticksInRange { get; init; }
+
+        public VerticalHorizontalFilterRange(long candlestickId, double rangeLimit, int numberOfCandlesticksInRange)
+            : base(candlestickId)
+        {
+            RangeLimit = rangeLimit;
+            NumberOfCandlesticksInRange = numberOfCandlesticksInRange;
+        }
+    }
+}
+
