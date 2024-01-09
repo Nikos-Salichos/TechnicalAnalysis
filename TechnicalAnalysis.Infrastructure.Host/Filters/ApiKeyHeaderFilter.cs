10a296c453977f39870329c9c1f969bbc8938dc5
Nikos S <niksal1991@gmail.com>
2024-01-09 09:09:48 +0200

    Added ApiKeyMiddleware

---
+++ TechnicalAnalysis.Infrastructure.Host/Filters/ApiKeyHeaderFilter.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace TechnicalAnalysis.Infrastructure.Host.Filters
+{
+    public class ApiKeyHeaderFilter : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            operation.Parameters ??= new List<OpenApiParameter>();
+
+            var apiKeyParameter = new OpenApiParameter
+            {
+                Name = "ApiKey",
+                In = ParameterLocation.Header,
+                Required = true,
+                Example = new OpenApiString("123") // Example value
+            };
+
+            operation.Parameters.Add(apiKeyParameter);
+        }
+
+    }
+
+}
