4d5d6e2ed6d2cf59cb5287b6ff1b52aeb50e9498
Nikos S <niksal1991@gmail.com>
2024-01-18 21:42:56 +0200

    Added Hangfire

--- TechnicalAnalysis.Infrastructure.Host/Hangfire/HangfireStartupJob.cs
+++ TechnicalAnalysis.Infrastructure.Host/Hangfire/HangfireStartupJob.cs
@@ -4,34 +4,57 @@ using TechnicalAnalysis.Domain.Interfaces.Application;
 
 namespace TechnicalAnalysis.Infrastructure.Host.Hangfire
 {
-    public static class HangfireStartupJob
+    internal static class HangfireStartupJob
     {
-        public static void EnqueueSynchronizeProvidersJob(WebApplication app)
+        public static async Task EnqueueSynchronizeProvidersJob(WebApplication app)
         {
             var serviceProvider = app.Services;
             var syncService = serviceProvider.GetService<ISyncService>();
 
             if (syncService != null)
             {
+                List<Task> tasks =
+                [
+                    syncService.SynchronizeProvidersAsync(new DataProviderTimeframeRequest(
+                            CommonModels.Enums.DataProvider.Alpaca,
+                            CommonModels.Enums.Timeframe.Daily)),
+
+                    syncService.SynchronizeProvidersAsync(new DataProviderTimeframeRequest(
+                        CommonModels.Enums.DataProvider.Binance,
+                        CommonModels.Enums.Timeframe.Daily)),
+
+                    syncService.SynchronizeProvidersAsync(new DataProviderTimeframeRequest(
+                        CommonModels.Enums.DataProvider.AlternativeMeCryptoAndFearIndex,
+                        CommonModels.Enums.Timeframe.Daily))
+                ];
+
+                await Task.WhenAll(tasks);
+
                 RecurringJob.AddOrUpdate(
                     "synchronize_providers_job_binance", // Unique identifier for the job
                     () => syncService.SynchronizeProvidersAsync(new DataProviderTimeframeRequest(
                         CommonModels.Enums.DataProvider.Binance,
                         CommonModels.Enums.Timeframe.Daily)),
-                   "*/30 * * * *"); // Run at the current minute and then every 30 minutes
+                   "*/30 * * * *");
 
                 RecurringJob.AddOrUpdate(
-                    "synchronize_providers_job_alpaca", // Unique identifier for the job
-                    () => syncService.SynchronizeProvidersAsync(new DataProviderTimeframeRequest(
-                        CommonModels.Enums.DataProvider.Alpaca,
-                        CommonModels.Enums.Timeframe.Daily)),
-                   "*/30 * * * *"); // Run at the current minute and then every 30 minutes
+                     "synchronize_providers_job_alpaca", // Unique identifier for the job
+                     () => syncService.SynchronizeProvidersAsync(new DataProviderTimeframeRequest(
+                         CommonModels.Enums.DataProvider.Alpaca,
+                         CommonModels.Enums.Timeframe.Daily)),
+                    "*/30 * * * *");
 
                 RecurringJob.AddOrUpdate("synchronize_providers_job_alternative_me_crypto_and_fear_index",
                     () => syncService.SynchronizeProvidersAsync(new DataProviderTimeframeRequest(
                         CommonModels.Enums.DataProvider.AlternativeMeCryptoAndFearIndex,
                         CommonModels.Enums.Timeframe.Daily)),
-                     "*/30 * * * *"); // Run at the current minute and then every 30 minutes
+                     "*/30 * * * *");
+            }
+
+            var analysisService = serviceProvider.GetService<IAnalysisService>();
+            if (analysisService != null)
+            {
+                await analysisService.GetPairsIndicatorsAsync(CommonModels.Enums.DataProvider.All);
             }
         }
     }
