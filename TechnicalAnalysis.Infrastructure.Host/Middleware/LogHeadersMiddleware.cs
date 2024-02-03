503b47ae4d910e136a3323250c414533362b5306
Nikos S <niksal1991@gmail.com>
2024-02-03 07:36:01 +0200

    Added LogHeadersMiddleware

---
+++ TechnicalAnalysis.Infrastructure.Host/Middleware/LogHeadersMiddleware.cs
@@ -0,0 +1,15 @@
+﻿namespace TechnicalAnalysis.Infrastructure.Host.Middleware
+{
+    public class LogHeadersMiddleware(RequestDelegate requestDelegate, ILogger<ExceptionHandlingMiddleware> logger)
+    {
+        public async Task InvokeAsync(HttpContext context)
+        {
+            foreach (var header in context.Request.Headers)
+            {
+                logger.LogInformation("Header: {Key}: {Value}", header.Key, header.Value);
+            }
+
+            await requestDelegate(context);
+        }
+    }
+}
