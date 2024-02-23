464571fc015d85a242bd74e11fb2d7569dcdcecb
Nikos S <niksal1991@gmail.com>
2024-02-23 08:47:04 +0200

    Changed model

--- TechnicalAnalysis.CommonModels/BusinessModels/Asset.cs
+++ TechnicalAnalysis.CommonModels/BusinessModels/Asset.cs
@@ -1,4 +1,5 @@
 ﻿using TechnicalAnalysis.CommonModels.BaseClasses;
+using TechnicalAnalysis.CommonModels.Enums;
 
 namespace TechnicalAnalysis.CommonModels.BusinessModels
 {
@@ -6,5 +7,20 @@ namespace TechnicalAnalysis.CommonModels.BusinessModels
     {
         public string Symbol { get; init; } = string.Empty;
         public DateTime CreatedDate { get; } = DateTime.UtcNow;
+        public AssetType AssetType { get; init; }
+
+        public bool Equals(Asset? other)
+        {
+            if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
+            return other != null && Symbol == other.Symbol;
+        }
+
+        public override bool Equals(object? obj) => obj is not null && Equals(obj as Asset);
+
+        public override int GetHashCode() => HashCode.Combine(Symbol);
     }
 }
