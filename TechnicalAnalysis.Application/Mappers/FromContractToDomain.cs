d477cebf4592539c1578de65f6132cf7d684c90e
Nikos S <niksal1991@gmail.com>
2024-02-23 08:46:47 +0200

    Changed contract

---
+++ TechnicalAnalysis.Application/Mappers/FromContractToDomain.cs
@@ -0,0 +1,33 @@
+﻿using TechnicalAnalysis.CommonModels.BusinessModels;
+using TechnicalAnalysis.CommonModels.Enums;
+using TechnicalAnalysis.Domain.Contracts.Output.CoinPaprika;
+
+namespace TechnicalAnalysis.Application.Mappers
+{
+    public static class FromContractToDomain
+    {
+        private static Asset ToDomain(this CoinPaprikaAsset asset)
+        {
+            AssetType assetType = AssetType.Unknown;
+
+            if (asset.Type is "coin")
+            {
+                assetType = AssetType.Layer1;
+            }
+
+            if (asset.Type == "token")
+            {
+                assetType = AssetType.Layer2;
+            }
+
+            return new()
+            {
+                Symbol = asset.Symbol,
+                AssetType = assetType
+            };
+        }
+
+        public static IEnumerable<Asset> ToDomain(this IEnumerable<CoinPaprikaAsset> assets)
+            => assets.Select(p => p.ToDomain()).ToList();
+    }
+}
