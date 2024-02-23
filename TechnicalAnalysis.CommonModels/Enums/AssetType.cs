7816c1adbce18f63ed66202ef75a0f45d89cb1e1
Nikos S <niksal1991@gmail.com>
2024-02-23 08:47:14 +0200

    Added type

---
+++ TechnicalAnalysis.CommonModels/Enums/AssetType.cs
@@ -0,0 +1,11 @@
+﻿namespace TechnicalAnalysis.CommonModels.Enums
+{
+    public enum AssetType
+    {
+        Unknown = 0,
+        Layer1 = 1,
+        Layer2 = 2,
+        ETF = 3,
+        Stock = 4
+    }
+}
