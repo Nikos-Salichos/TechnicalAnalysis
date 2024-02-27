cecd82f1f7ad5ffb320e03e9d717316bbc3b822b
Nikos S <niksal1991@gmail.com>
2024-02-27 18:03:49 +0200

    Added model

---
+++ TechnicalAnalysis.Domain/Contracts/Output/EnhancedScanGroup.cs
@@ -0,0 +1,10 @@
+﻿using TechnicalAnalysis.CommonModels.Indicators.Advanced;
+
+namespace TechnicalAnalysis.Domain.Contracts.Output
+{
+    public class EnhancedScanGroup
+    {
+        public DateTime CandlestickCloseDate { get; set; }
+        public List<EnhancedScan> EnhancedScans { get; set; } = [];
+    }
+}
