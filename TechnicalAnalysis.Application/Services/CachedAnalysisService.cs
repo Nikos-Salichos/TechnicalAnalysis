04fbafd8674d6c1a670f7dcfdd0148e0c38bef10
Nikos S <niksal1991@gmail.com>
2024-02-27 18:02:40 +0200

    Changed service

--- TechnicalAnalysis.Application/Services/CachedAnalysisService.cs
+++ TechnicalAnalysis.Application/Services/CachedAnalysisService.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using TechnicalAnalysis.CommonModels.BusinessModels;
 using TechnicalAnalysis.CommonModels.Enums;
+using TechnicalAnalysis.Domain.Contracts.Output;
 using TechnicalAnalysis.Domain.Interfaces.Application;
 using TechnicalAnalysis.Domain.Interfaces.Infrastructure;
 using DataProvider = TechnicalAnalysis.CommonModels.Enums.DataProvider;
@@ -10,16 +11,19 @@ namespace TechnicalAnalysis.Application.Services
     public class CachedAnalysisService(IAnalysisService inner, IRedisRepository redisRepository,
         ICommunication communication, IRabbitMqService rabbitMqService) : IAnalysisService
     {
-        public async Task<IEnumerable<PairExtended>> GetIndicatorsByPairNamesAsync(IEnumerable<string> pairNames, Timeframe timeframe)
+        public async Task<IEnumerable<PairExtended>> GetIndicatorsByPairNamesAsync(IEnumerable<string> pairNames, Timeframe timeframe, HttpContext? httpContext = null)
         {
             var pairsFromCache = new List<PairExtended>();
 
-            foreach (var pairName in pairNames)
+            if (httpContext?.Request.Headers.ContainsKey("C-Invalid") == false)
             {
-                var cachedPair = await redisRepository.GetRecordAsync<PairExtended>(pairName);
-                if (cachedPair != null)
+                foreach (var pairName in pairNames)
                 {
-                    pairsFromCache.Add(cachedPair);
+                    var cachedPair = await redisRepository.GetRecordAsync<PairExtended>(pairName);
+                    if (cachedPair != null)
+                    {
+                        pairsFromCache.Add(cachedPair);
+                    }
                 }
             }
 
@@ -39,14 +43,15 @@ namespace TechnicalAnalysis.Application.Services
             return pairsFromCache;
         }
 
+        //TODO fix it to return proper type
         public async Task<IEnumerable<PairExtended>> GetPairsIndicatorsAsync(DataProvider provider, HttpContext? httpContext = null)
         {
             var fileName = $"All-Pairs-Indicators-{DateTime.UtcNow}";
 
             if (httpContext?.Request.Headers.ContainsKey("C-Invalid") == false)
             {
-                var cachedPairs = await redisRepository.GetRecordAsync<IEnumerable<PairExtended>>(provider.ToString());
-                if (cachedPairs?.Any() == true)
+                var cachedPairs = await redisRepository.GetRecordAsync<List<EnhancedPairResult>>(provider.ToString());
+                if (cachedPairs?.Count > 0)
                 {
                     await communication.CreateAttachmentSendMessage(cachedPairs, fileName);
                     rabbitMqService.PublishMessage(cachedPairs);
@@ -54,7 +59,7 @@ namespace TechnicalAnalysis.Application.Services
                     // Example how to consume message
                     // var message = await rabbitMqService.ConsumeMessageAsync<IEnumerable<PairExtended>>();
 
-                    return cachedPairs;
+                    return [];
                 }
             }
 
@@ -69,18 +74,18 @@ namespace TechnicalAnalysis.Application.Services
                          .OrderByDescending(c => c.CloseDate)
                          .ThenBy(c => c.EnhancedScans.OrderBy(es => es.OrderOfSignal))
                          .GroupBy(c => c.PoolOrPairId)
-                         .Select(group => new
+                         .Select(group => new EnhancedScanGroup
                          {
                              CandlestickCloseDate = group.First().CloseDate,
-                             group.First().EnhancedScans
+                             EnhancedScans = group.First().EnhancedScans.ToList()
                          })
                          .ToList();
 
-                     return new
+                     return new EnhancedPairResult
                      {
-                         pair.Symbol,
+                         Symbol = pair.Symbol,
                          EnhancedScans = enhancedScans,
-                         enhancedScans[0].EnhancedScans[0].OrderOfSignal
+                         OrderOfSignal = enhancedScans[0].EnhancedScans[0].OrderOfSignal
                      };
                  })
                  .Where(result => result.EnhancedScans.Count > 0)
@@ -91,38 +96,12 @@ namespace TechnicalAnalysis.Application.Services
                 .ThenBy(result => result.OrderOfSignal)
                 .ToList();
 
-            /*            var vhfPairs = pairs
-                           .OrderByDescending(pair => pair.CreatedAt)
-                           .Select(pair =>
-                           {
-                               var enhancedScans = pair.Candlesticks
-                                   .Where(c => c.VerticalHorizontalFilterRanges.Count > 0)
-                                   .OrderByDescending(c => c.CloseDate)
-                                   .GroupBy(c => c.PoolOrPairId)
-                                   .Select(group => new
-                                   {
-                                       CandlestickCloseDate = group.First().CloseDate,
-                                       group.First().VerticalHorizontalFilterRanges
-                                   })
-                                   .ToList();
-
-                               return new
-                               {
-                                   pair.Symbol,
-                                   VerticalHorizontalFilterRanges = enhancedScans
-                               };
-                           })
-                           .Where(result => result.VerticalHorizontalFilterRanges.Count > 0)
-                           .ToList();
-
-                        var sortedPairsByRange = vhfPairs.OrderByDescending(result => result.VerticalHorizontalFilterRanges.FirstOrDefault()?.CandlestickCloseDate).ToList();*/
-
             //TODO Instead of anonymous object create a class
             await redisRepository.SetRecordAsync(provider.ToString(), sortedPairsByEnhanced, null, null);
             await communication.CreateAttachmentSendMessage(sortedPairsByEnhanced, fileName);
-            rabbitMqService.PublishMessage(pairs);
+            rabbitMqService.PublishMessage(sortedPairsByEnhanced);
 
-            return pairs;
+            return [];
         }
     }
 }
