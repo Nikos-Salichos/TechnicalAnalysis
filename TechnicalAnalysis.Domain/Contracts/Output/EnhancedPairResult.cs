4adf3829321da919d2451fdf9ce8cf2263a30b13
Nikos S <niksal1991@gmail.com>
2024-02-27 18:03:42 +0200

    Added model

---
+++ TechnicalAnalysis.Domain/Contracts/Output/EnhancedPairResult.cs
@@ -0,0 +1,9 @@
+﻿namespace TechnicalAnalysis.Domain.Contracts.Output
+{
+    public class EnhancedPairResult
+    {
+        public string Symbol { get; set; } = string.Empty;
+        public List<EnhancedScanGroup> EnhancedScans { get; set; } = [];
+        public int OrderOfSignal { get; set; }
+    }
+}
