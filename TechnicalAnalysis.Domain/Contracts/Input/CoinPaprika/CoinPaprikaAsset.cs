c92819f96046b9a258bd043a8139fa3958155e3f
Nikos S <niksal1991@gmail.com>
2024-02-27 18:02:53 +0200

    Changed asset

---
+++ TechnicalAnalysis.Domain/Contracts/Input/CoinPaprika/CoinPaprikaAsset.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TechnicalAnalysis.Domain.Contracts.Input.CoinPaprika
+{
+    public class CoinPaprikaAsset
+    {
+        [JsonPropertyName("id")]
+        public string CoinPaprikaId { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("symbol")]
+        public string Symbol { get; set; }
+
+        [JsonPropertyName("rank")]
+        public int Rank { get; set; }
+
+        [JsonPropertyName("is_new")]
+        public bool IsNew { get; set; }
+
+        [JsonPropertyName("is_active")]
+        public bool IsActive { get; set; }
+
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+
+        [JsonPropertyName("CreatedAt")]
+        public DateTime CreatedAt { get; set; }
+    }
+}
