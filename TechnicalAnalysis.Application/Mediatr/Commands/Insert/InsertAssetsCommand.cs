891e03289f62805970fc45e7f585cccfba78cc8d
Nikos S <niksal1991@gmail.com>
2024-03-02 12:25:39 +0200

    Changed command

---
+++ TechnicalAnalysis.Application/Mediatr/Commands/Insert/InsertAssetsCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using TechnicalAnalysis.CommonModels.BusinessModels;
+
+namespace TechnicalAnalysis.Application.Mediatr.Commands.Insert
+{
+    public class InsertAssetsCommand(IEnumerable<Asset> assets) : IRequest
+    {
+        public IEnumerable<Asset> Assets { get; } = assets;
+    }
+}
