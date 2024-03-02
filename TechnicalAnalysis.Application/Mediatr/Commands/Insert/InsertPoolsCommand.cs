f4a64160bf7a0f795e861ab10591d5bb5920d270
Nikos S <niksal1991@gmail.com>
2024-03-02 12:25:59 +0200

    Changed command

---
+++ TechnicalAnalysis.Application/Mediatr/Commands/Insert/InsertPoolsCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using TechnicalAnalysis.Domain.Entities;
+
+namespace TechnicalAnalysis.Application.Mediatr.Commands.Insert
+{
+    public class InsertPoolsCommand(IEnumerable<Pool> pools) : IRequest
+    {
+        public IEnumerable<Pool> Pools { get; } = pools;
+    }
+}
