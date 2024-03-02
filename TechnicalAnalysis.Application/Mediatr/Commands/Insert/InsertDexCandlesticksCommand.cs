ce0a22bc5549a6f79e3ffc5adde43e22373da215
Nikos S <niksal1991@gmail.com>
2024-03-02 12:25:56 +0200

    Changed command

---
+++ TechnicalAnalysis.Application/Mediatr/Commands/Insert/InsertDexCandlesticksCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using TechnicalAnalysis.Domain.Entities;
+
+namespace TechnicalAnalysis.Application.Mediatr.Commands.Insert
+{
+    public class InsertDexCandlesticksCommand(IEnumerable<DexCandlestick> dexCandlesticks) : IRequest
+    {
+        public IEnumerable<DexCandlestick> DexCandlesticks { get; } = dexCandlesticks;
+    }
+}
