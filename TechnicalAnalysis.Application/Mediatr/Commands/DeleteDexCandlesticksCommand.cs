c2aeba5b6a1ca88e187c045b17bc0dbd89c3c739
Nikos S <niksal1991@gmail.com>
2024-03-02 12:27:12 +0200

    Removed command

--- TechnicalAnalysis.Application/Mediatr/Commands/DeleteDexCandlesticksCommand.cs
+++
@@ -1,9 +0,0 @@
-﻿using MediatR;
-
-namespace TechnicalAnalysis.Application.Mediatr.Commands
-{
-    public class DeleteDexCandlesticksCommand(IEnumerable<long> ids) : IRequest
-    {
-        public IEnumerable<long> Ids { get; } = ids;
-    }
-}
