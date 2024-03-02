a7e33ad8c34be92dc9bd0c01b28472a25e897687
Nikos S <niksal1991@gmail.com>
2024-03-02 12:27:05 +0200

    Changed command

---
+++ TechnicalAnalysis.Application/Mediatr/Commands/Update/UpdateExchangeCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using TechnicalAnalysis.CommonModels.BusinessModels;
+
+namespace TechnicalAnalysis.Application.Mediatr.Commands.Update
+{
+    public class UpdateExchangeCommand(ProviderPairAssetSyncInfo providerPairAssetSyncInfo,
+        ProviderCandlestickSyncInfo providerCandlestickSyncInfo) : IRequest
+    {
+        public ProviderPairAssetSyncInfo ProviderPairAssetSyncInfo { get; } = providerPairAssetSyncInfo;
+        public ProviderCandlestickSyncInfo ProviderCandlestickSyncInfo { get; } = providerCandlestickSyncInfo;
+    }
+}
