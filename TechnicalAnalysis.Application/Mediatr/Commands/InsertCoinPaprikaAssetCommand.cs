8b1adda36b07b27fe27a0d3f55e369524ea7eb90
Nikos S <niksal1991@gmail.com>
2024-03-02 12:20:28 +0200

    Changed command

---
+++ TechnicalAnalysis.Application/Mediatr/Commands/InsertCoinPaprikaAssetCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using TechnicalAnalysis.CommonModels.BusinessModels;
+
+namespace TechnicalAnalysis.Application.Mediatr.Commands
+{
+    public class InsertCoinPaprikaAssetCommand(IEnumerable<CoinPaprikaAsset> assets) : IRequest
+    {
+        public IEnumerable<CoinPaprikaAsset> Assets { get; } = assets;
+    }
+}
