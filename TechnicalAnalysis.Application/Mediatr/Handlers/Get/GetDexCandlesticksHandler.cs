1882cea756e1428253117457f1e14790e55ec91b
Nikos S <niksal1991@gmail.com>
2024-03-04 07:42:04 +0200

    Changed handler

---
+++ TechnicalAnalysis.Application/Mediatr/Handlers/Get/GetDexCandlesticksHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using TechnicalAnalysis.Application.Mediatr.Queries;
+using TechnicalAnalysis.Domain.Entities;
+using TechnicalAnalysis.Domain.Interfaces.Infrastructure;
+
+namespace TechnicalAnalysis.Application.Mediatr.Handlers.Get
+{
+    public class GetDexCandlesticksHandler(IPostgreSqlRepository repository)
+        : IRequestHandler<GetDexCandlesticksQuery, IEnumerable<DexCandlestick>>
+    {
+        public async Task<IEnumerable<DexCandlestick>> Handle(GetDexCandlesticksQuery getDexCandlesticksQuery, CancellationToken cancellationToken)
+        {
+            var result = await repository.GetDexCandlesticksAsync();
+            if (result.HasError)
+            {
+                return Enumerable.Empty<DexCandlestick>();
+            }
+            return result.SuccessValue;
+        }
+    }
+}
