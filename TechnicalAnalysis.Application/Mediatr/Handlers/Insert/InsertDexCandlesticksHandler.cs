9aee3ff947cdb257f26135761ac041ad887532cc
Nikos S <niksal1991@gmail.com>
2024-03-04 08:04:08 +0200

    Changed handler

---
+++ TechnicalAnalysis.Application/Mediatr/Handlers/Insert/InsertDexCandlesticksHandler.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using TechnicalAnalysis.Application.Mediatr.Commands.Insert;
+using TechnicalAnalysis.Domain.Interfaces.Infrastructure;
+
+namespace TechnicalAnalysis.Application.Mediatr.Handlers.Insert
+{
+    public class InsertDexCandlesticksHandler(IPostgreSqlRepository repository) : IRequestHandler<InsertDexCandlesticksCommand>
+    {
+        public async Task Handle(InsertDexCandlesticksCommand request, CancellationToken cancellationToken)
+        {
+            await repository.InsertDexCandlesticksAsync(request.DexCandlesticks);
+        }
+    }
+}
