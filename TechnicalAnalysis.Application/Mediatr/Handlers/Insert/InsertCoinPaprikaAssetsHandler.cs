e669f70201c150c3ccfbad57ea78d49df25501d1
Nikos S <niksal1991@gmail.com>
2024-03-04 08:04:11 +0200

    Changed handler

---
+++ TechnicalAnalysis.Application/Mediatr/Handlers/Insert/InsertCoinPaprikaAssetsHandler.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using TechnicalAnalysis.Application.Mediatr.Commands.Insert;
+using TechnicalAnalysis.Domain.Interfaces.Infrastructure;
+
+namespace TechnicalAnalysis.Application.Mediatr.Handlers.Insert
+{
+    public class InsertCoinPaprikaAssetsHandler(IPostgreSqlRepository repository) : IRequestHandler<InsertCoinPaprikaAssetCommand>
+    {
+        public async Task Handle(InsertCoinPaprikaAssetCommand request, CancellationToken cancellationToken)
+        {
+            await repository.InsertCoinPaprikaAssetsAsync(request.Assets);
+        }
+    }
+}
