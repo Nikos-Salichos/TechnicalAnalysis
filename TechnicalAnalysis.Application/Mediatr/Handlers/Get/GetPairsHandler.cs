7a9039736c895ec10aec2d3018d759b864e8f151
Nikos S <niksal1991@gmail.com>
2024-03-04 07:42:18 +0200

    Changed handler

---
+++ TechnicalAnalysis.Application/Mediatr/Handlers/Get/GetPairsHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using TechnicalAnalysis.Application.Mappers;
+using TechnicalAnalysis.Application.Mediatr.Queries;
+using TechnicalAnalysis.CommonModels.BusinessModels;
+using TechnicalAnalysis.Domain.Interfaces.Infrastructure;
+
+namespace TechnicalAnalysis.Application.Mediatr.Handlers.Get
+{
+    public class GetPairsHandler(IPostgreSqlRepository repository) : IRequestHandler<GetPairsQuery, IEnumerable<PairExtended>>
+    {
+        public async Task<IEnumerable<PairExtended>> Handle(GetPairsQuery getCoinsQuery, CancellationToken cancellationToken)
+        {
+            var result = await repository.GetPairsAsync();
+            if (result.HasError)
+            {
+                return Enumerable.Empty<PairExtended>();
+            }
+            return result.SuccessValue.ToDomain();
+        }
+    }
+}
