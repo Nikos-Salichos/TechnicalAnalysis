00323f37fc83a400a9542ea40f33bfdb6fba4994
Nikos S <niksal1991@gmail.com>
2024-03-04 08:04:02 +0200

    Changed handler

---
+++ TechnicalAnalysis.Application/Mediatr/Handlers/Insert/InsertAssetsHandler.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using TechnicalAnalysis.Application.Mediatr.Commands.Insert;
+using TechnicalAnalysis.Domain.Interfaces.Infrastructure;
+
+namespace TechnicalAnalysis.Application.Mediatr.Handlers.Insert
+{
+    public class InsertAssetsHandler(IPostgreSqlRepository repository) : IRequestHandler<InsertAssetsCommand>
+    {
+        public async Task Handle(InsertAssetsCommand request, CancellationToken cancellationToken)
+        {
+            await repository.InsertAssetsAsync(request.Assets);
+        }
+    }
+}
