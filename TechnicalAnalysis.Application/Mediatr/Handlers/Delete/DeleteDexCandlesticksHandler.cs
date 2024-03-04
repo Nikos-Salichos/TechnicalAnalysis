77e49b62a1121bfd569e47860e55dddb551559e3
Nikos S <niksal1991@gmail.com>
2024-03-04 07:41:48 +0200

    Changed mediatr

---
+++ TechnicalAnalysis.Application/Mediatr/Handlers/Delete/DeleteDexCandlesticksHandler.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using TechnicalAnalysis.Application.Mediatr.Commands.Delete;
+using TechnicalAnalysis.Domain.Entities;
+using TechnicalAnalysis.Domain.Interfaces.Infrastructure;
+
+namespace TechnicalAnalysis.Application.Mediatr.Handlers.Delete
+{
+    public class DeleteDexCandlesticksHandler(IPostgreSqlRepository repository) : IRequestHandler<DeleteDexCandlesticksCommand>
+    {
+        public async Task Handle(DeleteDexCandlesticksCommand request, CancellationToken cancellationToken)
+        {
+            await repository.DeleteEntitiesByIdsAsync<DexCandlestick>(request.Ids, "DexCandlesticks");
+        }
+    }
+}
