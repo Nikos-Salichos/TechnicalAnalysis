0a9878d98310f44f87064ad2c34a014c0332fcbd
Nikos S <niksal1991@gmail.com>
2024-03-04 08:04:22 +0200

    Changed handler

---
+++ TechnicalAnalysis.Application/Mediatr/Handlers/Insert/InsertPoolsHandler.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using TechnicalAnalysis.Application.Mediatr.Commands.Insert;
+using TechnicalAnalysis.Domain.Interfaces.Infrastructure;
+
+namespace TechnicalAnalysis.Application.Mediatr.Handlers.Insert
+{
+    public class InsertPoolsHandler(IPostgreSqlRepository repository) : IRequestHandler<InsertPoolsCommand>
+    {
+        public async Task Handle(InsertPoolsCommand request, CancellationToken cancellationToken)
+        {
+            await repository.InsertPoolsAsync(request.Pools);
+        }
+    }
+}
