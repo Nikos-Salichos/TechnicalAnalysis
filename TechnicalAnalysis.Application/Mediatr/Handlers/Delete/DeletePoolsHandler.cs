466fcdb05c0a60b014f32fc8f01c4c939f24dea0
Nikos S <niksal1991@gmail.com>
2024-03-04 07:41:51 +0200

    Changed mediatr

---
+++ TechnicalAnalysis.Application/Mediatr/Handlers/Delete/DeletePoolsHandler.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using TechnicalAnalysis.Application.Mediatr.Commands.Delete;
+using TechnicalAnalysis.Domain.Contracts.Input.DexV3;
+using TechnicalAnalysis.Domain.Interfaces.Infrastructure;
+
+namespace TechnicalAnalysis.Application.Mediatr.Handlers.Delete;
+
+public class DeletePoolsHandler(IPostgreSqlRepository repository) : IRequestHandler<DeletePoolsCommand>
+{
+    public async Task Handle(DeletePoolsCommand request, CancellationToken cancellationToken)
+    {
+        await repository.DeleteEntitiesByIdsAsync<Pool>(request.Ids, "Pools");
+    }
+}
