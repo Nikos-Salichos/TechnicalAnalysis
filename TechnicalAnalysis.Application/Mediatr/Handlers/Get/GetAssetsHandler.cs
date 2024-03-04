241e3a9e03e4642e88d6caa0af8b9fe675e0a0b4
Nikos S <niksal1991@gmail.com>
2024-03-04 07:41:58 +0200

    Changed handler

---
+++ TechnicalAnalysis.Application/Mediatr/Handlers/Get/GetAssetsHandler.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using TechnicalAnalysis.Application.Mediatr.Queries;
+using TechnicalAnalysis.CommonModels.BusinessModels;
+using TechnicalAnalysis.Domain.Interfaces.Infrastructure;
+
+namespace TechnicalAnalysis.Application.Mediatr.Handlers.Get;
+
+public class GetAssetsHandler(IPostgreSqlRepository repository) : IRequestHandler<GetAssetsQuery, IEnumerable<Asset>>
+{
+    public async Task<IEnumerable<Asset>> Handle(GetAssetsQuery getAssetsQuery, CancellationToken cancellationToken)
+    {
+        var result = await repository.GetAssetsAsync();
+        if (result.HasError)
+        {
+            return [];
+        }
+        return result.SuccessValue;
+    }
+}
