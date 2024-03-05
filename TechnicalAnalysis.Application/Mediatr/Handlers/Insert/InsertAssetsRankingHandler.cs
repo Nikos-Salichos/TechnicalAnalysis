3e0357bb5290576152d714fca9f44c85532aad21
Nikos S <niksal1991@gmail.com>
2024-03-05 20:18:21 +0200

    Refactored code

---
+++ TechnicalAnalysis.Application/Mediatr/Handlers/Insert/InsertAssetsRankingHandler.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using TechnicalAnalysis.Application.Mediatr.Commands.Insert;
+using TechnicalAnalysis.Domain.Interfaces.Infrastructure;
+
+namespace TechnicalAnalysis.Application.Mediatr.Handlers.Insert
+{
+    public class InsertAssetsRankingHandler(IPostgreSqlRepository repository) : IRequestHandler<InsertAssetsRankingCommand>
+    {
+        public async Task Handle(InsertAssetsRankingCommand request, CancellationToken cancellationToken)
+        {
+            await repository.InsertCoinPaprikaAssetsAsync(request.Assets);
+        }
+    }
+}
