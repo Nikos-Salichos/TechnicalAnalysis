979358c34b128536a81eec54728d835a907b7f71
Nikos S <niksal1991@gmail.com>
2024-03-05 20:18:00 +0200

    Added command

---
+++ TechnicalAnalysis.Application/Mediatr/Commands/Insert/InsertAssetsRankingCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using TechnicalAnalysis.CommonModels.BusinessModels;
+
+namespace TechnicalAnalysis.Application.Mediatr.Commands.Insert
+{
+    public class InsertAssetsRankingCommand(IEnumerable<CoinPaprikaAsset> assets) : IRequest
+    {
+        public IEnumerable<CoinPaprikaAsset> Assets { get; } = assets;
+    }
+}
