5f2bcae26cb6abcc8bea21bc1b48c49c58bb0cd6
Nikos S <niksal1991@gmail.com>
2024-03-05 20:18:12 +0200

    Refactored code

---
+++ TechnicalAnalysis.Application/Mediatr/Handlers/Get/GetAssetsRankingHandler.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using TechnicalAnalysis.CommonModels.BusinessModels;
+using TechnicalAnalysis.Domain.Interfaces.Infrastructure;
+
+namespace TechnicalAnalysis.Application.Mediatr.Handlers.Get
+{
+    public class GetAssetsRankingHandler(IPostgreSqlRepository repository) : IRequestHandler<GetAssetsRankingQuery, IEnumerable<CoinPaprikaAsset>>
+    {
+        public async Task<IEnumerable<CoinPaprikaAsset>> Handle(GetAssetsRankingQuery getCoinPaprikaAssetsQuery, CancellationToken cancellationToken)
+        {
+            var result = await repository.GetCoinPaprikaAssetsAsync();
+            if (result.HasError)
+            {
+                return [];
+            }
+            return result.SuccessValue;
+        }
+    }
+}
