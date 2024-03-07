6d57dd4c6998f487e06667c2bee407d0a81271a2
Nikos S <niksal1991@gmail.com>
2024-03-07 08:18:11 +0200

    Added interface

---
+++ TechnicalAnalysis.Domain/Interfaces/Infrastructure/ICoinPaprikaHttpClient.cs
@@ -0,0 +1,10 @@
+﻿using TechnicalAnalysis.Domain.Contracts.Input.CoinPaprika;
+using TechnicalAnalysis.Domain.Interfaces.Utilities;
+
+namespace TechnicalAnalysis.Domain.Interfaces.Infrastructure
+{
+    public interface ICoinPaprikaHttpClient
+    {
+        Task<IResult<IEnumerable<CoinPaprikaAssetContract>, string>> SyncAssets();
+    }
+}
