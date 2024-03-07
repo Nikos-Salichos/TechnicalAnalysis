d142989f0400d479bcf29254f19350fe07901b4e
Nikos S <niksal1991@gmail.com>
2024-03-07 08:17:56 +0200

    Added coinmarketcap client

---
+++ TechnicalAnalysis.Domain/Interfaces/Infrastructure/ICoinMarketCapHttpClient.cs
@@ -0,0 +1,10 @@
+﻿using TechnicalAnalysis.Domain.Contracts.Input.CoinMarketCap;
+using TechnicalAnalysis.Domain.Interfaces.Utilities;
+
+namespace TechnicalAnalysis.Domain.Interfaces.Infrastructure
+{
+    public interface ICoinMarketCapHttpClient
+    {
+        Task<IResult<CoinMarketCapAssetContract, string>> SyncAssets();
+    }
+}
