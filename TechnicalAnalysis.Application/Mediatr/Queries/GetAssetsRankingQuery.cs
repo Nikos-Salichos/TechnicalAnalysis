4c4070cfc16d4b97c2ec2e402b9ecf297f80dc06
Nikos S <niksal1991@gmail.com>
2024-03-07 08:17:35 +0200

    Added query

---
+++ TechnicalAnalysis.Application/Mediatr/Queries/GetAssetsRankingQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using TechnicalAnalysis.CommonModels.BusinessModels;
+
+namespace TechnicalAnalysis.Application.Mediatr.Queries
+{
+    public class GetAssetsRankingQuery : IRequest<IEnumerable<AssetRanking>>
+    {
+    }
+}
