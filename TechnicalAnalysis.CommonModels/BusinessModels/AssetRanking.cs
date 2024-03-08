7b5c0cd9efa881049dad665dd467735f6f258204
Nikos S <niksal1991@gmail.com>
2024-03-08 18:59:45 +0200

    Changed model

---
+++ TechnicalAnalysis.CommonModels/BusinessModels/AssetRanking.cs
@@ -0,0 +1,30 @@
+﻿using TechnicalAnalysis.CommonModels.Enums;
+
+namespace TechnicalAnalysis.CommonModels.BusinessModels
+{
+    public sealed class AssetRanking : IEquatable<AssetRanking>
+    {
+        public string Symbol { get; init; } = string.Empty;
+        public string Name { get; init; } = string.Empty;
+        public DateTime CreatedDate { get; init; }
+        public AssetType AssetType { get; init; }
+        public DataProvider DataProvider { get; init; }
+
+        public bool Equals(AssetRanking? other)
+        {
+            if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
+            return other != null
+                && Symbol == other.Symbol
+                && Name == other.Name
+                && AssetType == other.AssetType;
+        }
+
+        public override bool Equals(object? obj) => obj is not null && Equals(obj as AssetRanking);
+
+        public override int GetHashCode() => HashCode.Combine(Symbol);
+    }
+}
