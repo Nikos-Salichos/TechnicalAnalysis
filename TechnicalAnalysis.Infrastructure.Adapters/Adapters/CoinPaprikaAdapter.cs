0c9196a58bf1116a78e813753b9b82f74f72681a
Nikos S <niksal1991@gmail.com>
2024-03-08 18:59:08 +0200

    Changed adapter

--- TechnicalAnalysis.Infrastructure.Adapters/Adapters/CoinPaprikaAdapter.cs
+++ TechnicalAnalysis.Infrastructure.Adapters/Adapters/CoinPaprikaAdapter.cs
@@ -1,35 +1,32 @@
 ﻿using MediatR;
 using Microsoft.Extensions.Logging;
 using TechnicalAnalysis.Application.Mappers;
-using TechnicalAnalysis.Application.Mediatr.Commands;
+using TechnicalAnalysis.Application.Mediatr.Commands.Insert;
 using TechnicalAnalysis.Application.Mediatr.Queries;
 using TechnicalAnalysis.CommonModels.Enums;
 using TechnicalAnalysis.Domain.Interfaces.Infrastructure;
 
 namespace TechnicalAnalysis.Infrastructure.Adapters.Adapters
 {
-    public class CoinPaprikaAdapter(ICoinPaprikaClient coinPaprikaClient, IMediator mediator, ILogger<CoinPaprikaAdapter> logger) : IAdapter
+    public class CoinPaprikaAdapter(ICoinPaprikaHttpClient coinPaprikaClient, IMediator mediator, ILogger<CoinPaprikaAdapter> logger) : IAdapter
     {
         public async Task<bool> Sync(DataProvider provider, Timeframe timeframe)
         {
             var response = await coinPaprikaClient.SyncAssets();
-
             if (response.HasError)
             {
                 logger.LogError("CoinPaprika response error {FailValue}", response.FailValue);
                 return false;
             }
 
-            //Fetch old assets
-            var fetchedAssets = await mediator.Send(new GetAssetsQuery());
+            var newLayerOneAssets = response.SuccessValue.Where(c => c.Type is "coin").ToList();
 
-            //Find differential;
-            var newAssets = response.SuccessValue.ToDomain().Except(fetchedAssets).Distinct().ToList();
+            var fetchedAssets = await mediator.Send(new GetAssetsRankingQuery());
 
-            //Insert differential
+            var newAssets = newLayerOneAssets.ToDomain().Except(fetchedAssets).Distinct().ToList();
             if (newAssets.Count > 0)
             {
-                await mediator.Send(new InsertAssetsCommand(newAssets));
+                await mediator.Send(new InsertAssetsRankingCommand(newAssets));
             }
 
             return true;
