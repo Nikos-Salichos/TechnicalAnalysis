66db2a40de9eaea69d5f569c2cc80f6807e79e89
Nikos S <niksal1991@gmail.com>
2024-03-12 09:01:22 +0200

    Added settings

---
+++ TechnicalAnalysis.Domain/Settings/CoinMarketCapSetting.cs
@@ -0,0 +1,8 @@
+﻿namespace TechnicalAnalysis.Domain.Settings
+{
+    public class CoinMarketCapSetting
+    {
+        public string ApiKey { get; init; } = string.Empty;
+        public string ListingsLatestEndpoint { get; init; } = string.Empty;
+    }
+}
