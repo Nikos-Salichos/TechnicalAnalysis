f0323fa75d45279af0e0797d805b13b846984330
Nikos S <niksal1991@gmail.com>
2024-03-16 23:27:12 +0200

    Added model

--- TechnicalAnalysis.CommonModels/BusinessModels/AssetRanking.cs
+++ TechnicalAnalysis.CommonModels/BusinessModels/AssetRanking.cs
@@ -2,26 +2,12 @@
 
 namespace TechnicalAnalysis.CommonModels.BusinessModels
 {
-    public sealed class AssetRanking : IEquatable<AssetRanking>
+    public sealed class AssetRanking
     {
         public string Symbol { get; init; } = string.Empty;
         public string Name { get; init; } = string.Empty;
         public DateTime CreatedDate { get; init; }
         public AssetType AssetType { get; init; }
         public DataProvider DataProvider { get; init; }
-
-        public bool Equals(AssetRanking? other)
-        {
-            if (ReferenceEquals(this, other))
-            {
-                return true;
-            }
-
-            return other != null && Name == other.Name;
-        }
-
-        public override bool Equals(object? obj) => obj is not null && Equals(obj as AssetRanking);
-
-        public override int GetHashCode() => HashCode.Combine(Name);
     }
 }
