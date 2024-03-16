3dab67e896b18039c4e48cd81b7b37e985903139
Nikos S <niksal1991@gmail.com>
2024-03-16 23:57:08 +0200

    Changed CandlestickExt

--- TechnicalAnalysis.CommonModels/BusinessModels/CandlestickExtended.cs
+++ TechnicalAnalysis.CommonModels/BusinessModels/CandlestickExtended.cs
@@ -18,50 +18,49 @@ namespace TechnicalAnalysis.CommonModels.BusinessModels
         public Timeframe Timeframe { get; set; }
         public DateTime OpenDate { get; set; }
         public DateTime CloseDate { get; set; }
-        public IList<BollingerBand> BollingerBands { get; init; } = new List<BollingerBand>();
-        public IList<DonchianChannel> DonchianChannels { get; init; } = new List<DonchianChannel>();
-        public IList<KeltnerChannel> KeltnerChannels { get; init; } = new List<KeltnerChannel>();
-        public IList<Rsi> Rsis { get; init; } = new List<Rsi>();
-        public IList<Stochastic> Stochastics { get; init; } = new List<Stochastic>();
-        public IList<Adx> Adxs { get; init; } = new List<Adx>();
-        public IList<Aroon> Aroons { get; init; } = new List<Aroon>();
-        public IList<Cci> Ccis { get; init; } = new List<Cci>();
-        public IList<MovingAverage> MovingAverages { get; init; } = new List<MovingAverage>();
-        public IList<Ichimoku> Ichimokus { get; init; } = new List<Ichimoku>();
-        public IList<EnhancedScan> EnhancedScans { get; init; } = new List<EnhancedScan>();
-        public IList<Lowest> Lowests { get; init; } = new List<Lowest>();
-        public IList<Highest> Highests { get; init; } = new List<Highest>();
-        public IList<DragonFlyDoji> DragonFlyDojis { get; init; } = new List<DragonFlyDoji>();
-        public IList<Hammer> Hammers { get; init; } = new List<Hammer>();
-        public IList<InvertedHammer> InvertedHammers { get; init; } = new List<InvertedHammer>();
-        public IList<SpinningTop> SpinningTops { get; init; } = new List<SpinningTop>();
-        public IList<Marubozu> Marubozus { get; init; } = new List<Marubozu>();
-        public IList<Macd> Macds { get; init; } = new List<Macd>();
-        public IList<PriceFunnel> PriceFunnels { get; init; } = new List<PriceFunnel>();
-        public IList<TypicalPriceReversal> TypicalPriceReversals { get; init; } = new List<TypicalPriceReversal>();
-        public IList<BollingerBandsFunnel> BollingerBandsFunnels { get; init; } = new List<BollingerBandsFunnel>();
-        public IList<FlagNestedCandlestickBody> FlagsNestedCandlesticksBody { get; init; } = new List<FlagNestedCandlestickBody>();
-        public IList<FlagNestedCandlestickRange> FlagsNestedCandlesticksRange { get; init; } = new List<FlagNestedCandlestickRange>();
-        public IList<Volatility> Volatilities { get; init; } = new List<Volatility>();
-        public IList<StandardDeviation> StandardDeviations { get; init; } = new List<StandardDeviation>();
-        public IList<RateOfChange> RateOfChanges { get; init; } = new List<RateOfChange>();
-        public IList<AverageTrueRange> AverageTrueRanges { get; init; } = new List<AverageTrueRange>();
-        public IList<ResistanceBreakout> ResistanceBreakouts { get; init; } = new List<ResistanceBreakout>();
-        public IList<Fractal> Fractals { get; init; } = new List<Fractal>();
-        public IList<FractalLowest> FractalLowests { get; init; } = new List<FractalLowest>();
-        public IList<StandardPivotPoint> StandardPivotPoints { get; init; } = new List<StandardPivotPoint>();
-        public IList<AverageRange> AverageRanges { get; init; } = new List<AverageRange>();
-        public IList<CloseRelativeToPivot> CloseRelativeToPivots { get; init; } = new List<CloseRelativeToPivot>();
-        public IList<VixFix> VixFixes { get; init; } = new List<VixFix>();
-        public IList<VerticalHorizontalFilter> VerticalHorizontalFilters { get; init; } = new List<VerticalHorizontalFilter>();
-        public IList<VerticalHorizontalFilterRange> VerticalHorizontalFilterRanges { get; init; } = new List<VerticalHorizontalFilterRange>();
-        public IList<OnBalanceVolume> OnBalanceVolumes { get; init; } = new List<OnBalanceVolume>();
-        public IList<PsychologicalLine> PsychologicalLines { get; init; } = new List<PsychologicalLine>();
+        public IList<BollingerBand> BollingerBands { get; init; } = [];
+        public IList<DonchianChannel> DonchianChannels { get; init; } = [];
+        public IList<KeltnerChannel> KeltnerChannels { get; init; } = [];
+        public IList<Rsi> Rsis { get; init; } = [];
+        public IList<Stochastic> Stochastics { get; init; } = [];
+        public IList<Adx> Adxs { get; init; } = [];
+        public IList<Aroon> Aroons { get; init; } = [];
+        public IList<Cci> Ccis { get; init; } = [];
+        public IList<MovingAverage> MovingAverages { get; init; } = [];
+        public IList<Ichimoku> Ichimokus { get; init; } = [];
+        public IList<EnhancedScan> EnhancedScans { get; init; } = [];
+        public IList<Lowest> Lowests { get; init; } = [];
+        public IList<Highest> Highests { get; init; } = [];
+        public IList<DragonFlyDoji> DragonFlyDojis { get; init; } = [];
+        public IList<Hammer> Hammers { get; init; } = [];
+        public IList<InvertedHammer> InvertedHammers { get; init; } = [];
+        public IList<SpinningTop> SpinningTops { get; init; } = [];
+        public IList<Marubozu> Marubozus { get; init; } = [];
+        public IList<Macd> Macds { get; init; } = [];
+        public IList<PriceFunnel> PriceFunnels { get; init; } = [];
+        public IList<TypicalPriceReversal> TypicalPriceReversals { get; init; } = [];
+        public IList<BollingerBandsFunnel> BollingerBandsFunnels { get; init; } = [];
+        public IList<FlagNestedCandlestickBody> FlagsNestedCandlesticksBody { get; init; } = [];
+        public IList<FlagNestedCandlestickRange> FlagsNestedCandlesticksRange { get; init; } = [];
+        public IList<Volatility> Volatilities { get; init; } = [];
+        public IList<StandardDeviation> StandardDeviations { get; init; } = [];
+        public IList<RateOfChange> RateOfChanges { get; init; } = [];
+        public IList<AverageTrueRange> AverageTrueRanges { get; init; } = [];
+        public IList<ResistanceBreakout> ResistanceBreakouts { get; init; } = [];
+        public IList<Fractal> Fractals { get; init; } = [];
+        public IList<FractalLowest> FractalLowests { get; init; } = [];
+        public IList<StandardPivotPoint> StandardPivotPoints { get; init; } = [];
+        public IList<AverageRange> AverageRanges { get; init; } = [];
+        public IList<CloseRelativeToPivot> CloseRelativeToPivots { get; init; } = [];
+        public IList<VixFix> VixFixes { get; init; } = [];
+        public IList<VerticalHorizontalFilter> VerticalHorizontalFilters { get; init; } = [];
+        public IList<VerticalHorizontalFilterRange> VerticalHorizontalFilterRanges { get; init; } = [];
+        public IList<OnBalanceVolume> OnBalanceVolumes { get; init; } = [];
+        public IList<PsychologicalLine> PsychologicalLines { get; init; } = [];
         public Trend FractalTrend { get; set; }
         public Trend PriceTrend { get; set; }
         public IDictionary<string, double> CorrelationPerPair { get; init; } = new Dictionary<string, double>();
         public long? NumberOfTrades { get; set; }
-        public long TxCount { get; set; }
         public long? Liquidity { get; set; }
         public decimal? Fees { get; set; }
         public decimal? TotalValueLockedUsd { get; set; }
