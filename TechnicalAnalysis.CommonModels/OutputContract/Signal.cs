bbd672b90c7a000f05a7d6a16b8bd33612af79cc
Nikos S <niksal1991@gmail.com>
2024-03-18 00:09:30 +0200

    Changed model

---
+++ TechnicalAnalysis.CommonModels/OutputContract/Signal.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TechnicalAnalysis.CommonModels.OutputContract
+{
+    public class Signal
+    {
+        [JsonPropertyName("BUY")]
+        public int Buy { get; set; }
+
+        [JsonPropertyName("SELL")]
+        public int Sell { get; set; }
+
+        [JsonPropertyName("OPENED_AT")]
+        public string OpenedAt { get; set; } = string.Empty;
+        public decimal? EntryPrice { get; init; }
+    }
+}
