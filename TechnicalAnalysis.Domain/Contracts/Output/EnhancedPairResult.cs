8db21ba6f1a9eb49edd6a5a0d751062215b2c5bb
Nikos S <niksal1991@gmail.com>
2024-03-18 00:13:27 +0200

    Changed model

--- TechnicalAnalysis.Domain/Contracts/Output/EnhancedPairResult.cs
+++ TechnicalAnalysis.Domain/Contracts/Output/EnhancedPairResult.cs
@@ -2,8 +2,8 @@
 {
     public class EnhancedPairResult
     {
-        public string Symbol { get; set; } = string.Empty;
-        public List<EnhancedScanGroup> EnhancedScans { get; set; } = [];
-        public int OrderOfSignal { get; set; }
+        public string Symbol { get; init; } = string.Empty;
+        public List<EnhancedScanGroup> EnhancedScans { get; init; } = [];
+        public int OrderOfSignal { get; init; }
     }
 }
