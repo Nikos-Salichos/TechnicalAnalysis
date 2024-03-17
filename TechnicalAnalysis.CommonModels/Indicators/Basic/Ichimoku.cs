06c5ef62c4a8f7f3c0afcb540f333b331be8ab0b
Nikos S <niksal1991@gmail.com>
2024-03-17 10:48:21 +0200

    Changed Ichimoku

--- TechnicalAnalysis.CommonModels/Indicators/Basic/Ichimoku.cs
+++ TechnicalAnalysis.CommonModels/Indicators/Basic/Ichimoku.cs
@@ -2,15 +2,12 @@
 
 namespace TechnicalAnalysis.CommonModels.Indicators.Basic
 {
-    public class Ichimoku : BaseIndicator
+    public class Ichimoku(long candlestickId) : BaseIndicator(candlestickId)
     {
         public decimal? TenkanSen { get; init; } // conversion line
         public decimal? KijunSen { get; init; } // base line
         public decimal? SenkouSpanA { get; init; } // leading span A
         public decimal? SenkouSpanB { get; init; } // leading span B
         public decimal? ChikouSpan { get; init; } // lagging span
-        public Ichimoku(long candlestickId) : base(candlestickId)
-        {
-        }
     }
 }
