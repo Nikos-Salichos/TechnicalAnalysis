ccb6f2aae702f6185d2c1ecd530133ff5781a055
Nikos S <niksal1991@gmail.com>
2024-03-18 00:09:37 +0200

    Changed model

---
+++ TechnicalAnalysis.CommonModels/OutputContract/PartialPair.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TechnicalAnalysis.CommonModels.OutputContract
+{
+    public class PartialPair
+    {
+        [JsonPropertyName("symbol")]
+        public string Symbol { get; init; }
+
+        [JsonPropertyName("candles")]
+        public IEnumerable<Candle> Candles { get; init; } = [];
+    }
+}
