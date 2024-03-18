ac49799cb8d27d70e2f757b18af013bcba207518
Nikos S <niksal1991@gmail.com>
2024-03-18 09:03:09 +0200

    Changed interface

---
+++ TechnicalAnalysis.Domain/Interfaces/Infrastructure/ICoinRankingHttpClient.cs
@@ -0,0 +1,10 @@
+﻿using TechnicalAnalysis.Domain.Contracts.Input.CoinRanking;
+using TechnicalAnalysis.Domain.Interfaces.Utilities;
+
+namespace TechnicalAnalysis.Domain.Interfaces.Infrastructure
+{
+    public interface ICoinRankingHttpClient
+    {
+        Task<IResult<CoinRankingAssetContract, string>> SyncAssets(int offset);
+    }
+}
