2175b24803e3a8df3c82f75c9cce9e753a2d30ee
Nikos S <niksal1991@gmail.com>
2024-03-20 08:09:19 +0200

    Changed adpter

---
+++ TechnicalAnalysis.Infrastructure.Adapters/Adapters/CoinRankingAdapter.cs
@@ -0,0 +1,103 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using TechnicalAnalysis.Application.Extensions;
+using TechnicalAnalysis.Application.Mediatr.Commands.Insert;
+using TechnicalAnalysis.Application.Mediatr.Commands.Update;
+using TechnicalAnalysis.Application.Mediatr.Queries;
+using TechnicalAnalysis.CommonModels.BusinessModels;
+using TechnicalAnalysis.CommonModels.Enums;
+using TechnicalAnalysis.Domain.Contracts.Input.CoinRanking;
+using TechnicalAnalysis.Domain.Interfaces.Infrastructure;
+
+namespace TechnicalAnalysis.Infrastructure.Adapters.Adapters
+{
+    internal sealed class CoinRankingAdapter(ICoinRankingHttpClient coinRankingHttpClient, IMediator mediator, ILogger<CoinRankingAdapter> logger)
+    : IAdapter
+    {
+        public async Task<bool> Sync(DataProvider provider, Timeframe timeframe, List<ProviderSynchronization> exchanges)
+        {
+            var coinRankingProvider = exchanges.Find(p => p.ProviderPairAssetSyncInfo.DataProvider == provider);
+            coinRankingProvider ??= new ProviderSynchronization
+            {
+                ProviderPairAssetSyncInfo = new ProviderPairAssetSyncInfo { DataProvider = provider }
+            };
+
+            if (coinRankingProvider.IsProviderAssetPairsSyncedToday())
+            {
+                logger.LogInformation("{Provider} synchronized for today", provider);
+                return true;
+            }
+
+            var fetchedAssets = (await mediator.Send(new GetAssetsRankingQuery())).ToList();
+
+            const int initialPage = 0;
+            var initialResponse = await coinRankingHttpClient.SyncAssets(initialPage);
+            if (initialResponse.HasError)
+            {
+                logger.LogError("{DataProvider} initial response error {FailValue}", nameof(provider), initialResponse.FailValue);
+                return false;
+            }
+
+            await ProcessNewAssets(fetchedAssets, initialResponse.SuccessValue.Data.Coins);
+
+            var totalCoins = initialResponse.SuccessValue.Data.Stats.Total;
+            var coinsPerPage = initialResponse.SuccessValue.Data.Coins.Count;
+            var totalCoinsProcessed = coinsPerPage;
+
+            if (totalCoinsProcessed >= totalCoins)
+            {
+                return true;
+            }
+
+            var totalPages = (int)Math.Ceiling((double)totalCoins / coinsPerPage);
+
+            for (int pageIndex = 1; pageIndex < totalPages; pageIndex++)
+            {
+                var response = await coinRankingHttpClient.SyncAssets(pageIndex);
+                if (response.HasError)
+                {
+                    logger.LogError("{DataProvider} response error on page {PageIndex} with {FailValue}", nameof(provider), pageIndex, response.FailValue);
+                    continue;
+                }
+
+                await ProcessNewAssets(fetchedAssets, response.SuccessValue.Data.Coins);
+
+                totalCoinsProcessed += response.SuccessValue.Data.Coins.Count;
+                if (totalCoinsProcessed >= totalCoins)
+                {
+                    break;
+                }
+            }
+
+            coinRankingProvider.ProviderPairAssetSyncInfo.UpdateProviderInfo();
+            await mediator.Send(new UpdateExchangeCommand(coinRankingProvider.ProviderPairAssetSyncInfo));
+
+            return true;
+        }
+
+        private async Task ProcessNewAssets(List<AssetRanking> existingAssets, List<Coin> newCoins)
+        {
+            var newAssets = new List<AssetRanking>();
+            foreach (var newCoin in newCoins)
+            {
+                var pairExists = existingAssets.Find(f => string.Equals(f.Name.Trim(), newCoin.Name.Trim(), StringComparison.InvariantCultureIgnoreCase));
+                if (pairExists is null)
+                {
+                    newAssets.Add(new AssetRanking
+                    {
+                        Name = newCoin.Name,
+                        Symbol = newCoin.Symbol,
+                        CreatedDate = DateTime.UnixEpoch.AddSeconds(newCoin.ListedAt),
+                        AssetType = AssetType.Layer1,
+                        DataProvider = DataProvider.CoinMarketCap
+                    });
+                }
+            }
+
+            if (newAssets.Count > 0)
+            {
+                await mediator.Send(new InsertAssetsRankingCommand(newAssets));
+            }
+        }
+    }
+}
