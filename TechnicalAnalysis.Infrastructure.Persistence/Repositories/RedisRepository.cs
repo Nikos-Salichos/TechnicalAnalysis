3d0e7f3c7d9c5d989f12be2b464a40fe1e478469
Nikos S <niksal1991@gmail.com>
2024-03-21 08:42:01 +0200

    Changed RedisRepository

--- TechnicalAnalysis.Infrastructure.Persistence/Repositories/RedisRepository.cs
+++ TechnicalAnalysis.Infrastructure.Persistence/Repositories/RedisRepository.cs
@@ -55,7 +55,5 @@ namespace TechnicalAnalysis.Infrastructure.Persistence.Repositories
                 SlidingExpiration = slidingExpireTime ?? timeUntilMidnightUtc
             };
         }
-
-
     }
 }
