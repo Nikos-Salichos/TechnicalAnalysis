0bd939ec92bc6626c739d35d5646e14a564fdca3
Nikos S <niksal1991@gmail.com>
2024-03-23 08:22:15 +0200

    Added handler

---
+++ TechnicalAnalysis.Application/Mediatr/Handlers/Get/GetStockFearAndGreedIndexHandler.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using TechnicalAnalysis.Application.Mediatr.Queries;
+using TechnicalAnalysis.CommonModels.BusinessModels;
+using TechnicalAnalysis.Domain.Interfaces.Infrastructure;
+
+namespace TechnicalAnalysis.Application.Mediatr.Handlers.Get
+{
+    public class GetStockFearAndGreedIndexHandler(IPostgreSqlRepository repository) : IRequestHandler<GetStockFearAndGreedIndexQuery, IEnumerable<StockFearAndGreedDomain>>
+    {
+        public async Task<IEnumerable<StockFearAndGreedDomain>> Handle(GetStockFearAndGreedIndexQuery getCryptoFearAndGreedIndexQuery, CancellationToken cancellationToken)
+        {
+            var result = await repository.GetStockFearAndGreedIndexAsync();
+            if (result.HasError)
+            {
+                return [];
+            }
+            return result.SuccessValue;
+        }
+    }
+}
