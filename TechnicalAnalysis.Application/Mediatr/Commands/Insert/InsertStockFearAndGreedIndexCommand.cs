743b964e58a5f78e2c153ec3e377fbb14f0ecec5
Nikos S <niksal1991@gmail.com>
2024-03-23 08:22:01 +0200

    Added command

---
+++ TechnicalAnalysis.Application/Mediatr/Commands/Insert/InsertStockFearAndGreedIndexCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using TechnicalAnalysis.CommonModels.BusinessModels;
+
+namespace TechnicalAnalysis.Application.Mediatr.Commands.Insert
+{
+    public class InsertStockFearAndGreedIndexCommand(StockFearAndGreedDomain stockFearAndGreedDatas) : IRequest
+    {
+        public StockFearAndGreedDomain StockFearAndGreedEntities { get; } = stockFearAndGreedDatas;
+    }
+}
