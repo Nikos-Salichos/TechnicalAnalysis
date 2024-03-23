5a506b3199e2049e8b3d8a41f266cca3f425b32e
Nikos S <niksal1991@gmail.com>
2024-03-23 08:22:21 +0200

    Added Handler

---
+++ TechnicalAnalysis.Application/Mediatr/Handlers/Insert/InsertStockFearAndGreedIndexHandler.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using TechnicalAnalysis.Application.Mediatr.Commands.Insert;
+using TechnicalAnalysis.Domain.Interfaces.Infrastructure;
+
+namespace TechnicalAnalysis.Application.Mediatr.Handlers.Insert
+{
+    public class InsertStockFearAndGreedIndexHandler(IPostgreSqlRepository repository) : IRequestHandler<InsertStockFearAndGreedIndexCommand>
+    {
+        public async Task Handle(InsertStockFearAndGreedIndexCommand request, CancellationToken cancellationToken)
+        {
+            await repository.InsertStockFearAndGreedIndex(request.StockFearAndGreedEntities);
+        }
+    }
+}
