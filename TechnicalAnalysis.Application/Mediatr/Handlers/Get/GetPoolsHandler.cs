a2cd6b5485bb28226698fb38d4855f8ebe803c5a
Nikos S <niksal1991@gmail.com>
2024-03-23 08:22:10 +0200

    Changed handler

--- TechnicalAnalysis.Application/Mediatr/Handlers/Get/GetPoolsHandler.cs
+++ TechnicalAnalysis.Application/Mediatr/Handlers/Get/GetPoolsHandler.cs
@@ -5,14 +5,14 @@ using TechnicalAnalysis.Domain.Interfaces.Infrastructure;
 
 namespace TechnicalAnalysis.Application.Mediatr.Handlers.Get
 {
-    public class GetPoolsHandler(IPostgreSqlRepository repository) : IRequestHandler<GetPoolsQuery, IEnumerable<Pool>>
+    public class GetPoolsHandler(IPostgreSqlRepository repository) : IRequestHandler<GetPoolsQuery, IEnumerable<PoolEntity>>
     {
-        public async Task<IEnumerable<Pool>> Handle(GetPoolsQuery getPoolsQuery, CancellationToken cancellationToken)
+        public async Task<IEnumerable<PoolEntity>> Handle(GetPoolsQuery getPoolsQuery, CancellationToken cancellationToken)
         {
             var result = await repository.GetPoolsAsync();
             if (result.HasError)
             {
-                return Enumerable.Empty<Pool>();
+                return Enumerable.Empty<PoolEntity>();
             }
             return result.SuccessValue;
         }
