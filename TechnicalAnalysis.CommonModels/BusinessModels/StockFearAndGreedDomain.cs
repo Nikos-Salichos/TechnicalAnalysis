054b3648b63e2c3d685f6555cc1ff241101b9ab5
Nikos S <niksal1991@gmail.com>
2024-03-24 11:52:19 +0200

    Changed domain

---
+++ TechnicalAnalysis.CommonModels/BusinessModels/StockFearAndGreedDomain.cs
@@ -0,0 +1,11 @@
+﻿namespace TechnicalAnalysis.CommonModels.BusinessModels
+{
+    public class StockFearAndGreedDomain
+    {
+        public string Value { get; init; } = string.Empty;
+
+        public string ValueClassification { get; init; } = string.Empty;
+
+        public DateTime DateTime { get; init; }
+    }
+}
