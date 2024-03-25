1a949cde2d7774409eb4f3413f674a0e0f51d2d6
Nikos S <niksal1991@gmail.com>
2024-03-25 09:10:14 +0200

    Changed settings

--- TechnicalAnalysis.Infrastructure.Gateway/Program.cs
+++ TechnicalAnalysis.Infrastructure.Gateway/Program.cs
@@ -3,8 +3,8 @@ using TechnicalAnalysis.Infrastructure.Gateway;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -15,10 +15,23 @@ var proxyBuilder = builder.Services.AddReverseProxy();
 // Initialize the reverse proxy from the "ReverseProxy" section of configuration
 proxyBuilder.LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+//builder.Services.AddLettuceEncrypt();
+
+builder.Services.AddCors(o => o.AddPolicy("AllowAll", builder =>
+{
+    builder.AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+}));
+
 var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
+
+app.UseCors("AllowAll");
+
+app.UseHsts();
 
 app.UseHttpsRedirection();
 
