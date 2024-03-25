0e3c47cd07b7f7def0bc4a98971843d4a96b40e9
Nikos S <niksal1991@gmail.com>
2024-03-25 09:09:30 +0200

    Changed httpclient

--- TechnicalAnalysis.Infrastructure.Adapters/HttpClients/AlpacaHttpClient.cs
+++ TechnicalAnalysis.Infrastructure.Adapters/HttpClients/AlpacaHttpClient.cs
@@ -15,24 +15,15 @@ namespace TechnicalAnalysis.Infrastructure.Adapters.HttpClients
 
         public async Task<IResult<IMultiPage<IBar>, string>> GetAlpacaData(string pairName, DateTime fromDateTime, DateTime toDateTime, BarTimeFrame barTimeFrame)
         {
-            try
+            logger.LogInformation("Method {Method}, pairName {pairName}, toDateTime {toDateTime}, barTimeFrame {barTimeFrame} ", nameof(GetAlpacaData), pairName, toDateTime, barTimeFrame);
+            var alpacaDataClient = Environments.Paper.GetAlpacaDataClient(new SecretKey(alpacaSettings.CurrentValue.ApiKey, alpacaSettings.CurrentValue.ApiSecret));
+            HistoricalBarsRequest historicalBarsRequest = new(pairName, fromDateTime, toDateTime, barTimeFrame)
             {
-                logger.LogInformation("Method {Method}, pairName {pairName}, toDateTime {toDateTime}, barTimeFrame {barTimeFrame} ", nameof(GetAlpacaData), pairName, toDateTime, barTimeFrame);
-                var alpacaDataClient = Environments.Paper.GetAlpacaDataClient(new SecretKey(alpacaSettings.CurrentValue.ApiKey, alpacaSettings.CurrentValue.ApiSecret));
-                HistoricalBarsRequest historicalBarsRequest = new(pairName, fromDateTime, toDateTime, barTimeFrame)
-                {
-                    Adjustment = Adjustment.SplitsAndDividends
-                };
-                var stockData = await _retryPolicy.ExecuteAsync(() => alpacaDataClient.GetHistoricalBarsAsync(historicalBarsRequest));
-                logger.LogInformation("deserializedData '{@stockData}' ", stockData);
-                return Result<IMultiPage<IBar>, string>.Success(stockData);
-            }
-            catch (Exception exception)
-            {
-                logger.LogError("Method {Method}, exception {exception}", nameof(GetAlpacaData), exception);
-                return Result<IMultiPage<IBar>, string>.Fail(exception.ToString());
-            }
+                Adjustment = Adjustment.SplitsAndDividends
+            };
+            var stockData = await _retryPolicy.ExecuteAsync(() => alpacaDataClient.GetHistoricalBarsAsync(historicalBarsRequest));
+            logger.LogInformation("deserializedData '{@stockData}' ", stockData);
+            return Result<IMultiPage<IBar>, string>.Success(stockData);
         }
-
     }
 }
