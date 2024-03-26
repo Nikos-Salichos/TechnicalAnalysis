957b82c3e0fb30e03034a84eef964aab08204f08
Nikos S <niksal1991@gmail.com>
2024-03-26 08:42:59 +0200

    Changed interface

--- TechnicalAnalysis.Domain/Interfaces/Infrastructure/IPostgreSqlRepository.cs
+++ TechnicalAnalysis.Domain/Interfaces/Infrastructure/IPostgreSqlRepository.cs
@@ -8,21 +8,23 @@ namespace TechnicalAnalysis.Domain.Interfaces.Infrastructure
     public interface IPostgreSqlRepository
     {
         Task<IResult<IEnumerable<CryptoFearAndGreedData>, string>> GetCryptoFearAndGreedIndexAsync();
+        Task<IResult<IEnumerable<StockFearAndGreedDomain>, string>> GetStockFearAndGreedIndexAsync();
         Task<IResult<IEnumerable<ProviderSynchronization>, string>> GetProvidersAsync();
         Task<IResult<IEnumerable<Candlestick>, string>> GetCandlesticksAsync();
         Task<IResult<IEnumerable<Pair>, string>> GetPairsAsync();
         Task<IResult<IEnumerable<Asset>, string>> GetAssetsAsync();
         Task<IResult<IEnumerable<AssetRanking>, string>> GetCoinPaprikaAssetsAsync();
-        Task<IResult<IEnumerable<Pool>, string>> GetPoolsAsync();
+        Task<IResult<IEnumerable<PoolEntity>, string>> GetPoolsAsync();
         Task<IResult<IEnumerable<DexCandlestick>, string>> GetDexCandlesticksAsync();
         Task InsertCryptoFearAndGreedIndex(IEnumerable<CryptoFearAndGreedData> indexes);
+        Task InsertStockFearAndGreedIndex(StockFearAndGreedDomain stockFearAndGreedEntity);
         Task InsertPairsAsync(IEnumerable<Pair> pairs);
         Task InsertCandlesticksAsync(IEnumerable<Candlestick> candlesticks);
         Task<IResult<string, string>> InsertAssetsAsync(IEnumerable<Asset> assets);
         Task<IResult<string, string>> InsertCoinPaprikaAssetsAsync(IEnumerable<AssetRanking> assets);
         Task UpdateProviderPairAssetSyncInfoAsync(ProviderPairAssetSyncInfo providerPairAssetSyncInfo);
         Task UpdateProviderCandlestickSyncInfoAsync(ProviderCandlestickSyncInfo providerCandlestickSyncInfo);
-        Task InsertPoolsAsync(IEnumerable<Pool> pools);
+        Task InsertPoolsAsync(IEnumerable<PoolEntity> pools);
         Task InsertDexCandlesticksAsync(IEnumerable<DexCandlestick> candlesticks);
         Task DeleteEntitiesByIdsAsync<T>(IEnumerable<long> ids, string tableName);
     }
