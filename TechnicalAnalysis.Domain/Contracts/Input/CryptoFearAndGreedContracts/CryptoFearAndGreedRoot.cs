5c151975ab8e56280ee0d790d102135dcedb6459
Nikos S <niksal1991@gmail.com>
2024-03-26 08:44:16 +0200

    Added root model

---
+++ TechnicalAnalysis.Domain/Contracts/Input/CryptoFearAndGreedContracts/CryptoFearAndGreedRoot.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TechnicalAnalysis.Domain.Contracts.Input.CryptoFearAndGreedContracts
+{
+    public class CryptoFearAndGreedRoot
+    {
+        [JsonPropertyName("data")]
+        public IEnumerable<CryptoFearAndGreedData> CryptoFearAndGreedDatas { get; set; } = new List<CryptoFearAndGreedData>();
+    }
+}
