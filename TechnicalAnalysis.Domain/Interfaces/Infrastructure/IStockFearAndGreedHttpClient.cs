7a14ffcee5a65c23dd9c5ca874b58d48b3a37e46
Nikos S <niksal1991@gmail.com>
2024-03-26 08:43:03 +0200

    Changed interface

---
+++ TechnicalAnalysis.Domain/Interfaces/Infrastructure/IStockFearAndGreedHttpClient.cs
@@ -0,0 +1,10 @@
+﻿using TechnicalAnalysis.Domain.Contracts.Input.StockFearAndGreedContracts;
+using TechnicalAnalysis.Domain.Interfaces.Utilities;
+
+namespace TechnicalAnalysis.Domain.Interfaces.Infrastructure
+{
+    public interface IStockFearAndGreedHttpClient
+    {
+        Task<IResult<StockFearAndGreedRoot, string>> GetStockFearAndGreedIndex();
+    }
+}
