6e60cac0f17c4f842c22b246a90cab700be018ae
Nikos S <niksal1991@gmail.com>
2024-03-27 08:35:41 +0200

    Added extra log

---
+++ TechnicalAnalysis.Infrastructure.Host/Serilog/LogRequestEnricher.cs
@@ -0,0 +1,12 @@
+﻿using Serilog;
+
+namespace TechnicalAnalysis.Infrastructure.Host.Serilog
+{
+    public static class LogRequestEnricher
+    {
+        public static void LogAdditionalInfo(IDiagnosticContext diagnosticContext, HttpContext httpContext)
+        {
+            diagnosticContext.Set("ClientIP", httpContext.Connection.RemoteIpAddress?.ToString());
+        }
+    }
+}
