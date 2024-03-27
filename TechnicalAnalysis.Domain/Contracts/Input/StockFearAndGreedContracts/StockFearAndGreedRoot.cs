cbeeb5c23253904887904040477664fc7f6e67e1
Nikos S <niksal1991@gmail.com>
2024-03-27 08:34:30 +0200

    Changed root

---
+++ TechnicalAnalysis.Domain/Contracts/Input/StockFearAndGreedContracts/StockFearAndGreedRoot.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace TechnicalAnalysis.Domain.Contracts.Input.StockFearAndGreedContracts
+{
+    public class StockFearAndGreedRoot
+    {
+        [JsonPropertyName("fgi")]
+        public StockFearAndGreedData StockFearAndGreedData { get; init; }
+
+        [JsonPropertyName("lastUpdated")]
+        public StockFearAndGreedLastUpdated StockFearAndGreedLastUpdated { get; set; }
+    }
+}
