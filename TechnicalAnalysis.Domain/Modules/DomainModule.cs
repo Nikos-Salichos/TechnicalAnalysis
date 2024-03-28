fab5cde45f794da82858bdd6723f3af70adff5e6
Nikos S <niksal1991@gmail.com>
2024-03-28 14:59:40 +0200

    Changed module

---
+++ TechnicalAnalysis.Domain/Modules/DomainModule.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TechnicalAnalysis.Domain.Settings;
+
+namespace TechnicalAnalysis.Domain.Modules
+{
+    public static class DomainModule
+    {
+        public static void AddDomainModule(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddOptions<AlpacaSetting>().Bind(configuration.GetSection("AlpacaSettings"));
+            services.AddOptions<BinanceSetting>().Bind(configuration.GetSection("BinanceSettings"));
+            services.AddOptions<DexSetting>().Bind(configuration.GetSection("DexSettings"));
+            services.AddOptions<CoinMarketCapSetting>().Bind(configuration.GetSection("CoinMarketCap"));
+            services.AddOptions<CoinRankingSetting>().Bind(configuration.GetSection("CoinRanking"));
+            services.AddOptions<RapidApiSetting>().Bind(configuration.GetSection("RapidApi"));
+        }
+    }
+}
