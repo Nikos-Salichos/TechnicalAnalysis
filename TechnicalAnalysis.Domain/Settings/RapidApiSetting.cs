327d4a8b23b91dab56a218a8a56015c2b9049973
Nikos S <niksal1991@gmail.com>
2024-03-28 14:59:52 +0200

    Added RapidApiSetting

---
+++ TechnicalAnalysis.Domain/Settings/RapidApiSetting.cs
@@ -0,0 +1,9 @@
+﻿namespace TechnicalAnalysis.Domain.Settings
+{
+    public class RapidApiSetting
+    {
+        public string StockFearAndGreedUri { get; init; } = string.Empty;
+        public string StockFearAndGreedHost { get; init; } = string.Empty;
+        public string StockFearAndGreedApiKey { get; init; } = string.Empty;
+    }
+}
