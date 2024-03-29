1e97253620a47fa1cf5f339581ca994b60cd84da
Nikos S <niksal1991@gmail.com>
2024-03-29 10:22:30 +0200

    Changed repository

--- TechnicalAnalysis.Infrastructure.Persistence/Repositories/PostgreSqlRepository.cs
+++ TechnicalAnalysis.Infrastructure.Persistence/Repositories/PostgreSqlRepository.cs
@@ -243,29 +243,21 @@ namespace TechnicalAnalysis.Infrastructure.Persistence.Repositories
 
         public async Task<IResult<string, string>> InsertCoinPaprikaAssetsAsync(IEnumerable<AssetRanking> assets)
         {
-            try
+            await using var dbConnection = new NpgsqlConnection(_connectionStringKey);
+            await dbConnection.OpenAsync();
+            await using var writer = await dbConnection.BeginBinaryImportAsync("COPY \"CoinPaprikaAssets\" (\"Name\", \"Symbol\", \"CreatedDate\", \"AssetType\", \"Provider\") FROM STDIN BINARY");
+            foreach (var asset in assets)
             {
-                await using var dbConnection = new NpgsqlConnection(_connectionStringKey);
-                await dbConnection.OpenAsync();
-                await using var writer = await dbConnection.BeginBinaryImportAsync("COPY \"CoinPaprikaAssets\" (\"Name\", \"Symbol\", \"CreatedDate\", \"AssetType\", \"Provider\") FROM STDIN BINARY");
-                foreach (var asset in assets)
-                {
-                    await writer.StartRowAsync();
-                    await WriteParameter(writer, asset.Name);
-                    await WriteParameter(writer, asset.Symbol);
-                    await WriteParameter(writer, asset.CreatedDate);
-                    await WriteParameter(writer, (long)asset.AssetType);
-                    await WriteParameter(writer, (long)asset.DataProvider);
-                }
+                await writer.StartRowAsync();
+                await WriteParameter(writer, asset.Name);
+                await WriteParameter(writer, asset.Symbol);
+                await WriteParameter(writer, asset.CreatedDate);
+                await WriteParameter(writer, (long)asset.AssetType);
+                await WriteParameter(writer, (long)asset.DataProvider);
+            }
 
-                await writer.CompleteAsync();
-                return Result<string, string>.Success(string.Empty);
-            }
-            catch (Exception exception)
-            {
-                logger.LogError("Exception{@exception}", exception);
-                return Result<string, string>.Fail(exception.ToString());
-            }
+            await writer.CompleteAsync();
+            return Result<string, string>.Success(string.Empty);
         }
 
         public async Task InsertCandlesticksAsync(IEnumerable<Candlestick> candlesticks)
