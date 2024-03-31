bc33ac57b861747001f99e2a42421847ab1bf366
Nikos S <niksal1991@gmail.com>
2024-03-31 09:51:18 +0300

    Changed command

--- TechnicalAnalysis.Application/Mediatr/Commands/Delete/DeleteDexCandlesticksCommand.cs
+++ TechnicalAnalysis.Application/Mediatr/Commands/Delete/DeleteDexCandlesticksCommand.cs
@@ -2,7 +2,7 @@
 
 namespace TechnicalAnalysis.Application.Mediatr.Commands.Delete
 {
-    public class DeleteDexCandlesticksCommand(IEnumerable<long> ids) : IRequest
+    public class DeleteDexCandlesticksCommand(IEnumerable<long> ids) : IRequest<bool>
     {
         public IEnumerable<long> Ids { get; } = ids;
     }
