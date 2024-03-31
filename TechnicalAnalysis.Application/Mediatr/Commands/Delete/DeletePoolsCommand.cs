0759b3e59c0e9a1875fca06261a52b681c9e1b20
Nikos S <niksal1991@gmail.com>
2024-03-31 09:51:21 +0300

    Changed command

--- TechnicalAnalysis.Application/Mediatr/Commands/Delete/DeletePoolsCommand.cs
+++ TechnicalAnalysis.Application/Mediatr/Commands/Delete/DeletePoolsCommand.cs
@@ -2,7 +2,7 @@
 
 namespace TechnicalAnalysis.Application.Mediatr.Commands.Delete
 {
-    public class DeletePoolsCommand(IEnumerable<long> ids) : IRequest
+    public class DeletePoolsCommand(IEnumerable<long> ids) : IRequest<bool>
     {
         public IEnumerable<long> Ids { get; } = ids;
     }
