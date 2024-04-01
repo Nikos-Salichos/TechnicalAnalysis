9bfc4dce013ec6aece8af7a994dd09184f2820ab
Nikos S <niksal1991@gmail.com>
2024-04-01 08:32:13 +0300

    Changed middleware

--- TechnicalAnalysis.Infrastructure.Host/Middleware/SecureHeadersMiddleware.cs
+++ TechnicalAnalysis.Infrastructure.Host/Middleware/SecureHeadersMiddleware.cs
@@ -9,7 +9,9 @@
             AddHeaderIfNotExists(httpContext, "X-Xss-Protection", "1; mode=block");
             AddHeaderIfNotExists(httpContext, "Referrer-Policy", "no-referrer");
             AddHeaderIfNotExists(httpContext, "Content-Security-Policy", "default-src 'self';");
-
+            AddHeaderIfNotExists(httpContext, "Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload");
+            AddHeaderIfNotExists(httpContext, "Permissions-Policy", "geolocation (self), camera (self)");
+            AddHeaderIfNotExists(httpContext, "Feature-Policy", "geolocation 'self', camera 'self'");
             await requestDelegate(httpContext);
         }
 
@@ -17,6 +19,5 @@
         {
             context.Response.Headers.TryAdd(key, value);
         }
-
     }
 }
