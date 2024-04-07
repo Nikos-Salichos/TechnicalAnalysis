e5d30d5c2af32530365cb6e526a52ed90e6708fa
Nikos S <niksal1991@gmail.com>
2024-04-07 15:25:15 +0300

    Simplify code

--- TechnicalAnalysis.CommonModels/Indicators/Basic/RateOfChange.cs
+++ TechnicalAnalysis.CommonModels/Indicators/Basic/RateOfChange.cs
@@ -2,12 +2,9 @@
 
 namespace TechnicalAnalysis.CommonModels.Indicators.Basic
 {
-    public class RateOfChange : BaseIndicator
+    public class RateOfChange(long candlestickId) : BaseIndicator(candlestickId)
     {
         public long Period { get; init; }
         public double? Value { get; init; }
-        public RateOfChange(long candlestickId) : base(candlestickId)
-        {
-        }
     }
 }
