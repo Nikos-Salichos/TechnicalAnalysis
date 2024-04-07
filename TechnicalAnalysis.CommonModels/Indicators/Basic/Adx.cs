ffef8c6c2e5c6e65b232b39e7bce682c3d75a4af
Nikos S <niksal1991@gmail.com>
2024-04-07 15:25:13 +0300

    Simplify code

--- TechnicalAnalysis.CommonModels/Indicators/Basic/Adx.cs
+++ TechnicalAnalysis.CommonModels/Indicators/Basic/Adx.cs
@@ -2,14 +2,11 @@
 
 namespace TechnicalAnalysis.CommonModels.Indicators.Basic
 {
-    public class Adx : BaseIndicator
+    public class Adx(long candlestickId) : BaseIndicator(candlestickId)
     {
         public double? PlusDi { get; init; }
         public double? MinusDi { get; init; }
         public double? Value { get; init; }
         public double? Adxr { get; init; }
-        public Adx(long candlestickId) : base(candlestickId)
-        {
-        }
     }
 }
