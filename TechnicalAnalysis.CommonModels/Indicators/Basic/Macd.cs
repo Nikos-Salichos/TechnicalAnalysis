119d1d86e35f0b9cb55d9ab0929523e3ec02cbc8
Nikos S <niksal1991@gmail.com>
2024-04-07 15:25:19 +0300

    Simplify code

--- TechnicalAnalysis.CommonModels/Indicators/Basic/Macd.cs
+++ TechnicalAnalysis.CommonModels/Indicators/Basic/Macd.cs
@@ -2,23 +2,13 @@
 
 namespace TechnicalAnalysis.CommonModels.Indicators.Basic
 {
-    public class Macd : BaseIndicator
+    public class Macd(long candlestickId, double? macdValue, double? signal, double? histogram, double? fastEma, double? slowEma) : BaseIndicator(candlestickId)
     {
-        public double? MacdValue { get; init; }
-        public double? Signal { get; init; }
-        public double? Histogram { get; init; }
-        public double? FastEma { get; init; }
-        public double? SlowEma { get; init; }
-
-        public Macd(long candlestickId, double? macdValue, double? signal, double? histogram, double? fastEma, double? slowEma)
-            : base(candlestickId)
-        {
-            MacdValue = macdValue;
-            Signal = signal;
-            Histogram = histogram;
-            FastEma = fastEma;
-            SlowEma = slowEma;
-        }
+        public double? MacdValue { get; init; } = macdValue;
+        public double? Signal { get; init; } = signal;
+        public double? Histogram { get; init; } = histogram;
+        public double? FastEma { get; init; } = fastEma;
+        public double? SlowEma { get; init; } = slowEma;
 
         public static Macd Create(long candlestickId, double? macdValue, double? signal, double? histogram, double? fastEma, double? slowEma)
         {
