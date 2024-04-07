e2e2dce362c94cdc7f25ad7ab37da476cd40341c
Nikos S <niksal1991@gmail.com>
2024-04-07 15:25:25 +0300

    Simplify code

--- TechnicalAnalysis.CommonModels/Indicators/Basic/Highest.cs
+++ TechnicalAnalysis.CommonModels/Indicators/Basic/Highest.cs
@@ -3,14 +3,10 @@ using TechnicalAnalysis.CommonModels.Enums;
 
 namespace TechnicalAnalysis.CommonModels.Indicators.Basic
 {
-    public class Highest : BaseIndicator
+    public class Highest(long candlestickId) : BaseIndicator(candlestickId)
     {
         public long Period { get; init; }
         public decimal? Value { get; set; }
         public PriceType PriceType { get; init; }
-
-        public Highest(long candlestickId) : base(candlestickId)
-        {
-        }
     }
 }
