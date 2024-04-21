85c3ee0983ed58c9100b482c253da2288dfa7bfe
Nikos S <niksal1991@gmail.com>
2024-04-21 16:15:49 +0300

    Updated Polly Policy

--- TechnicalAnalysis.Domain/Utilities/PollyPolicy.cs
+++ TechnicalAnalysis.Domain/Utilities/PollyPolicy.cs
@@ -12,23 +12,30 @@ namespace TechnicalAnalysis.Domain.Utilities
 
         public IAsyncPolicy<T> CreatePolicies<T>(int retries, TimeSpan timeSpan)
         {
-            var retryDelays = Backoff.DecorrelatedJitterBackoffV2(medianFirstRetryDelay: TimeSpan.FromSeconds(5), retryCount: retries).ToArray();
+            var retryDelays = DecorrelatedJitterBackoffV2(retries);
 
-            return Policy.WrapAsync(
-                Policy.TimeoutAsync<T>(timeSpan, TimeoutStrategy.Optimistic, onTimeoutAsync: (context, timespan, task) =>
+            var timeoutPolicy = Policy.TimeoutAsync<T>(
+                timeSpan, TimeoutStrategy.Optimistic, onTimeoutAsync: (context, timespan, task) =>
                 {
-                    _logger.LogError("Timeout occurred after {timespan}. Context: {context}", timespan, context);
+                    _logger.LogError("Timeout occurred after {TimeSpan}. Context: {Context}", timespan, context);
                     return Task.CompletedTask;
-                }),
-                Policy<T>.Handle<Exception>()
-                    .WaitAndRetryAsync(retryDelays,
-                        onRetry: (exception, delay, retryAttempt, _) =>
-                        {
-                            _logger.LogError("Retry attempt {retryAttempt} of {retries}. Delaying for {delay.TotalSeconds} seconds. Exception: {exception}",
-                                retryAttempt, retries, delay.TotalSeconds, exception.Exception);
-                        })
-            );
+                });
+
+            var retryPolicy = Policy<T>.Handle<Exception>().WaitAndRetryAsync(
+                retryDelays,
+                onRetry: (exception, delay, retryAttempt, context) =>
+                {
+                    _logger.LogError("Retry attempt {RetryAttempt} of {Retries}. Delaying for {Delay} seconds. Exception: {Exception}",
+                        retryAttempt, retries, delay.TotalSeconds, exception);
+                });
+
+            return Policy.WrapAsync(timeoutPolicy, retryPolicy);
         }
 
+        private static TimeSpan[] DecorrelatedJitterBackoffV2(int retries)
+        {
+            return Backoff.DecorrelatedJitterBackoffV2(
+                medianFirstRetryDelay: TimeSpan.FromSeconds(5), retryCount: retries).ToArray();
+        }
     }
 }
