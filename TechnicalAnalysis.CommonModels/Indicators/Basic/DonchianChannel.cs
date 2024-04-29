ceee49d291d86581c1a0c0d985287a2db874be86
Nikos S <niksal1991@gmail.com>
2024-04-29 08:41:46 +0300

    Changed Donchian Channel

--- TechnicalAnalysis.CommonModels/Indicators/Basic/DonchianChannel.cs
+++ TechnicalAnalysis.CommonModels/Indicators/Basic/DonchianChannel.cs
@@ -2,17 +2,12 @@
 
 namespace TechnicalAnalysis.CommonModels.Indicators.Basic
 {
-    public class DonchianChannel(long candlestickId, long period, decimal? upperBand, decimal? centerline, decimal? lowerBand, decimal? width) : BaseIndicator(candlestickId)
+    public class DonchianChannel(long candlestickId) : BaseIndicator(candlestickId)
     {
-        public long Period { get; init; } = period;
-        public decimal? UpperBand { get; init; } = upperBand;
-        public decimal? Centerline { get; init; } = centerline;
-        public decimal? LowerBand { get; init; } = lowerBand;
-        public decimal? Width { get; init; } = width;
-
-        public static DonchianChannel Create(long candlestickId, long period, decimal? upperBand, decimal? centerline, decimal? lowerBand, decimal? width)
-        {
-            return new DonchianChannel(candlestickId, period, upperBand, centerline, lowerBand, width);
-        }
+        public required long Period { get; init; }
+        public required decimal? UpperBand { get; init; }
+        public required decimal? Centerline { get; init; }
+        public required decimal? LowerBand { get; init; }
+        public required decimal? Width { get; init; }
     }
 }
