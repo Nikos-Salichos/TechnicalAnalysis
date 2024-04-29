0dba7aac9cec86607cea68173821b2f8dba7253c
Nikos S <niksal1991@gmail.com>
2024-04-29 08:41:34 +0300

    Changed Adx

--- TechnicalAnalysis.CommonModels/Indicators/Basic/Adx.cs
+++ TechnicalAnalysis.CommonModels/Indicators/Basic/Adx.cs
@@ -4,9 +4,9 @@ namespace TechnicalAnalysis.CommonModels.Indicators.Basic
 {
     public class Adx(long candlestickId) : BaseIndicator(candlestickId)
     {
-        public double? PlusDi { get; init; }
-        public double? MinusDi { get; init; }
-        public double? Value { get; init; }
-        public double? Adxr { get; init; }
+        public required double? PlusDi { get; init; }
+        public required double? MinusDi { get; init; }
+        public required double? Value { get; init; }
+        public required double? Adxr { get; init; }
     }
 }
