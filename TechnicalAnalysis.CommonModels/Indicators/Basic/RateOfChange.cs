3586b6901b104ef78feb47bda4c7184bfc85afef
Nikos S <niksal1991@gmail.com>
2024-04-30 07:44:52 +0300

    Changed model

--- TechnicalAnalysis.CommonModels/Indicators/Basic/RateOfChange.cs
+++ TechnicalAnalysis.CommonModels/Indicators/Basic/RateOfChange.cs
@@ -4,7 +4,7 @@ namespace TechnicalAnalysis.CommonModels.Indicators.Basic
 {
     public class RateOfChange(long candlestickId) : BaseIndicator(candlestickId)
     {
-        public long Period { get; init; }
-        public double? Value { get; init; }
+        public required long Period { get; init; }
+        public required double? Value { get; init; }
     }
 }
