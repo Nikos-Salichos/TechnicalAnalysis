2ada7f299be64d9391202be09c004bcc388f9b25
Nikos S <niksal1991@gmail.com>
2024-04-30 07:44:41 +0300

    Changed model

--- TechnicalAnalysis.CommonModels/Indicators/Basic/BollingerBand.cs
+++ TechnicalAnalysis.CommonModels/Indicators/Basic/BollingerBand.cs
@@ -4,13 +4,13 @@ namespace TechnicalAnalysis.CommonModels.Indicators.Basic
 {
     public class BollingerBand(long candlestickId) : BaseIndicator(candlestickId)
     {
-        public long Period { get; init; }
-        public int? StandardDeviation { get; init; } = 2;
-        public decimal? UpperBand { get; init; }
-        public decimal? MiddleBand { get; init; }
-        public decimal? LowerBand { get; init; }
-        public decimal? PercentageBandwidth { get; init; }
-        public decimal? Width { get; init; }
+        public required long Period { get; init; }
+        public required int? StandardDeviation { get; init; } = 2;
+        public required decimal? UpperBand { get; init; }
+        public required decimal? MiddleBand { get; init; }
+        public required decimal? LowerBand { get; init; }
+        public required decimal? PercentageBandwidth { get; init; }
+        public required decimal? Width { get; init; }
         public bool? OpenPriceOutOfBollinger { get; init; }
         public bool? WholeCandlestickOutOfBollinger { get; init; }
         public bool? LowPriceOutOfBollinger { get; init; }
