385e4fd158bc8b4fe2fe9623423d296ba59a4a11
Nikos S <niksal1991@gmail.com>
2024-04-30 07:45:02 +0300

    Changed model

--- TechnicalAnalysis.CommonModels/Indicators/Basic/StandardPivotPoint.cs
+++ TechnicalAnalysis.CommonModels/Indicators/Basic/StandardPivotPoint.cs
@@ -5,13 +5,13 @@ namespace TechnicalAnalysis.CommonModels.Indicators.Basic
 {
     public class StandardPivotPoint(long candlestickId) : BaseIndicator(candlestickId)
     {
-        public Timeframe Timeframe { get; init; }
-        public decimal? PivotPoint { get; init; }
-        public decimal? Support1 { get; init; }
-        public decimal? Support2 { get; init; }
-        public decimal? Support3 { get; init; }
-        public decimal? Resistance1 { get; init; }
-        public decimal? Resistance2 { get; init; }
-        public decimal? Resistance3 { get; init; }
+        public required Timeframe Timeframe { get; init; }
+        public required decimal? PivotPoint { get; init; }
+        public required decimal? Support1 { get; init; }
+        public required decimal? Support2 { get; init; }
+        public required decimal? Support3 { get; init; }
+        public required decimal? Resistance1 { get; init; }
+        public required decimal? Resistance2 { get; init; }
+        public required decimal? Resistance3 { get; init; }
     }
 }
