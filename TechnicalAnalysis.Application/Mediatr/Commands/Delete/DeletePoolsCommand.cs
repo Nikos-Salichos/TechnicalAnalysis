dd891eb494f3c6f9f0755d5aa28c344f9a574e04
Nikos S <niksal1991@gmail.com>
2024-05-01 09:45:11 +0300

    Updated delete command

--- TechnicalAnalysis.Application/Mediatr/Commands/Delete/DeletePoolsCommand.cs
+++ TechnicalAnalysis.Application/Mediatr/Commands/Delete/DeletePoolsCommand.cs
@@ -2,8 +2,8 @@
 
 namespace TechnicalAnalysis.Application.Mediatr.Commands.Delete
 {
-    public class DeletePoolsCommand(IEnumerable<long> ids) : IRequest<bool>
+    public class DeletePoolsCommand(List<long> ids) : IRequest<bool>
     {
-        public IEnumerable<long> Ids { get; } = ids;
+        public List<long> Ids { get; } = ids;
     }
 }
