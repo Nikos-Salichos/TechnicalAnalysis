59337d01329f60aff7f6bf12f3454731b7ead035
Nikos S <niksal1991@gmail.com>
2024-05-01 09:45:36 +0300

    Updated insert command

--- TechnicalAnalysis.Application/Mediatr/Commands/Insert/InsertCryptoFearAndGreedIndexCommand.cs
+++ TechnicalAnalysis.Application/Mediatr/Commands/Insert/InsertCryptoFearAndGreedIndexCommand.cs
@@ -3,8 +3,8 @@ using TechnicalAnalysis.Domain.Contracts.Input.CryptoFearAndGreedContracts;
 
 namespace TechnicalAnalysis.Application.Mediatr.Commands.Insert
 {
-    public class InsertCryptoFearAndGreedIndexCommand(IEnumerable<CryptoFearAndGreedData> cryptoFearAndGreedDatas) : IRequest
+    public class InsertCryptoFearAndGreedIndexCommand(List<CryptoFearAndGreedData> cryptoFearAndGreedDatas) : IRequest
     {
-        public IEnumerable<CryptoFearAndGreedData> CryptoFearAndGreedDatas { get; } = cryptoFearAndGreedDatas;
+        public List<CryptoFearAndGreedData> CryptoFearAndGreedDatas { get; } = cryptoFearAndGreedDatas;
     }
 }
