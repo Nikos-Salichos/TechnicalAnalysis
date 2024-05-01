0fdc92ec83a25dd1f05474bd86d0aa1dc5d87d4a
Nikos S <niksal1991@gmail.com>
2024-05-01 09:45:19 +0300

    Updated insert command

--- TechnicalAnalysis.Application/Mediatr/Commands/Insert/InsertAssetsCommand.cs
+++ TechnicalAnalysis.Application/Mediatr/Commands/Insert/InsertAssetsCommand.cs
@@ -3,8 +3,8 @@ using TechnicalAnalysis.CommonModels.BusinessModels;
 
 namespace TechnicalAnalysis.Application.Mediatr.Commands.Insert
 {
-    public class InsertAssetsCommand(IEnumerable<Asset> assets) : IRequest
+    public class InsertAssetsCommand(List<Asset> assets) : IRequest
     {
-        public IEnumerable<Asset> Assets { get; } = assets;
+        public List<Asset> Assets { get; } = assets;
     }
 }
