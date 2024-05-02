e5b3491b20c8fcda9ee1f23b7bc85fc3bc794fb7
Nikos S <niksal1991@gmail.com>
2024-05-02 22:14:56 +0300

    Updated Handler

--- TechnicalAnalysis.Application/Mediatr/Handlers/Get/GetExchangesHandler.cs
+++ TechnicalAnalysis.Application/Mediatr/Handlers/Get/GetExchangesHandler.cs
@@ -5,15 +5,14 @@ using TechnicalAnalysis.Domain.Interfaces.Infrastructure;
 
 namespace TechnicalAnalysis.Application.Mediatr.Handlers.Get
 {
-    public class GetExchangesHandler(IPostgreSqlRepository repository)
-        : IRequestHandler<GetProviderSynchronizationQuery, IEnumerable<ProviderSynchronization>>
+    public class GetExchangesHandler(IPostgreSqlRepository repository) : IRequestHandler<GetProviderSynchronizationQuery, List<ProviderSynchronization>>
     {
-        public async Task<IEnumerable<ProviderSynchronization>> Handle(GetProviderSynchronizationQuery getProvidersQuery, CancellationToken cancellationToken)
+        public async Task<List<ProviderSynchronization>> Handle(GetProviderSynchronizationQuery getProvidersQuery, CancellationToken cancellationToken)
         {
             var providers = await repository.GetProvidersAsync();
             if (providers.HasError)
             {
-                return Enumerable.Empty<ProviderSynchronization>();
+                return [];
             }
             return providers.SuccessValue;
         }
