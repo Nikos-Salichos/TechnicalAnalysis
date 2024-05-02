e801154ab3e8bede295eb22ffbe4fc1a2c5748c3
Nikos S <niksal1991@gmail.com>
2024-05-02 09:05:12 +0300

    Changed mapper

--- TechnicalAnalysis.Application/Mappers/FromDomainToInputContract.cs
+++ TechnicalAnalysis.Application/Mappers/FromDomainToInputContract.cs
@@ -5,7 +5,7 @@ namespace TechnicalAnalysis.Application.Mappers
 {
     public static class FromDomainToInputContract
     {
-        public static List<BinancePair> ToContract(this IEnumerable<PairExtended> pairs)
+        public static List<BinancePair> ToContract(this List<PairExtended> pairs)
             => pairs.Select(p => p.ToContract()).ToList();
 
         private static BinanceCandlestick ToContract(this CandlestickExtended candlestick)
