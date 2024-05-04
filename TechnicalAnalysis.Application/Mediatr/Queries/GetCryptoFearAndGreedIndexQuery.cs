7bdb2cbb3035b398c89a98b4097a752aaf7a9480
Nikos S <niksal1991@gmail.com>
2024-05-04 08:09:41 +0300

    Updated Query

--- TechnicalAnalysis.Application/Mediatr/Queries/GetCryptoFearAndGreedIndexQuery.cs
+++ TechnicalAnalysis.Application/Mediatr/Queries/GetCryptoFearAndGreedIndexQuery.cs
@@ -3,7 +3,7 @@ using TechnicalAnalysis.Domain.Contracts.Input.CryptoFearAndGreedContracts;
 
 namespace TechnicalAnalysis.Application.Mediatr.Queries
 {
-    public class GetCryptoFearAndGreedIndexQuery : IRequest<IEnumerable<CryptoFearAndGreedData>>
+    public class GetCryptoFearAndGreedIndexQuery : IRequest<List<CryptoFearAndGreedData>>
     {
     }
 }
