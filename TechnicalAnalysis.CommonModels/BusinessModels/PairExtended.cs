d0c42931b8d0b6293b6450491955d768072764d0
Nikos S <niksal1991@gmail.com>
2024-05-05 20:27:45 +0300

    Updated domain model

--- TechnicalAnalysis.CommonModels/BusinessModels/PairExtended.cs
+++ TechnicalAnalysis.CommonModels/BusinessModels/PairExtended.cs
@@ -25,6 +25,7 @@ namespace TechnicalAnalysis.CommonModels.BusinessModels
         public long? NumberOfTrades { get; init; }
         public decimal? AllTimeHighPrice { get; set; }
         public decimal? AllTimeLowPrice { get; set; }
+        public ProductType ProductType { get; init; }
         public List<CandlestickExtended> Candlesticks { get; set; } = [];
 
         public bool Equals(PairExtended? other)
