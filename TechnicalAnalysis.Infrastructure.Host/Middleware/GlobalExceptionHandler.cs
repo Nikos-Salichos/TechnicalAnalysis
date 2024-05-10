3d119f5c3b44000e7a8feaab31048983d4a69b15
Nikos S <niksal1991@gmail.com>
2024-05-10 15:34:18 +0300

    Added GlobalExceptionHandler

---
+++ TechnicalAnalysis.Infrastructure.Host/Middleware/GlobalExceptionHandler.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TechnicalAnalysis.Infrastructure.Host.Middleware
+{
+    public class GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger) : IExceptionHandler
+    {
+        public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+        {
+            logger.LogError(exception, "Exception occurred: {Message}", exception.Message);
+
+            var problemDetails = new ProblemDetails
+            {
+                Title = "Application error",
+                Status = StatusCodes.Status500InternalServerError,
+                Detail = exception.Message,
+            };
+
+            httpContext.Response.StatusCode = problemDetails.Status.Value;
+            await httpContext.Response.WriteAsJsonAsync(problemDetails, cancellationToken);
+
+            return true;
+        }
+    }
+}
