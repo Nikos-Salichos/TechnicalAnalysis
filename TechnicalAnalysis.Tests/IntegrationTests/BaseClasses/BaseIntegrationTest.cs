ee41bf42158fe99d8b1067f4dc364336e2913c7f
Nikos S <niksal1991@gmail.com>
2024-05-10 15:34:41 +0300

    Changed folder structure

---
+++ TechnicalAnalysis.Tests/IntegrationTests/BaseClasses/BaseIntegrationTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TechnicalAnalysis.Domain.Interfaces.Infrastructure;
+
+namespace TechnicalAnalysis.Tests.IntegrationTests.BaseClasses
+{
+    public abstract class BaseIntegrationTest : IClassFixture<IntegrationTestWebAppFactory>, IDisposable
+    {
+        private readonly IServiceScope _serviceScope;
+        protected readonly IPostgreSqlRepository PostgreSqlRepository;
+
+        protected BaseIntegrationTest(IntegrationTestWebAppFactory factory)
+        {
+            _serviceScope = factory.Services.CreateScope();
+            PostgreSqlRepository = _serviceScope.ServiceProvider.GetRequiredService<IPostgreSqlRepository>();
+        }
+
+        public void Dispose()
+        {
+            _serviceScope?.Dispose();
+        }
+    }
+}
