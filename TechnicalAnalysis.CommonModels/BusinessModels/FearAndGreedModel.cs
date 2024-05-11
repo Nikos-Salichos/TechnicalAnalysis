5f203a221b239e88231308d641fa113ec6e162f3
Nikos S <niksal1991@gmail.com>
2024-05-11 18:14:37 +0300

    Added model

---
+++ TechnicalAnalysis.CommonModels/BusinessModels/FearAndGreedModel.cs
@@ -0,0 +1,13 @@
+﻿using TechnicalAnalysis.CommonModels.Enums;
+
+namespace TechnicalAnalysis.CommonModels.BusinessModels
+{
+    public class FearAndGreedModel
+    {
+        public string Value { get; init; } = string.Empty;
+
+        public ValueClassificationType ValueClassificationType { get; init; }
+
+        public DateTime DateTime { get; init; } = DateTime.UtcNow;
+    }
+}
