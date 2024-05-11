ede0724494075922c610bf8ce525d84cf02f65e4
Nikos S <niksal1991@gmail.com>
2024-05-11 18:13:52 +0300

    Changed handler

--- TechnicalAnalysis.Application/Mediatr/Handlers/Get/GetCryptoFearAndGreedIndexHandler.cs
+++ TechnicalAnalysis.Application/Mediatr/Handlers/Get/GetCryptoFearAndGreedIndexHandler.cs
@@ -1,13 +1,13 @@
 ﻿using MediatR;
 using TechnicalAnalysis.Application.Mediatr.Queries;
-using TechnicalAnalysis.Domain.Contracts.Input.CryptoFearAndGreedContracts;
+using TechnicalAnalysis.CommonModels.BusinessModels;
 using TechnicalAnalysis.Domain.Interfaces.Infrastructure;
 
 namespace TechnicalAnalysis.Application.Mediatr.Handlers.Get
 {
-    public class GetCryptoFearAndGreedIndexHandler(IPostgreSqlRepository repository) : IRequestHandler<GetCryptoFearAndGreedIndexQuery, List<CryptoFearAndGreedData>>
+    public class GetCryptoFearAndGreedIndexHandler(IPostgreSqlRepository repository) : IRequestHandler<GetCryptoFearAndGreedIndexQuery, List<FearAndGreedModel>>
     {
-        public async Task<List<CryptoFearAndGreedData>> Handle(GetCryptoFearAndGreedIndexQuery getCryptoFearAndGreedIndexQuery, CancellationToken cancellationToken)
+        public async Task<List<FearAndGreedModel>> Handle(GetCryptoFearAndGreedIndexQuery getCryptoFearAndGreedIndexQuery, CancellationToken cancellationToken)
         {
             var result = await repository.GetCryptoFearAndGreedIndexAsync();
             if (result.HasError)
