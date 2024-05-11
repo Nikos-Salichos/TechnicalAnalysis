69316fa52edac077b4375b0f3b0ca1b6d2c3f4c9
Nikos S <niksal1991@gmail.com>
2024-05-11 18:15:24 +0300

    Added new enum

---
+++ TechnicalAnalysis.CommonModels/Enums/ValueClassificationType.cs
@@ -0,0 +1,12 @@
+﻿namespace TechnicalAnalysis.CommonModels.Enums
+{
+    public enum ValueClassificationType
+    {
+        Unknown = 0,
+        ExtremeFear = 1,
+        Fear = 2,
+        Neutral = 3,
+        Greed = 4,
+        ExtremeGreed = 5,
+    }
+}
