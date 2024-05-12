18fe66b55ba2cc6782ea261ba0120939f4b1bafb
Nikos S <niksal1991@gmail.com>
2024-05-12 11:11:50 +0300

    Changed IH

--- TechnicalAnalysis.CommonModels/Indicators/CandlestickFormations/InvertedHammer.cs
+++ TechnicalAnalysis.CommonModels/Indicators/CandlestickFormations/InvertedHammer.cs
@@ -2,14 +2,9 @@
 
 namespace TechnicalAnalysis.CommonModels.Indicators.CandlestickFormations
 {
-    public class InvertedHammer(long candlestickId, int orderOfSignal, bool isInvertedHammer) : BaseIndicator(candlestickId)
+    public class InvertedHammer(long candlestickId) : BaseIndicator(candlestickId)
     {
-        public bool IsInvertedHammer { get; init; } = isInvertedHammer;
-        public int OrderOfSignal { get; init; } = orderOfSignal;
-
-        public static InvertedHammer Create(long candlestickId, int orderOfSignal, bool isInvertedHammer)
-        {
-            return new InvertedHammer(candlestickId, orderOfSignal, isInvertedHammer);
-        }
+        public required bool IsInvertedHammer { get; init; }
+        public int OrderOfSignal { get; init; }
     }
 }
