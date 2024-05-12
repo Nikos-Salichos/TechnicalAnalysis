f79b3c7e11b541bdc9c9f3db7d941c135819637f
Nikos S <niksal1991@gmail.com>
2024-05-12 11:11:24 +0300

    Changed DFD

--- TechnicalAnalysis.CommonModels/Indicators/CandlestickFormations/DragonFlyDoji.cs
+++ TechnicalAnalysis.CommonModels/Indicators/CandlestickFormations/DragonFlyDoji.cs
@@ -2,14 +2,9 @@
 
 namespace TechnicalAnalysis.CommonModels.Indicators.CandlestickFormations
 {
-    public class DragonFlyDoji(long candlestickId, bool isDragonflyDoji, int orderOfSignal) : BaseIndicator(candlestickId)
+    public class DragonFlyDoji(long candlestickId) : BaseIndicator(candlestickId)
     {
-        public bool IsDragonFlyDoji { get; init; } = isDragonflyDoji;
-        public int OrderOfSignal { get; init; } = orderOfSignal;
-
-        public static DragonFlyDoji Create(long candlestickId, bool isDragonflyDoji, int orderOfSignal)
-        {
-            return new DragonFlyDoji(candlestickId, isDragonflyDoji, orderOfSignal);
-        }
+        public required bool IsDragonFlyDoji { get; init; }
+        public int OrderOfSignal { get; init; }
     }
 }
