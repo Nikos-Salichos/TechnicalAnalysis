050cfeca03ebb5a7c544c0c3dd6f223375d2ac42
Nikos S <niksal1991@gmail.com>
2024-05-12 11:10:55 +0300

    Changed fractal range

--- TechnicalAnalysis.CommonModels/Indicators/Advanced/FractalRange.cs
+++ TechnicalAnalysis.CommonModels/Indicators/Advanced/FractalRange.cs
@@ -4,8 +4,8 @@ namespace TechnicalAnalysis.CommonModels.Indicators.Advanced
 {
     public class FractalRange(long candlestickId) : BaseIndicator(candlestickId)
     {
-        public int NumberOfFractalsInNumber { get; init; }
-        public decimal? HighestPriceOfFractals { get; init; }
-        public decimal? LowestPriceOfFractals { get; init; }
+        public required int NumberOfFractalsInNumber { get; init; }
+        public required decimal? HighestPriceOfFractals { get; init; }
+        public required decimal? LowestPriceOfFractals { get; init; }
     }
 }
