49980c88812915ef3b60a42fa70a8420b6f61d34
Nikos S <niksal1991@gmail.com>
2024-05-13 10:00:25 +0300

    Changed crypto fear and greed

--- TechnicalAnalysis.Domain/Contracts/Input/CryptoFearAndGreedContracts/CryptoFearAndGreedData.cs
+++ TechnicalAnalysis.Domain/Contracts/Input/CryptoFearAndGreedContracts/CryptoFearAndGreedData.cs
@@ -4,8 +4,6 @@ namespace TechnicalAnalysis.Domain.Contracts.Input.CryptoFearAndGreedContracts
 {
     public class CryptoFearAndGreedData
     {
-        private string _timestamp = string.Empty;
-
         [JsonPropertyName("value")]
         public string Value { get; init; } = string.Empty;
 
@@ -13,30 +11,6 @@ namespace TechnicalAnalysis.Domain.Contracts.Input.CryptoFearAndGreedContracts
         public string ValueClassification { get; init; } = string.Empty;
 
         [JsonPropertyName("timestamp")]
-        public string Timestamp
-        {
-            get => _timestamp;
-            init
-            {
-                _timestamp = value;
-                if (long.TryParse(_timestamp, out long unixTimestamp))
-                {
-                    // Converts the Unix timestamp to UTC DateTime and then gets only the Date part
-                    TimestampAsDateTime = DateTimeOffset.FromUnixTimeSeconds(unixTimestamp).UtcDateTime.Date;
-                }
-                else
-                {
-                    // Handle invalid timestamp, could also throw an exception or log an error
-                    TimestampAsDateTime = DateTime.MinValue.Date;
-                }
-            }
-        }
-
-        /// <summary>
-        /// The DateTime representation of the Unix timestamp. This property is not serialized.
-        /// </summary>
-        [JsonIgnore]
-        public DateTime TimestampAsDateTime { get; private set; }
+        public string Timestamp { get; init; } = string.Empty;
     }
-
 }
