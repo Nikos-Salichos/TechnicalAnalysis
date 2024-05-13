139ea46da4835ec65685672d1cef941402169854
Nikos S <niksal1991@gmail.com>
2024-05-13 10:00:38 +0300

    Changed interface

---
+++ TechnicalAnalysis.Domain/Interfaces/Infrastructure/ICnnStockFearAndGreedHttpClient.cs
@@ -0,0 +1,10 @@
+﻿using TechnicalAnalysis.Domain.Contracts.Input.Cnn;
+using TechnicalAnalysis.Domain.Interfaces.Utilities;
+
+namespace TechnicalAnalysis.Domain.Interfaces.Infrastructure
+{
+    public interface ICnnStockFearAndGreedHttpClient
+    {
+        Task<IResult<RootStockFearAndGreed, string>> GetCnnStockFearAndGreedIndex();
+    }
+}
