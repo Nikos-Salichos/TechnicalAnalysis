5f4144fe797e0e50bb92d7992e9611c5abfe5aa2
Nikos S <niksal1991@gmail.com>
2024-05-13 10:00:44 +0300

    Changed repo

--- TechnicalAnalysis.Domain/Interfaces/Infrastructure/IPostgreSqlRepository.cs
+++ TechnicalAnalysis.Domain/Interfaces/Infrastructure/IPostgreSqlRepository.cs
@@ -1,5 +1,4 @@
 ﻿using TechnicalAnalysis.CommonModels.BusinessModels;
-using TechnicalAnalysis.Domain.Contracts.Input.CryptoFearAndGreedContracts;
 using TechnicalAnalysis.Domain.Entities;
 using TechnicalAnalysis.Domain.Interfaces.Utilities;
 
@@ -7,8 +6,8 @@ namespace TechnicalAnalysis.Domain.Interfaces.Infrastructure
 {
     public interface IPostgreSqlRepository
     {
-        Task<IResult<List<CryptoFearAndGreedData>, string>> GetCryptoFearAndGreedIndexAsync();
-        Task<IResult<List<StockFearAndGreedDomain>, string>> GetStockFearAndGreedIndexAsync();
+        Task<IResult<List<FearAndGreedModel>, string>> GetCryptoFearAndGreedIndexAsync();
+        Task<IResult<List<FearAndGreedModel>, string>> GetStockFearAndGreedIndexAsync();
         Task<IResult<List<ProviderSynchronization>, string>> GetProvidersAsync();
         Task<IResult<List<Candlestick>, string>> GetCandlesticksAsync();
         Task<IResult<List<Pair>, string>> GetPairsAsync();
@@ -16,8 +15,8 @@ namespace TechnicalAnalysis.Domain.Interfaces.Infrastructure
         Task<IResult<List<AssetRanking>, string>> GetCoinPaprikaAssetsAsync();
         Task<IResult<List<PoolEntity>, string>> GetPoolsAsync();
         Task<IResult<List<DexCandlestick>, string>> GetDexCandlesticksAsync();
-        Task InsertCryptoFearAndGreedIndex(List<CryptoFearAndGreedData> indexes);
-        Task InsertStockFearAndGreedIndex(StockFearAndGreedDomain stockFearAndGreedEntity);
+        Task InsertCryptoFearAndGreedIndex(List<FearAndGreedModel> fearAndGreedModels);
+        Task InsertStockFearAndGreedIndex(List<FearAndGreedModel> fearAndGreedModels);
         Task InsertPairsAsync(List<Pair> pairs);
         Task InsertCandlesticksAsync(List<Candlestick> candlesticks);
         Task InsertAssetsAsync(List<Asset> assets);
