fb0f1ce4e5177c287d6e032662480430d8c1041c
Nikos S <niksal1991@gmail.com>
2024-05-14 06:37:29 +0300

    Updated Adapter

--- TechnicalAnalysis.Infrastructure.Adapters/Adapters/CryptoFearAndGreedAdapter.cs
+++ TechnicalAnalysis.Infrastructure.Adapters/Adapters/CryptoFearAndGreedAdapter.cs
@@ -1,12 +1,12 @@
 ﻿using MediatR;
 using Microsoft.Extensions.Logging;
 using TechnicalAnalysis.Application.Extensions;
+using TechnicalAnalysis.Application.Mappers;
 using TechnicalAnalysis.Application.Mediatr.Commands.Insert;
 using TechnicalAnalysis.Application.Mediatr.Commands.Update;
 using TechnicalAnalysis.Application.Mediatr.Queries;
 using TechnicalAnalysis.CommonModels.BusinessModels;
 using TechnicalAnalysis.CommonModels.Enums;
-using TechnicalAnalysis.Domain.Contracts.Input.CryptoFearAndGreedContracts;
 using TechnicalAnalysis.Domain.Interfaces.Infrastructure;
 
 namespace TechnicalAnalysis.Infrastructure.Adapters.Adapters
@@ -32,6 +32,7 @@ namespace TechnicalAnalysis.Infrastructure.Adapters.Adapters
             var cryptoFearAndGreedIndexData = await mediator.Send(new GetCryptoFearAndGreedIndexQuery());
 
             int calculatedDates = 0;
+            // If there are no elements in cryptoFearAndGreedIndexData we use 0 to fetch data for all available dates
             if (cryptoFearAndGreedIndexData.Count > 0)
             {
                 calculatedDates = CalculateNumberOfDates(cryptoFearAndGreedIndexData);
@@ -41,7 +42,6 @@ namespace TechnicalAnalysis.Infrastructure.Adapters.Adapters
                 }
             }
 
-            // If there are no elements in cryptoFearAndGreedIndexData we use 0 to fetch data from all providers
             var response = await cryptoFearAndGreedHttpClient.GetCryptoFearAndGreedIndex(calculatedDates);
 
             if (response.HasError)
@@ -49,16 +49,23 @@ namespace TechnicalAnalysis.Infrastructure.Adapters.Adapters
                 return false;
             }
 
-            await mediator.Send(new InsertCryptoFearAndGreedIndexCommand(response.SuccessValue));
+            var domainFearAndGreed = response.SuccessValue.ToDomain();
+
+            var distinctItems = domainFearAndGreed.Where(item => item.DateTime.Date != DateTime.UtcNow.Date)
+                                                       .GroupBy(item => item.DateTime.Date)
+                                                       .Select(group => group.First())
+                                                       .ToList();
+
+            await mediator.Send(new InsertCryptoFearAndGreedIndexCommand(distinctItems));
 
             alternativeMeCryptoAndFearProvider.ProviderPairAssetSyncInfo.UpdateProviderInfo();
             await mediator.Send(new UpdateExchangeCommand(alternativeMeCryptoAndFearProvider.ProviderPairAssetSyncInfo));
             return true;
         }
 
-        private static int CalculateNumberOfDates(List<CryptoFearAndGreedData> cryptoFearAndGreedData)
+        private static int CalculateNumberOfDates(List<FearAndGreedModel> cryptoFearAndGreedData)
         {
-            var latestDataBasedOnDatetime = cryptoFearAndGreedData.Max(e => e.TimestampAsDateTime);
+            var latestDataBasedOnDatetime = cryptoFearAndGreedData.Max(e => e.DateTime);
             return (DateTime.Today - latestDataBasedOnDatetime).Days;
         }
     }
