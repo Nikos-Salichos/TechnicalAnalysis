c0e81a7631510f64e5aa0226f878e31e0ae3998b
Nikos S <niksal1991@gmail.com>
2024-05-18 19:46:39 +0300

    Added dataprovider test

---
+++ TechnicalAnalysis.Tests/UnitTests/DataProviderExtensionTest.cs
@@ -0,0 +1,22 @@
+﻿using TechnicalAnalysis.Application.Extensions;
+using TechnicalAnalysis.CommonModels.BusinessModels;
+
+namespace TechnicalAnalysis.Tests.UnitTests
+{
+    public class DataProviderExtensionTest
+    {
+        [Fact]
+        public void IsProviderAssetPairsSyncedToday_ProviderIsNull_ReturnsFalse()
+        {
+            // Arrange
+            ProviderSynchronization provider = null;
+
+            // Act
+            var result = DataProviderExtension.IsProviderAssetPairsSyncedToday(provider);
+
+            // Assert
+            Assert.False(result);
+        }
+
+    }
+}
