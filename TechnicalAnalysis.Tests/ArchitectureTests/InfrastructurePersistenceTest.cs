03e7654aed1f24fdfb95db60d3602f586c8cc623
Nikos S <niksal1991@gmail.com>
2024-05-19 09:20:12 +0300

    Updated Persistence arch tests

--- TechnicalAnalysis.Tests/ArchitectureTests/InfrastructurePersistenceTest.cs
+++ TechnicalAnalysis.Tests/ArchitectureTests/InfrastructurePersistenceTest.cs
@@ -6,10 +6,10 @@ namespace TechnicalAnalysis.Tests.ArchitectureTests
     public class InfrastructurePersistenceTest
     {
         [Fact]
-        public void Infrastructure_ShouldNotHave_DependencyOnAnyProjectExceptDomain()
+        public void InfrastructurePersistence_ShouldNotHave_DependencyOnAnyProjectExceptDomain()
         {
             var result = Types.InNamespace(typeof(Infrastructure.Persistence.Repositories.RedisRepository).Namespace)
-                               .Should().NotHaveDependencyOnAny(BaseArchitectureSetup.ApplicationProject, BaseArchitectureSetup.PresentationProject)
+                               .Should().NotHaveDependencyOnAny(BaseArchitectureSetup.ApplicationProject, BaseArchitectureSetup.InfrastructureHostProject)
                                .GetResult();
 
             result.IsSuccessful.Should().BeTrue();
@@ -21,6 +21,18 @@ namespace TechnicalAnalysis.Tests.ArchitectureTests
             var result = Types.InNamespace(typeof(Infrastructure.Persistence.Repositories.RedisRepository).Namespace)
                                    .Should().HaveNameEndingWith("Repository")
                                    .GetResult();
+
+            result.IsSuccessful.Should().BeTrue();
+        }
+
+        [Fact]
+        public void InfrastructureAdapters_ShouldNotHave_DependencyOnAnyProjectExceptDomainAndApplication()
+        {
+            var result = Types.InNamespace(typeof(Infrastructure.Adapters.Adapters.BinanceAdapter).Namespace)
+                               .Should().NotHaveDependencyOnAny(BaseArchitectureSetup.InfrastructureGatewayProject,
+                                    BaseArchitectureSetup.InfrastructurePersistenceProject,
+                                    BaseArchitectureSetup.InfrastructureHostProject)
+                               .GetResult();
 
             result.IsSuccessful.Should().BeTrue();
         }
