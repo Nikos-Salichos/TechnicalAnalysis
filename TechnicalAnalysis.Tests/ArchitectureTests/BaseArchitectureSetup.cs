74042e5183fe43b88491314d70eccc90e36572b6
Nikos S <niksal1991@gmail.com>
2024-05-19 09:19:45 +0300

    Updated BaseArchitectureSetup

--- TechnicalAnalysis.Tests/ArchitectureTests/BaseArchitectureSetup.cs
+++ TechnicalAnalysis.Tests/ArchitectureTests/BaseArchitectureSetup.cs
@@ -3,13 +3,11 @@ namespace TechnicalAnalysis.Tests.ArchitectureTests
     public static class BaseArchitectureSetup
     {
         public const string ApplicationProject = "TechnicalAnalysis.Application";
-        public const string InfrastructurePersistenceProject = "TechnicalAnalysis.Infrastructure.Persistence";
-        public const string InfrastructureAdaptersProject = "TechnicalAnalysis.Infrastructure.Adapters";
-        public const string InfrastructureHostProject = "TechnicalAnalysis.Infrastructure.Host";
-        public const string InfrastructureClientProject = "TechnicalAnalysis.Infrastructure.Client";
-        public const string PresentationProject = "TechnicalAnalysis.Infrastructure.Host";
-        public const string DomainProject = "TechnicalAnalysis.Domain";
-        public const string DomainProjectEntities = "TechnicalAnalysis.Domain.Entities";
         public const string CommonModelsProject = "TechnicalAnalysis.CommonModels";
+        public const string DomainProject = "TechnicalAnalysis.Domain";
+        public const string InfrastructureAdaptersProject = "TechnicalAnalysis.Infrastructure.Adapters";
+        public const string InfrastructureGatewayProject = "TechnicalAnalysis.Infrastructure.Gateway";
+        public const string InfrastructureHostProject = "TechnicalAnalysis.Infrastructure.Host";
+        public const string InfrastructurePersistenceProject = "TechnicalAnalysis.Infrastructure.Persistence";
     }
 }
