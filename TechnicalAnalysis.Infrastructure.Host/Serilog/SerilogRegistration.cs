1b88610cf9d90bf8e30f6216b16aa758eb4f575b
Nikos S <niksal1991@gmail.com>
2024-05-21 16:19:23 +0300

    Changed serilog registration

--- TechnicalAnalysis.Infrastructure.Host/Serilog/SerilogRegistration.cs
+++ TechnicalAnalysis.Infrastructure.Host/Serilog/SerilogRegistration.cs
@@ -6,8 +6,20 @@ namespace TechnicalAnalysis.Infrastructure.Host.Serilog
     {
         public static void SerilogConfiguration(this WebApplicationBuilder builder)
         {
+            var otelExporterOtlpEndpoint = Environment.GetEnvironmentVariable("OTEL_EXPORTER_OTLP_ENDPOINT");
+
+            if (string.IsNullOrWhiteSpace(otelExporterOtlpEndpoint))
+            {
+                throw new ArgumentNullException($"{nameof(otelExporterOtlpEndpoint)} cannot be string empty or null, please fill variable in docker-compose.yml");
+            }
+
             builder.Host.UseSerilog((_, loggerConfiguration) => loggerConfiguration
-                               .ReadFrom.Configuration(builder.Configuration));
+                .WriteTo.OpenTelemetry(options =>
+                {
+                    options.Endpoint = otelExporterOtlpEndpoint;
+                })
+                .ReadFrom.Configuration(builder.Configuration));
+
         }
     }
 }
