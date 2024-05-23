5ef69fd48ddbcb0c2c564dcb7827960c20acba9c
Nikos S <niksal1991@gmail.com>
2024-05-23 06:57:03 +0300

    Updates SerilogRegistration

--- TechnicalAnalysis.Infrastructure.Host/Serilog/SerilogRegistration.cs
+++ TechnicalAnalysis.Infrastructure.Host/Serilog/SerilogRegistration.cs
@@ -6,19 +6,34 @@ namespace TechnicalAnalysis.Infrastructure.Host.Serilog
     {
         public static void SerilogConfiguration(this WebApplicationBuilder builder)
         {
-            var otelExporterOtlpEndpoint = Environment.GetEnvironmentVariable("OTEL_EXPORTER_OTLP_ENDPOINT");
+            string? otelExporterOtlpEndpoint = null;
 
-            if (string.IsNullOrWhiteSpace(otelExporterOtlpEndpoint))
+            if (builder.Environment.EnvironmentName != "IntegrationTest")
             {
-                throw new ArgumentNullException($"{nameof(otelExporterOtlpEndpoint)} cannot be string empty or null, please fill variable in docker-compose.yml");
+                otelExporterOtlpEndpoint = Environment.GetEnvironmentVariable("OTEL_EXPORTER_OTLP_ENDPOINT");
+
+                if (string.IsNullOrWhiteSpace(otelExporterOtlpEndpoint))
+                {
+                    throw new ArgumentNullException(nameof(otelExporterOtlpEndpoint), "OTEL_EXPORTER_OTLP_ENDPOINT cannot be string empty or null, please fill variable in docker-compose.yml");
+                }
             }
 
-            builder.Host.UseSerilog((_, loggerConfiguration) => loggerConfiguration
-                .WriteTo.OpenTelemetry(options =>
-                {
-                    options.Endpoint = otelExporterOtlpEndpoint;
-                })
-                .ReadFrom.Configuration(builder.Configuration));
+            builder.Host.UseSerilog((context, loggerConfiguration) =>
+            {
+                loggerConfiguration
+                    .WriteTo.OpenTelemetry(options =>
+                    {
+                        if (!string.IsNullOrWhiteSpace(otelExporterOtlpEndpoint))
+                        {
+                            options.Endpoint = otelExporterOtlpEndpoint;
+                        }
+                        // options.ResourceAttributes = new Dictionary<string, object>
+                        // {
+                        //     { "service.name", "TechnicalAnalysis" }
+                        // };
+                    })
+                    .ReadFrom.Configuration(context.Configuration);
+            });
 
         }
     }
