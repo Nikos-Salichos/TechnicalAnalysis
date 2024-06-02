f9a0b6666d48322fe8984d2af2c3e0aaebd81202
Nikos S <niksal1991@gmail.com>
2024-06-02 16:08:15 +0300

    Updated base dex contract

--- TechnicalAnalysis.Domain/Contracts/Input/DexV3/BaseDexContract.cs
+++ TechnicalAnalysis.Domain/Contracts/Input/DexV3/BaseDexContract.cs
@@ -5,17 +5,17 @@ namespace TechnicalAnalysis.Domain.Contracts.Input.DexV3
     public abstract class BaseDexContract
     {
         [JsonPropertyName("feesUSD")]
-        public string FeesRawData { get; init; }
+        public string? FeesRawData { get; init; }
 
         [JsonPropertyName("volumeUsd")]
-        public string VolumeRawData { get; init; }
+        public string? VolumeRawData { get; init; }
 
         [JsonPropertyName("liquidity")]
-        public string LiquidityRawData { get; init; }
+        public string? LiquidityRawData { get; init; }
 
-        public virtual string TotalValueLockedRawData { get; set; }
+        public virtual string? TotalValueLockedRawData { get; set; }
 
         [JsonPropertyName("txCount")]
-        public string NumberOfTrades { get; init; }
+        public string? NumberOfTrades { get; init; }
     }
 }
