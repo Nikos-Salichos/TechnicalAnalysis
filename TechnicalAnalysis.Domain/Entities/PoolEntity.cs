88138870b1609cbc1c4cac06dd734d04cf7e06f4
Nikos S <niksal1991@gmail.com>
2024-06-04 22:02:13 +0300

    Changed to nullable

--- TechnicalAnalysis.Domain/Entities/PoolEntity.cs
+++ TechnicalAnalysis.Domain/Entities/PoolEntity.cs
@@ -6,12 +6,12 @@ namespace TechnicalAnalysis.Domain.Entities
     public class PoolEntity : BaseEntity
     {
         public DataProvider Provider { get; init; }
-        public string PoolContract { get; init; }
+        public string? PoolContract { get; init; }
         public long Token0Id { get; init; }
-        public string Token0Contract { get; init; }
+        public string? Token0Contract { get; init; }
         public long Token1Id { get; init; }
-        public string Token1Contract { get; init; }
-        public string FeeTier { get; init; }
+        public string? Token1Contract { get; init; }
+        public string? FeeTier { get; init; }
         public decimal? Fees { get; init; }
         public long? Liquidity { get; init; }
         public decimal? TotalValueLocked { get; init; }
