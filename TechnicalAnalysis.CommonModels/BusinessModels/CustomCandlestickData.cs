f0b9903f8fe63201afe432b484248f2db807265e
Nikos S <niksal1991@gmail.com>
2024-06-04 22:01:40 +0300

    Added custom data

---
+++ TechnicalAnalysis.CommonModels/BusinessModels/CustomCandlestickData.cs
@@ -0,0 +1,13 @@
+﻿namespace TechnicalAnalysis.CommonModels.BusinessModels
+{
+    public class CustomCandlestickData
+    {
+        public required decimal OpenPrice { get; init; }
+        public required decimal HighPrice { get; init; }
+        public required decimal LowPrice { get; init; }
+        public required decimal ClosePrice { get; init; }
+        public required decimal Volume { get; init; }
+        public DateTime OpenDate { get; init; }
+        public DateTime CloseDate { get; init; }
+    }
+}
