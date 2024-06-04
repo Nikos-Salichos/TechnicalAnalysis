4122822e2b6ca89a27ef894273a80032e7a3767e
Nikos S <niksal1991@gmail.com>
2024-06-04 22:02:24 +0300

    Updated interface

--- TechnicalAnalysis.Domain/Interfaces/Application/IAnalysisService.cs
+++ TechnicalAnalysis.Domain/Interfaces/Application/IAnalysisService.cs
@@ -10,5 +10,6 @@ namespace TechnicalAnalysis.Domain.Interfaces.Application
         Task<List<EnhancedPairResult>> GetEnhancedPairResultsAsync(DataProvider provider, HttpContext? httpContext = null);
         Task<List<PairExtended>> GetIndicatorsByPairNamesAsync(List<string> pairNames, Timeframe timeframe, HttpContext? httpContext = null);
         Task<List<AssetRanking>> GetLayerOneAssetsAsync();
+        Task<List<CandlestickExtended>> GetCustomCandlesticksAnalysisAsync(List<CustomCandlestickData> customCandlestickData);
     }
 }
