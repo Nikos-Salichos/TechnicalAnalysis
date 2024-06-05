274aea93d6701c4efcd03283e3f8b1df641634e2
Nikos S <niksal1991@gmail.com>
2024-06-05 06:24:28 +0300

    Added ParseCandlestickData_SetsDecimalProperty_Correctly

---
+++ TechnicalAnalysis.Tests/UnitTests/CandlestickExtensionTest.cs
@@ -0,0 +1,22 @@
+﻿using TechnicalAnalysis.Application.Extensions;
+using TechnicalAnalysis.Domain.Contracts.Input.Binance;
+
+namespace TechnicalAnalysis.Tests.UnitTests
+{
+    public class CandlestickExtensionTest
+    {
+        [Fact]
+        public void ParseCandlestickData_SetsDecimalProperty_Correctly()
+        {
+            var candlestick = new BinanceCandlestick();
+            var cell = "12345.6789";
+            var property = typeof(BinanceCandlestick).GetProperty(nameof(BinanceCandlestick.OpenPrice));
+
+            CandlestickExtension.ParseCandlestickData(candlestick, cell, property);
+
+            Assert.Equal(12345.6789m, candlestick.OpenPrice);
+        }
+
+
+    }
+}
