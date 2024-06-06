4d5d4816afe45c19d37d21496142838e649a2289
Nikos S <niksal1991@gmail.com>
2024-06-06 21:29:16 +0300

    Converted to primary ctor

--- TechnicalAnalysis.Domain/Messages/MailData.cs
+++ TechnicalAnalysis.Domain/Messages/MailData.cs
@@ -2,56 +2,37 @@
 
 namespace TechnicalAnalysis.Domain.Messages
 {
-    public class MailData
+    public class MailData(List<string> to,
+        string subject,
+        string? body = null,
+        List<MimePart>? attachments = null,
+        string? from = null,
+        string? displayName = null,
+        string? replyTo = null,
+        string? replyToName = null,
+        List<string>? bcc = null,
+        List<string>? cc = null)
     {
         // Receiver
-        public List<string> To { get; }
-        public List<string> Bcc { get; }
+        public List<string> To { get; } = to;
+        public List<string> Bcc { get; } = bcc ?? [];
 
-        public List<string> Cc { get; }
+        public List<string> Cc { get; } = cc ?? [];
 
         // Sender
-        public string? From { get; }
+        public string? From { get; } = from;
 
-        public string? DisplayName { get; }
+        public string? DisplayName { get; } = displayName;
 
-        public string? ReplyTo { get; }
+        public string? ReplyTo { get; } = replyTo;
 
-        public string? ReplyToName { get; }
+        public string? ReplyToName { get; } = replyToName;
 
         // Content
-        public string Subject { get; }
+        public string Subject { get; } = subject;
 
-        public string? Body { get; }
+        public string? Body { get; } = body;
 
-        public List<MimePart>? Attachments { get; }
-
-        public MailData(List<string> to,
-            string subject,
-            string? body = null,
-            List<MimePart>? attachments = null,
-            string? from = null,
-            string? displayName = null,
-            string? replyTo = null,
-            string? replyToName = null,
-            List<string>? bcc = null,
-            List<string>? cc = null)
-        {
-            // Receiver
-            To = to;
-            Bcc = bcc ?? new List<string>();
-            Cc = cc ?? new List<string>();
-
-            // Sender
-            From = from;
-            DisplayName = displayName;
-            ReplyTo = replyTo;
-            ReplyToName = replyToName;
-
-            // Content
-            Subject = subject;
-            Body = body;
-            Attachments = attachments;
-        }
+        public List<MimePart>? Attachments { get; } = attachments;
     }
 }
