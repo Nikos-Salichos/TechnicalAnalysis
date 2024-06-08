7f000a91b2a4e0999251881f25cc6e0a90ac9ec2
Nikos S <niksal1991@gmail.com>
2024-06-08 06:53:14 +0300

    Make props nullable

--- TechnicalAnalysis.Domain/Contracts/Input/Binance/BinanceSymbol.cs
+++ TechnicalAnalysis.Domain/Contracts/Input/Binance/BinanceSymbol.cs
@@ -5,16 +5,16 @@ namespace TechnicalAnalysis.Domain.Contracts.Input.Binance
     public class BinanceSymbol
     {
         [JsonPropertyName("symbol")]
-        public string Symbol { get; init; }
+        public string? Symbol { get; init; }
 
         [JsonPropertyName("baseAsset")]
-        public string BaseAsset { get; init; }
+        public string? BaseAsset { get; init; }
 
         [JsonPropertyName("quoteAsset")]
-        public string QuoteAsset { get; init; }
+        public string? QuoteAsset { get; init; }
 
         [JsonPropertyName("status")]
-        public string Status { get; init; }
+        public string? Status { get; init; }
     }
 
     public class BinanceSymbolComparer : IEqualityComparer<BinanceSymbol>
