b825a35321c7f54455f94caed807444fbebb163a
Nikos S <niksal1991@gmail.com>
2024-06-09 08:23:09 +0300

    Changed model

--- TechnicalAnalysis.CommonModels/BusinessModels/AssetRanking.cs
+++ TechnicalAnalysis.CommonModels/BusinessModels/AssetRanking.cs
@@ -4,8 +4,8 @@ namespace TechnicalAnalysis.CommonModels.BusinessModels
 {
     public sealed class AssetRanking
     {
-        public string Symbol { get; init; } = string.Empty;
-        public string Name { get; init; } = string.Empty;
+        public string? Symbol { get; init; }
+        public string? Name { get; init; }
         public DateTime CreatedDate { get; init; }
         public ProductType AssetType { get; init; }
         public DataProvider DataProvider { get; init; }
