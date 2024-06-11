45c00f33719e3755c66ae334c0ff54f716a0ce43
Nikos S <niksal1991@gmail.com>
2024-06-11 15:28:44 +0300

    Changed contract

--- TechnicalAnalysis.Domain/Contracts/Input/CryptoFearAndGreedContracts/CryptoFearAndGreedData.cs
+++ TechnicalAnalysis.Domain/Contracts/Input/CryptoFearAndGreedContracts/CryptoFearAndGreedData.cs
@@ -5,12 +5,12 @@ namespace TechnicalAnalysis.Domain.Contracts.Input.CryptoFearAndGreedContracts
     public class CryptoFearAndGreedData
     {
         [JsonPropertyName("value")]
-        public string Value { get; init; } = string.Empty;
+        public string? Value { get; init; }
 
         [JsonPropertyName("value_classification")]
-        public string ValueClassification { get; init; } = string.Empty;
+        public string? ValueClassification { get; init; }
 
         [JsonPropertyName("timestamp")]
-        public string Timestamp { get; init; } = string.Empty;
+        public string? Timestamp { get; init; }
     }
 }
