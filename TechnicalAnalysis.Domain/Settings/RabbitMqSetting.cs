bf10a4ca002c4d6a5a20c3e76d47be22262bd294
Nikos S <niksal1991@gmail.com>
2024-06-12 07:39:46 +0300

    Changed to nullable properties

--- TechnicalAnalysis.Domain/Settings/RabbitMqSetting.cs
+++ TechnicalAnalysis.Domain/Settings/RabbitMqSetting.cs
@@ -2,9 +2,9 @@
 {
     public class RabbitMqSetting
     {
-        public string Hostname { get; init; } = string.Empty;
+        public string? Hostname { get; init; }
         public int Port { get; init; }
-        public string Username { get; init; } = string.Empty;
-        public string Password { get; init; } = string.Empty;
+        public string? Username { get; init; }
+        public string? Password { get; init; }
     }
 }
