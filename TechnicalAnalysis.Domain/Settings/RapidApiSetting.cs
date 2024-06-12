37aec839174101669a1a5a1e5ac533b26f1ebdf1
Nikos S <niksal1991@gmail.com>
2024-06-12 07:39:51 +0300

    Changed to nullable properties

--- TechnicalAnalysis.Domain/Settings/RapidApiSetting.cs
+++ TechnicalAnalysis.Domain/Settings/RapidApiSetting.cs
@@ -2,8 +2,8 @@
 {
     public class RapidApiSetting
     {
-        public string StockFearAndGreedUri { get; init; } = string.Empty;
-        public string StockFearAndGreedHost { get; init; } = string.Empty;
-        public string StockFearAndGreedApiKey { get; init; } = string.Empty;
+        public string? StockFearAndGreedUri { get; init; }
+        public string? StockFearAndGreedHost { get; init; }
+        public string? StockFearAndGreedApiKey { get; init; }
     }
 }
