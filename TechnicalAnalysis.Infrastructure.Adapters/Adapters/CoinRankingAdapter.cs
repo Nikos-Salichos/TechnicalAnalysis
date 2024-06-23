0947acafcf8f7d2d774a71cf5e1180b6aa22c664
Nikos S <niksal1991@gmail.com>
2024-06-23 16:30:03 +0300

    Changed coinranking adapter

--- TechnicalAnalysis.Infrastructure.Adapters/Adapters/CoinRankingAdapter.cs
+++ TechnicalAnalysis.Infrastructure.Adapters/Adapters/CoinRankingAdapter.cs
@@ -40,44 +40,6 @@ namespace TechnicalAnalysis.Infrastructure.Adapters.Adapters
 
             await ProcessNewAssets(fetchedAssets, initialResponse.SuccessValue.Data.Coins);
 
-            var totalCoins = initialResponse.SuccessValue.Data.Stats.Total;
-            var coinsPerPage = initialResponse.SuccessValue.Data.Coins.Count;
-            var totalCoinsProcessed = coinsPerPage;
-
-            if (totalCoinsProcessed >= totalCoins)
-            {
-                return true;
-            }
-
-            var totalPages = (int)Math.Ceiling((double)totalCoins / coinsPerPage);
-
-            for (int pageIndex = 1; pageIndex < totalPages; pageIndex++)
-            {
-                var response = await coinRankingHttpClient.SyncAssets(pageIndex);
-                if (response.HasError)
-                {
-                    if (pageIndex >= 2)
-                    {
-                        // Free version of API in CoinRanking allows only 1 page of data, any page >=2 will fail
-                        logger.LogError("{DataProvider} response error on page {PageIndex} with {FailValue} in Premium API", nameof(provider), pageIndex, response.FailValue);
-                        continue;
-                    }
-                    else
-                    {
-                        logger.LogError("{DataProvider} response error on page {PageIndex} with {FailValue} in Free API", nameof(provider), pageIndex, response.FailValue);
-                        return false;
-                    }
-                }
-
-                await ProcessNewAssets(fetchedAssets, response.SuccessValue.Data.Coins);
-
-                totalCoinsProcessed += response.SuccessValue.Data.Coins.Count;
-                if (totalCoinsProcessed >= totalCoins)
-                {
-                    break;
-                }
-            }
-
             coinRankingProvider.ProviderPairAssetSyncInfo.UpdateProviderInfo();
             await mediator.Send(new UpdateExchangeCommand(coinRankingProvider.ProviderPairAssetSyncInfo));
 
