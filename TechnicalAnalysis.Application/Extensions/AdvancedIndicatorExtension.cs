091bf1dd98e7c71931bda806c7c94f1715cfa26d
Nikos S <niksal1991@gmail.com>
2024-06-28 11:31:55 +0300

    Simplify code

--- TechnicalAnalysis.Application/Extensions/AdvancedIndicatorExtension.cs
+++ TechnicalAnalysis.Application/Extensions/AdvancedIndicatorExtension.cs
@@ -1290,7 +1290,7 @@ namespace TechnicalAnalysis.Application.Extensions
             var obv1 = currentIndex >= 1 ? candlesticks[currentIndex - 1].OnBalanceVolumes.FirstOrDefault()?.Value : null;
             var obv2 = currentIndex >= 2 ? candlesticks[currentIndex - 2].OnBalanceVolumes.FirstOrDefault()?.Value : null;
             var obv3 = currentIndex >= 3 ? candlesticks[currentIndex - 3].OnBalanceVolumes.FirstOrDefault()?.Value : null;
-            var obv4 = currentIndex - 4 >= 0 ? candlesticks[currentIndex - 4].OnBalanceVolumes.FirstOrDefault()?.Value : null;
+            var obv4 = currentIndex >= 4 ? candlesticks[currentIndex - 4].OnBalanceVolumes.FirstOrDefault()?.Value : null;
 
             if (obv == null || obv1 == null || obv2 == null || obv3 == null || obv4 == null)
             {
