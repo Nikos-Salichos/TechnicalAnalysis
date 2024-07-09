69c7f2c823ddfa7dff5c4ab455c7e6979d4bda4a
Nikos S <niksal1991@gmail.com>
2024-07-09 21:34:41 +0300

    Changed Interface

--- TechnicalAnalysis.Domain/Interfaces/Infrastructure/IRedisRepository.cs
+++ TechnicalAnalysis.Domain/Interfaces/Infrastructure/IRedisRepository.cs
@@ -2,7 +2,7 @@
 {
     public interface IRedisRepository
     {
-        Task SetRecordAsync<T>(string recordId, T data, TimeSpan? absoluteExpireTime = null, TimeSpan? slidingExpireTime = null);
+        Task SetRecordAsync<T>(string? recordId, T data, TimeSpan? absoluteExpireTime = null, TimeSpan? slidingExpireTime = null);
         Task<T?> GetRecordAsync<T>(string recordId);
     }
 }
