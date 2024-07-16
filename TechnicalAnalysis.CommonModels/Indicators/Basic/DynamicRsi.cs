d511cfa5c15e0ce8427ed921ddabae6581da37fd
Nikos S <niksal1991@gmail.com>
2024-07-16 06:35:57 +0300

    Added dynamic rsi

---
+++ TechnicalAnalysis.CommonModels/Indicators/Basic/DynamicRsi.cs
@@ -0,0 +1,16 @@
+﻿using TechnicalAnalysis.CommonModels.BaseClasses;
+
+namespace TechnicalAnalysis.CommonModels.Indicators.Basic
+{
+    public class DynamicRsi(long candlestickId) : BaseIndicator(candlestickId)
+    {
+        public required long Period { get; init; }
+        public required double Overbought { get; init; }
+        public required double Oversold { get; init; }
+        public required double? Value { get; init; }
+        public required long NumberOfRsiLowerThanPreviousRsis { get; init; }
+        public bool RsiChangedDirectionFromPreviousCandlestick { get; set; }
+        public long NumberOfConsecutiveLowerRsi { get; set; }
+        public long NumberOfConsecutiveHigherRsi { get; set; }
+    }
+}
