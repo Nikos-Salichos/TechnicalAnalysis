5a3785ee7401acbcd3feeafab56485b08a73a06e
Nikos S <niksal1991@gmail.com>
2024-07-26 15:55:45 +0300

    Updated MailSettings

--- TechnicalAnalysis.Domain/Messages/MailSettings.cs
+++ TechnicalAnalysis.Domain/Messages/MailSettings.cs
@@ -1,14 +1,30 @@
-﻿namespace TechnicalAnalysis.Domain.Messages
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TechnicalAnalysis.Domain.Messages
 {
     public class MailSettings
     {
         public string? DisplayName { get; init; }
+
+        [Required]
         public string? From { get; init; }
+
+        [Required]
         public string? EmailAddress { get; init; }
+
+        [Required]
         public string? Password { get; init; }
+
+        [Required]
         public string? Host { get; init; }
+
+        [Required]
         public int Port { get; init; }
+
+        [Required]
         public bool UseSSL { get; init; }
+
+        [Required]
         public bool UseStartTls { get; init; }
     }
 }
