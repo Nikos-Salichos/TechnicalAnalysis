c007839250dc44d317a485ad3f55666dc899c0a1
Nikos S <niksal1991@gmail.com>
2024-07-29 09:17:30 +0300

    Added Attributes

--- TechnicalAnalysis.Domain/Settings/CoinMarketCapSetting.cs
+++ TechnicalAnalysis.Domain/Settings/CoinMarketCapSetting.cs
@@ -1,8 +1,13 @@
-﻿namespace TechnicalAnalysis.Domain.Settings
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TechnicalAnalysis.Domain.Settings
 {
     public class CoinMarketCapSetting
     {
-        public string? ApiKey { get; init; }
-        public string? ListingsLatestEndpoint { get; init; }
+        [Required]
+        public string ApiKey { get; init; }
+
+        [Required]
+        public string ListingsLatestEndpoint { get; init; }
     }
 }
