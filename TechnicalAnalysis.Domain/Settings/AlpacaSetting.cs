ceaa6214b172e347c2f74b66c448b9132f5b7ee0
Nikos S <niksal1991@gmail.com>
2024-07-29 09:17:27 +0300

    Added Attributes

--- TechnicalAnalysis.Domain/Settings/AlpacaSetting.cs
+++ TechnicalAnalysis.Domain/Settings/AlpacaSetting.cs
@@ -1,8 +1,13 @@
-﻿namespace TechnicalAnalysis.Domain.Settings
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TechnicalAnalysis.Domain.Settings
 {
     public class AlpacaSetting
     {
+        [Required]
         public string ApiKey { get; init; }
+
+        [Required]
         public string ApiSecret { get; init; }
     }
 }
