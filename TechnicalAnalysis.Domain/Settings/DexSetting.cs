a4e40b2af1640ffb4c9ec0999db8282c8f9e8827
Nikos S <niksal1991@gmail.com>
2024-07-29 09:17:33 +0300

    Added Attributes

--- TechnicalAnalysis.Domain/Settings/DexSetting.cs
+++ TechnicalAnalysis.Domain/Settings/DexSetting.cs
@@ -1,8 +1,13 @@
-﻿namespace TechnicalAnalysis.Domain.Settings
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TechnicalAnalysis.Domain.Settings
 {
     public class DexSetting
     {
+        [Required]
         public string UniswapEndpoint { get; init; }
+
+        [Required]
         public string PancakeswapEndpoint { get; init; }
     }
 }
