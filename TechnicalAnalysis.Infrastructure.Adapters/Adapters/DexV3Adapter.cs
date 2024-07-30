b90f3894de8ddf0a9634b4f585a6539c5829b948
Nikos S <niksal1991@gmail.com>
2024-07-30 12:41:57 +0300

    Added AssetType

--- TechnicalAnalysis.Infrastructure.Adapters/Adapters/DexV3Adapter.cs
+++ TechnicalAnalysis.Infrastructure.Adapters/Adapters/DexV3Adapter.cs
@@ -102,13 +102,13 @@ namespace TechnicalAnalysis.Infrastructure.Adapters.Adapters
                 if (!existingSymbols.Contains(pool.Token0.Symbol))
                 {
                     existingSymbols.Add(pool.Token0.Symbol);
-                    newAssets.Add(new Asset { Symbol = pool.Token0.Symbol });
+                    newAssets.Add(new Asset { Symbol = pool.Token0.Symbol, ProductType = ProductType.CryptoRegardlessLayer });
                 }
 
                 if (!existingSymbols.Contains(pool.Token1.Symbol))
                 {
                     existingSymbols.Add(pool.Token1.Symbol);
-                    newAssets.Add(new Asset { Symbol = pool.Token1.Symbol });
+                    newAssets.Add(new Asset { Symbol = pool.Token1.Symbol, ProductType = ProductType.CryptoRegardlessLayer });
                 }
             }
 
