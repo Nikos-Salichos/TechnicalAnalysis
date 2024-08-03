e80f402cd878bd4a0305190599eb53b84d0db9bc
Nikos S <niksal1991@gmail.com>
2024-08-03 08:32:02 +0300

    Added ProductType

--- TechnicalAnalysis.Domain/Contracts/Output/EnhancedPairResult.cs
+++ TechnicalAnalysis.Domain/Contracts/Output/EnhancedPairResult.cs
@@ -1,8 +1,11 @@
-﻿namespace TechnicalAnalysis.Domain.Contracts.Output
+﻿using TechnicalAnalysis.CommonModels.Enums;
+
+namespace TechnicalAnalysis.Domain.Contracts.Output
 {
     public class EnhancedPairResult
     {
         public string? Symbol { get; init; }
+        public ProductType ProductType { get; init; }
         public List<EnhancedScanGroup> EnhancedScans { get; init; } = [];
     }
 }
