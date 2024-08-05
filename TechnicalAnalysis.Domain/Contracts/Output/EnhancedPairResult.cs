31231977140dd4373f4459f662875eaa7347d7b2
Nikos S <niksal1991@gmail.com>
2024-08-05 08:58:54 +0300

    Added product type

--- TechnicalAnalysis.Domain/Contracts/Output/EnhancedPairResult.cs
+++ TechnicalAnalysis.Domain/Contracts/Output/EnhancedPairResult.cs
@@ -5,7 +5,9 @@ namespace TechnicalAnalysis.Domain.Contracts.Output
     public class EnhancedPairResult
     {
         public string? Symbol { get; init; }
+
         public ProductType ProductType { get; init; }
+
         public List<EnhancedScanGroup> EnhancedScans { get; init; } = [];
     }
 }
