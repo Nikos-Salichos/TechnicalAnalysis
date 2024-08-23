5b606b25beec23c6a168e515fb38f1357ad0aa93
Nikolaos Salichos <nikolaos.salichos@betssongroup.com>
2024-08-23 07:58:00 +0300

    Added required props

--- TechnicalAnalysis.CommonModels/BusinessModels/AssetRanking.cs
+++ TechnicalAnalysis.CommonModels/BusinessModels/AssetRanking.cs
@@ -4,10 +4,10 @@ namespace TechnicalAnalysis.CommonModels.BusinessModels
 {
     public sealed class AssetRanking
     {
-        public string? Symbol { get; init; }
-        public string? Name { get; init; }
-        public DateTime CreatedDate { get; init; }
-        public ProductType ProductType { get; init; }
-        public DataProvider DataProvider { get; init; }
+        public required string? Symbol { get; init; }
+        public required string? Name { get; init; }
+        public required DateTime CreatedDate { get; init; } = DateTime.UtcNow;
+        public required ProductType ProductType { get; init; }
+        public required DataProvider DataProvider { get; init; }
     }
 }
