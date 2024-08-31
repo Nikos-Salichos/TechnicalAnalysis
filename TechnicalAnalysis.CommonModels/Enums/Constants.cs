f264a89c490204346cb923380f35f0f572b684ec
Nikolaos Salichos <nikolaos.salichos@betssongroup.com>
2024-08-31 08:51:48 +0300

    Updated constants

--- TechnicalAnalysis.CommonModels/Enums/Constants.cs
+++ TechnicalAnalysis.CommonModels/Enums/Constants.cs
@@ -28,6 +28,7 @@
         public const int FractalsPeriod = 2;
         public const int BollingerBandsPeriod = 20;
         public const int BollingerBandsStdPeriod = 2;
+        public const int DonchianChannelPeriod = 20;
 
         public const int DecimalPlacesRound = 20;
         public const double StockThresholdPercentage = 0;
