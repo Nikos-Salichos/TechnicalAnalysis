fc6eb47a29a36ffd3bb463cb6a70c9d8e4a51a5a
Nikos S <niksal1991@gmail.com>
2024-09-26 09:13:13 +0300

    Changed AnalysisController

--- TechnicalAnalysis.Infrastructure.Host/Controllers/AnalysisController.cs
+++ TechnicalAnalysis.Infrastructure.Host/Controllers/AnalysisController.cs
@@ -1,7 +1,5 @@
-﻿using FluentValidation;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.RateLimiting;
-using TechnicalAnalysis.CommonModels.ApiRequests;
 using TechnicalAnalysis.CommonModels.BusinessModels;
 using TechnicalAnalysis.CommonModels.Enums;
 using TechnicalAnalysis.Domain.Interfaces.Application;
@@ -11,39 +9,8 @@ namespace TechnicalAnalysis.Infrastructure.Host.Controllers
 {
     [Route("api/v1/analysis")]
     [ApiController]
-    public class AnalysisController(ISyncService syncService, IAnalysisService analysisService, ILogger<AnalysisController> logger,
-         IValidator<DataProviderTimeframeRequest> dataProviderTimeframeRequest) : ControllerBase
+    public class AnalysisController(IAnalysisService analysisService, ILogger<AnalysisController> logger) : ControllerBase
     {
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        [ProducesResponseType(StatusCodes.Status429TooManyRequests)]
-        [EnableRateLimiting("fixed-by-ip")]
-        [HttpGet("SynchronizeProviders")]
-        public Task<IActionResult> SynchronizeProvidersAsync(
-            [FromQuery] DataProvider dataProvider = DataProvider.All,
-            [FromQuery] Timeframe timeframe = Timeframe.Daily)
-        {
-            var request = new DataProviderTimeframeRequest(dataProvider, timeframe);
-            logger.LogInformation("DataProviderTimeframeRequest {@dataProviderTimeframeRequest}", request);
-
-            var validationResult = dataProviderTimeframeRequest.Validate(request);
-            if (!validationResult.IsValid)
-            {
-                logger.LogError("ValidationResult {@validationResult}", validationResult);
-                var errors = validationResult.Errors.Select(e => e.ErrorMessage);
-                return Task.FromResult<IActionResult>(BadRequest(errors));
-            }
-
-            return SynchronizeInternalAsync(request);
-
-            async Task<IActionResult> SynchronizeInternalAsync(DataProviderTimeframeRequest request)
-            {
-                await syncService.SynchronizeProvidersAsync(request);
-                return NoContent();
-            }
-        }
-
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [ProducesResponseType(StatusCodes.Status429TooManyRequests)]
