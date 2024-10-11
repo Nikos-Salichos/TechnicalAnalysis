9eeb5ba5981a1a81e7d0fc2dc4d660ebd2141bb6
Nikos S <niksal1991@gmail.com>
2024-10-11 07:06:44 +0300

    Added FredApiSetting

---
+++ TechnicalAnalysis.Domain/Settings/FredApiSetting.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TechnicalAnalysis.Domain.Settings
+{
+    public class FredApiSetting
+    {
+        [Required]
+        public string ApiKey { get; init; }
+
+        [Required]
+        public string VixEndpoint { get; init; }
+    }
+}
