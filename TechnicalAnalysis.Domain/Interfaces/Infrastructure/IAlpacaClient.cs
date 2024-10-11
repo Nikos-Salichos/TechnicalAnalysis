7e0ee560f06b5b9263bbf2feee8784475595b7cd
Nikos S <niksal1991@gmail.com>
2024-10-11 19:13:57 +0300

    Renamed AlpacaClient

---
+++ TechnicalAnalysis.Domain/Interfaces/Infrastructure/IAlpacaClient.cs
@@ -0,0 +1,10 @@
+﻿using Alpaca.Markets;
+using TechnicalAnalysis.Domain.Interfaces.Utilities;
+
+namespace TechnicalAnalysis.Domain.Interfaces.Infrastructure
+{
+    public interface IAlpacaClient
+    {
+        Task<IResult<IMultiPage<IBar>, string>> GetAlpacaData(string pairName, DateTime fromDateTime, DateTime toDateTime, BarTimeFrame barTimeFrame);
+    }
+}
