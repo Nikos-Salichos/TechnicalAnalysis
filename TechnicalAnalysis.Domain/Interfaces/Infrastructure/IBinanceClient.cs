84181f8f1ad1ee0aabb006ca15bee01664e10db2
Nikos S <niksal1991@gmail.com>
2024-10-12 10:55:53 +0300

    Changed BinanceClient

---
+++ TechnicalAnalysis.Domain/Interfaces/Infrastructure/IBinanceClient.cs
@@ -0,0 +1,11 @@
+﻿using TechnicalAnalysis.Domain.Contracts.Input.Binance;
+using TechnicalAnalysis.Domain.Interfaces.Utilities;
+
+namespace TechnicalAnalysis.Domain.Interfaces.Infrastructure
+{
+    public interface IBinanceClient
+    {
+        Task<IResult<BinanceExchangeInfoResponse, string>> GetBinanceAssetsAndPairs();
+        Task<IResult<object[][], string>> GetBinanceCandlesticks(Dictionary<string, string>? queryParams = null);
+    }
+}
