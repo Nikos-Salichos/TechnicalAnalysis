a469ff6d2a2af2df0618efa337dfb0dcd14ac50d
Nikos S <niksal1991@gmail.com>
2024-10-14 10:28:57 +0300

    Changed CoinPaprika

---
+++ TechnicalAnalysis.Domain/Interfaces/Infrastructure/ICoinPaprikaClient.cs
@@ -0,0 +1,10 @@
+﻿using TechnicalAnalysis.Domain.Contracts.Input.CoinPaprika;
+using TechnicalAnalysis.Domain.Interfaces.Utilities;
+
+namespace TechnicalAnalysis.Domain.Interfaces.Infrastructure
+{
+    public interface ICoinPaprikaClient
+    {
+        Task<IResult<List<CoinPaprikaAssetContract>, string>> SyncAssets();
+    }
+}
