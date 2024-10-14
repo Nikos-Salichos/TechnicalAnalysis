ce61ef23622d47ceb378a9fe1c2f43a10087e122
Nikos S <niksal1991@gmail.com>
2024-10-14 10:29:27 +0300

    Changed CoinRanking

---
+++ TechnicalAnalysis.Domain/Interfaces/Infrastructure/ICoinRankingClient.cs
@@ -0,0 +1,10 @@
+﻿using TechnicalAnalysis.Domain.Contracts.Input.CoinRanking;
+using TechnicalAnalysis.Domain.Interfaces.Utilities;
+
+namespace TechnicalAnalysis.Domain.Interfaces.Infrastructure
+{
+    public interface ICoinRankingClient
+    {
+        Task<IResult<CoinRankingAssetContract, string>> SyncAssets(int offset);
+    }
+}
