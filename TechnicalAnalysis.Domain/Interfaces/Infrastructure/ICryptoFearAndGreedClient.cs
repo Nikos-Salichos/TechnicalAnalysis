09197ddf8b1ed6d869902d56f27f9c46600fc1fb
Nikos S <niksal1991@gmail.com>
2024-10-15 08:27:34 +0300

    Changed fear and greed

---
+++ TechnicalAnalysis.Domain/Interfaces/Infrastructure/ICryptoFearAndGreedClient.cs
@@ -0,0 +1,10 @@
+﻿using TechnicalAnalysis.Domain.Contracts.Input.CryptoFearAndGreedContracts;
+using TechnicalAnalysis.Domain.Interfaces.Utilities;
+
+namespace TechnicalAnalysis.Domain.Interfaces.Infrastructure
+{
+    public interface ICryptoFearAndGreedClient
+    {
+        Task<IResult<List<CryptoFearAndGreedData>, string>> GetCryptoFearAndGreedIndex(int numberOfDates);
+    }
+}
