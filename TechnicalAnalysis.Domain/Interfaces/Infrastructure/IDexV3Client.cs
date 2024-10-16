94818b999414dc0d5b3dfc9f21d03b687f046d5b
Nikos S <niksal1991@gmail.com>
2024-10-16 08:12:05 +0300

    Changed IDexClient

---
+++ TechnicalAnalysis.Domain/Interfaces/Infrastructure/IDexV3Client.cs
@@ -0,0 +1,11 @@
+﻿using TechnicalAnalysis.CommonModels.Enums;
+using TechnicalAnalysis.Domain.Contracts.Input.DexV3;
+using TechnicalAnalysis.Domain.Interfaces.Utilities;
+
+namespace TechnicalAnalysis.Domain.Interfaces.Infrastructure
+{
+    public interface IDexV3Client
+    {
+        Task<IResult<DexV3ApiResponse, string>> GetMostActivePoolsAsync(int numberOfPools, int numberOfData, DataProvider provider);
+    }
+}
