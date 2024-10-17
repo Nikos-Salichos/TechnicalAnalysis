a811339ba7b21bff3e18fb73309d1c1f44dbcfbc
Nikos S <niksal1991@gmail.com>
2024-10-17 08:12:39 +0300

    Changed FredApiClient

---
+++ TechnicalAnalysis.Infrastructure.Adapters/Adapters/FredApiAdapter.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using TechnicalAnalysis.CommonModels.BusinessModels;
+using TechnicalAnalysis.CommonModels.Enums;
+using TechnicalAnalysis.Domain.Interfaces.Infrastructure;
+
+namespace TechnicalAnalysis.Infrastructure.Adapters.Adapters
+{
+    public class FredApiAdapter(ILogger<FredApiAdapter> logger, IMediator mediator, IFredApiClient fredApiHttpClient) : IAdapter
+    {
+        public async Task<bool> Sync(DataProvider provider, Timeframe timeframe, List<ProviderSynchronization> exchanges)
+        {
+            var result = await fredApiHttpClient.SyncVix();
+
+            if (result.HasError)
+            {
+                logger.LogError("{DataProvider} initial response error {FailValue}", nameof(provider), result.FailValue);
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
