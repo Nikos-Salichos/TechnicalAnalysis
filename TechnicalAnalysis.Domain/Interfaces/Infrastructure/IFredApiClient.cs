00544a84754e432184374898364f4dfddf20f45b
Nikos S <niksal1991@gmail.com>
2024-10-17 08:12:21 +0300

    Added FredApiClient

---
+++ TechnicalAnalysis.Domain/Interfaces/Infrastructure/IFredApiClient.cs
@@ -0,0 +1,10 @@
+﻿using TechnicalAnalysis.Domain.Contracts.Input.FredApiContracts;
+using TechnicalAnalysis.Domain.Interfaces.Utilities;
+
+namespace TechnicalAnalysis.Domain.Interfaces.Infrastructure
+{
+    public interface IFredApiClient
+    {
+        Task<IResult<FredVixContract, string>> SyncVix();
+    }
+}
