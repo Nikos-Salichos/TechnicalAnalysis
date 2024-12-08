11cc57ceb5ad5648431d650e2b8a2b52f75af331
Nikos-Salichos <niksal1991@gmail.com>
2024-12-08 10:21:53 +0100

    Changed to Result

--- TechnicalAnalysis.Domain/Interfaces/Infrastructure/ICnnStockFearAndGreedClient.cs
+++ TechnicalAnalysis.Domain/Interfaces/Infrastructure/ICnnStockFearAndGreedClient.cs
@@ -1,10 +1,11 @@
 ﻿using TechnicalAnalysis.Domain.Contracts.Input.Cnn;
 using TechnicalAnalysis.Domain.Interfaces.Utilities;
+using TechnicalAnalysis.Domain.Utilities;
 
 namespace TechnicalAnalysis.Domain.Interfaces.Infrastructure
 {
     public interface ICnnStockFearAndGreedClient
     {
-        Task<IResult<RootStockFearAndGreed, string>> GetCnnStockFearAndGreedIndex();
+        Task<Result<RootStockFearAndGreed, string>> GetCnnStockFearAndGreedIndex();
     }
 }
