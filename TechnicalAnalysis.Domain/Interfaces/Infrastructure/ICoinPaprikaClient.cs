ccc622c2f10b946d4127bf65947b3974f970c965
Nikos-Salichos <niksal1991@gmail.com>
2024-12-13 00:11:35 +0100

    Changed client

--- TechnicalAnalysis.Domain/Interfaces/Infrastructure/ICoinPaprikaClient.cs
+++ TechnicalAnalysis.Domain/Interfaces/Infrastructure/ICoinPaprikaClient.cs
@@ -1,10 +1,11 @@
 ﻿using TechnicalAnalysis.Domain.Contracts.Input.CoinPaprika;
 using TechnicalAnalysis.Domain.Interfaces.Utilities;
+using TechnicalAnalysis.Domain.Utilities;
 
 namespace TechnicalAnalysis.Domain.Interfaces.Infrastructure
 {
     public interface ICoinPaprikaClient
     {
-        Task<IResult<List<CoinPaprikaAssetContract>, string>> SyncAssets();
+        Task<Result<List<CoinPaprikaAssetContract>, string>> SyncAssets();
     }
 }
