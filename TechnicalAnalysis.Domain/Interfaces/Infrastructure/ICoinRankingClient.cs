987cd894aa82bbfe34fe663f4636e40770a54d96
Nikos-Salichos <niksal1991@gmail.com>
2024-12-13 00:11:47 +0100

    Changed client

--- TechnicalAnalysis.Domain/Interfaces/Infrastructure/ICoinRankingClient.cs
+++ TechnicalAnalysis.Domain/Interfaces/Infrastructure/ICoinRankingClient.cs
@@ -1,10 +1,11 @@
 ﻿using TechnicalAnalysis.Domain.Contracts.Input.CoinRanking;
 using TechnicalAnalysis.Domain.Interfaces.Utilities;
+using TechnicalAnalysis.Domain.Utilities;
 
 namespace TechnicalAnalysis.Domain.Interfaces.Infrastructure
 {
     public interface ICoinRankingClient
     {
-        Task<IResult<CoinRankingAssetContract, string>> SyncAssets(int offset);
+        Task<Result<CoinRankingAssetContract, string>> SyncAssets(int offset);
     }
 }
